aad8d3185f2f5578e24da5a465689fbbe6fc94d7
Ken Pratt <ken@kenpratt.net>
2019-12-08 21:24:58 -0800

    day 5 part 1

--- day-05-sunny-with-a-chance-of-asteroids/main.rb
+++ day-05-sunny-with-a-chance-of-asteroids/main.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 require_relative '../utils/log'
 require_relative '../utils/profile'
 
@@ -14,27 +16,130 @@ def modify_input(input, noun, verb)
   out  
 end
 
-def part1(input)
-  arr = input.clone
-  i = 0
-  while i < arr.size
-    op = arr[i]
-    case op
+class Instruction
+  attr_reader :opcode
+
+  def initialize(op)
+    @op = op
+    @opcode = op % 100
+  end
+
+  def param_count
+    case @opcode
     when 1, 2
-      val1 = arr[arr[i + 1]]
-      val2 = arr[arr[i + 2]]
-      res = op == 1 ? val1 + val2 : val1 * val2
-      arr[arr[i + 3]] = res
-      i += 4
-    when 2
-      i += 4
+      3
+    when 3, 4
+      1
     when 99
-      return arr
+      0
     else
-      raise "Unknown opcode: #{op}"
+      raise "Unknown opcode: #{@opcode}"
     end
   end
-  raise "Didn't halt"
+
+  def size
+    param_count + 1
+  end
+
+  def param1_mode
+    (@op / 100) % 10
+  end
+
+  def param2_mode
+    (@op / 1000) % 10
+  end
+
+  def param3_mode
+    (@op / 10000) % 10
+  end
+end
+
+class IntcodeComputer
+  attr_reader :memory, :output
+
+  def initialize(program, input)
+    @memory = program.clone
+    @instruction_pointer = 0
+    @input = input.clone
+    @output = []
+    @halt = false
+  end
+
+  def self.run(program, input=[])
+    computer = IntcodeComputer.new(program, input)
+    computer.run
+    computer
+  end
+
+  def run
+    @halt = false
+    while !@halt && @instruction_pointer < @memory.size
+      op = read(0, 1)
+      inst = Instruction.new(op)
+      execute(inst)
+      @instruction_pointer += inst.size
+    end
+    validate_output!
+  end
+
+  def execute(inst)
+    case inst.opcode
+    when 1
+      val1 = read(1, inst.param1_mode)
+      val2 = read(2, inst.param2_mode)
+      write(3, inst.param3_mode, val1 + val2)
+    when 2
+      val1 = read(1, inst.param1_mode)
+      val2 = read(2, inst.param2_mode)
+      write(3, inst.param3_mode, val1 * val2)
+    when 3
+      val = @input.shift
+      write(1, inst.param1_mode, val)
+    when 4
+      @output << read(1, inst.param1_mode)
+    when 99
+      @halt = true
+    else
+      raise "Unknown opcode: #{@opcode}"
+    end
+  end
+
+  def read(offset, mode)
+    case mode
+    when 0
+      # position mode (indirect)
+      pos = @memory[@instruction_pointer + offset]
+      @memory[pos]
+    when 1
+      # immediate mode (direct)
+      @memory[@instruction_pointer + offset]
+    else
+      raise "Unknown read mode: #{mode}"
+    end
+  end
+
+  def write(offset, mode, to_write)
+    case mode
+    when 0
+      # position mode (indirect)
+      pos = @memory[@instruction_pointer + offset]
+      @memory[pos] = to_write
+    when 1
+      raise "Cannot use immedate mode for writes"
+    else
+      raise "Unknown write mode: #{mode}"
+    end
+  end
+
+  def validate_output!
+    output[0...-1].each_with_index do |val, i|
+      raise "Bad output value at index #{i}: #{val}" unless val == 0
+    end
+  end
+end
+
+def part1(program)
+  IntcodeComputer.run(program, [1])
 end
 
 def part2(input)
@@ -46,7 +151,7 @@ def main
   input = process_input(raw_input)
 
   log.info "Part 1:"
-  log.info measure{part1(input)}
+  log.info measure{part1(input).output.last}
 
   log.info "Part 2:"
   log.info measure{part2(input)}
--- day-05-sunny-with-a-chance-of-asteroids/test.rb
+++ day-05-sunny-with-a-chance-of-asteroids/test.rb
@@ -11,12 +11,13 @@ class TestOldTests < Minitest::Test
     ['2,3,0,3,99', '2,3,0,6,99'],
     ['2,4,4,5,99,0', '2,4,4,5,99,9801'],
     ['1,1,1,4,99,5,6,0,99', '30,1,1,4,2,5,6,0,99'],
+    ['1101,100,-1,4,0', '1101,100,-1,4,99'],
   ]
 
   def test_examples
     TESTS.each do |raw_input, output|
       input = process_input(raw_input)
-      res = part1(input)
+      res = part1(input).memory
       assert_equal(output, res.join(','))
     end
   end
