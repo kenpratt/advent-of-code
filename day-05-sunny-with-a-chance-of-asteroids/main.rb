d96e2fcd1aab760b2c4d3dd0a61982db6acc7665
Ken Pratt <ken@kenpratt.net>
2019-12-09 13:23:32 -0800

    day 5 part 2

--- day-05-sunny-with-a-chance-of-asteroids/main.rb
+++ day-05-sunny-with-a-chance-of-asteroids/main.rb
@@ -5,11 +5,11 @@ require_relative '../utils/profile'
 
 INPUT_FILE = File.join(__dir__, 'input.txt')
 
-def process_input(raw_input)
+def parse_program(raw_input)
   raw_input.strip.split(',').map(&:to_i)
 end
 
-def modify_input(input, noun, verb)
+def modify_program(input, noun, verb)
   out = input.clone
   out[1] = noun
   out[2] = verb
@@ -30,6 +30,10 @@ class Instruction
       3
     when 3, 4
       1
+    when 5, 6
+      2
+    when 7, 8
+      3
     when 99
       0
     else
@@ -76,8 +80,14 @@ class IntcodeComputer
     while !@halt && @instruction_pointer < @memory.size
       op = read(0, 1)
       inst = Instruction.new(op)
+      curr_insruction_pointer = @instruction_pointer
+
       execute(inst)
-      @instruction_pointer += inst.size
+
+      # advance instruction pointer, unless the instruction modified it
+      if @instruction_pointer == curr_insruction_pointer
+        @instruction_pointer += inst.size
+      end
     end
     validate_output!
   end
@@ -85,18 +95,52 @@ class IntcodeComputer
   def execute(inst)
     case inst.opcode
     when 1
+      # add (v1 + v2)
       val1 = read(1, inst.param1_mode)
       val2 = read(2, inst.param2_mode)
       write(3, inst.param3_mode, val1 + val2)
     when 2
+      # multiply (v1 * v2)
       val1 = read(1, inst.param1_mode)
       val2 = read(2, inst.param2_mode)
       write(3, inst.param3_mode, val1 * val2)
     when 3
+      # takes a single integer as input and saves it to the position given by its only parameter.
       val = @input.shift
       write(1, inst.param1_mode, val)
     when 4
+      # outputs the value of its only parameter.
       @output << read(1, inst.param1_mode)
+    when 5
+      # jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value
+      # from the second parameter. Otherwise, it does nothing.
+      val1 = read(1, inst.param1_mode)
+      val2 = read(2, inst.param2_mode)
+      if val1 != 0
+        @instruction_pointer = val2
+      end
+    when 6
+      # jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value
+      # from the second parameter. Otherwise, it does nothing.
+      val1 = read(1, inst.param1_mode)
+      val2 = read(2, inst.param2_mode)
+      if val1 == 0
+        @instruction_pointer = val2
+      end
+    when 7
+      # less than: if the first parameter is less than the second parameter, it stores 1 in the position
+      # given by the third parameter. Otherwise, it stores 0.
+      val1 = read(1, inst.param1_mode)
+      val2 = read(2, inst.param2_mode)
+      to_write = val1 < val2 ? 1 : 0
+      write(3, inst.param3_mode, to_write)
+    when 8
+      # Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the
+      # position given by the third parameter. Otherwise, it stores 0.
+      val1 = read(1, inst.param1_mode)
+      val2 = read(2, inst.param2_mode)
+      to_write = val1 == val2 ? 1 : 0
+      write(3, inst.param3_mode, to_write)
     when 99
       @halt = true
     else
@@ -138,23 +182,19 @@ class IntcodeComputer
   end
 end
 
-def part1(program)
-  IntcodeComputer.run(program, [1])
-end
-
-def part2(input)
-  nil
+def run_program(program, input)
+  IntcodeComputer.run(program, input)
 end
 
 def main
-  raw_input = File.read(INPUT_FILE)
-  input = process_input(raw_input)
+  program_str = File.read(INPUT_FILE)
+  program = parse_program(program_str)
 
   log.info "Part 1:"
-  log.info measure{part1(input).output.last}
+  log.info measure{run_program(program, [1]).output.last}
 
   log.info "Part 2:"
-  log.info measure{part2(input)}
+  log.info measure{run_program(program, [5]).output.last}
 end
 
 if __FILE__ == $0
--- day-05-sunny-with-a-chance-of-asteroids/test.rb
+++ day-05-sunny-with-a-chance-of-asteroids/test.rb
@@ -4,8 +4,8 @@ require_relative './main'
 
 log.level = Logger::DEBUG
 
-class TestOldTests < Minitest::Test
-  TESTS = [
+class TestDay2 < Minitest::Test
+  EXAMPLES = [
     ['1,9,10,3,2,3,11,0,99,30,40,50', '3500,9,10,70,2,3,11,0,99,30,40,50'],
     ['1,0,0,0,99', '2,0,0,0,99'],
     ['2,3,0,3,99', '2,3,0,6,99'],
@@ -14,37 +14,89 @@ class TestOldTests < Minitest::Test
     ['1101,100,-1,4,0', '1101,100,-1,4,99'],
   ]
 
+  INPUT_FILE = File.join(__dir__, '..', 'day-02-program-alarm', 'input.txt')
+
   def test_examples
-    TESTS.each do |raw_input, output|
-      input = process_input(raw_input)
-      res = part1(input).memory
-      assert_equal(output, res.join(','))
+    EXAMPLES.each do |program_str, expected_result|
+      program = parse_program(program_str)
+      computer = run_program(program, [])
+      result = computer.memory.join(',')
+      assert_equal(result, expected_result)
     end
   end
-end  
 
-class TestPart1 < Minitest::Test
-  TESTS = [
-  ]
+  def test_part_1
+    program_str = File.read(INPUT_FILE)
+    program = parse_program(program_str)
 
-  def test_examples
-    TESTS.each do |raw_input, output|
-      input = process_input(raw_input)
-      res = part1(input)
-      assert_equal(output, res.join(','))
+    # replace position 1 with the value 12
+    # replace position 2 with the value 2
+    modified_program = modify_program(program, 12, 2)
+    computer = run_program(modified_program, [])
+    result = computer.memory[0]
+    assert_equal(result, 3166704)
+  end
+
+  def test_part_2
+    program_str = File.read(INPUT_FILE)
+    program = parse_program(program_str)
+
+    combinations = (0..99).map {|x| (0..99).map {|y| [x, y]}}.flatten(1)
+    winner = combinations.find do |noun, verb|
+      modified_program = modify_program(program, noun, verb)
+      computer = run_program(modified_program, [])
+      result = computer.memory[0]
+      result == 19690720
     end
+    assert_equal(winner, [80, 18])
   end
 end
 
-class TestPart2 < Minitest::Test
-  TESTS = [
+class TestDay5 < Minitest::Test
+  EXAMPLES = [
+    ['3,9,8,9,10,9,4,9,99,-1,8', 8, 1], # 8 == 8
+    ['3,9,8,9,10,9,4,9,99,-1,8', 7, 0], # 7 == 8
+    ['3,9,7,9,10,9,4,9,99,-1,8', 7, 1], # 7 < 8
+    ['3,9,7,9,10,9,4,9,99,-1,8', 8, 0], # 8 < 8
+    ['3,9,7,9,10,9,4,9,99,-1,8', 9, 0], # 9 < 8
+    ['3,3,1108,-1,8,3,4,3,99', 8, 1], # 8 == 8
+    ['3,3,1108,-1,8,3,4,3,99', 7, 0], # 7 == 8
+    ['3,3,1107,-1,8,3,4,3,99', 7, 1], # 7 < 8
+    ['3,3,1107,-1,8,3,4,3,99', 8, 0], # 8 < 8
+    ['3,3,1107,-1,8,3,4,3,99', 9, 0], # 9 < 8
+    ['3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9', 0, 0],
+    ['3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9', 1, 1],
+    ['3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9', 2, 1],
+    ['3,3,1105,-1,9,1101,0,0,12,4,12,99,1', 0, 0],
+    ['3,3,1105,-1,9,1101,0,0,12,4,12,99,1', 1, 1],
+    ['3,3,1105,-1,9,1101,0,0,12,4,12,99,1', 2, 1],
+    ['3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99', 7, 999],
+    ['3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99', 8, 1000],
+    ['3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99', 9, 1001],
   ]
 
+  def test_part_1
+    program_str = File.read(INPUT_FILE)
+    program = parse_program(program_str)
+    computer = run_program(program, [1])
+    result = computer.output.last
+    assert_equal(result, 6745903)
+  end
+
   def test_examples
-    TESTS.each do |raw_input, output|
-      input = process_input(raw_input)
-      res = part2(input)
-      assert_equal(output, res.join(','))
+    EXAMPLES.each do |program_str, input, expected_result|
+      program = parse_program(program_str)
+      computer = run_program(program, [input])
+      result = computer.output.last
+      assert_equal(expected_result, result)
     end
+  end
+
+  def test_part_1
+    program_str = File.read(INPUT_FILE)
+    program = parse_program(program_str)
+    computer = run_program(program, [5])
+    result = computer.output.last
+    assert_equal(result, 9168267)
   end
 end
