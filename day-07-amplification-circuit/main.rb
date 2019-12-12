482cb3734262bf644be3de85cf37133e7d7a975f
Ken Pratt <ken@kenpratt.net>
2019-12-11 23:04:58 -0800

    day 7 part 1

--- day-07-amplification-circuit/main.rb
+++ day-07-amplification-circuit/main.rb
@@ -22,7 +22,26 @@ def run_program(program, input)
   IntcodeComputer.run(program, input)
 end
 
+def run_amplifiers(program, phase_settings, initial_input)
+  phase_settings.inject(initial_input) do |input, phase_setting|
+    computer = run_program(program, [phase_setting, input])
+    computer.output.last
+  end
+end
+
+def find_best_phase_setting_permutation(program, phase_settings, initial_input)
+  phase_settings.permutation.inject(nil) do |curr_best, setting_to_try|
+    thruster_signal = run_amplifiers(program, setting_to_try, initial_input)
+    if curr_best.nil? || thruster_signal > curr_best[0]
+      [thruster_signal, setting_to_try]
+    else
+      curr_best
+    end
+  end
+end
+
 def main
+  # (0..4).to_a.permutation
   program_str = File.read(INPUT_FILE)
   program = parse_program(program_str)
 
--- day-07-amplification-circuit/test.rb
+++ day-07-amplification-circuit/test.rb
@@ -53,6 +53,8 @@ class TestDay2 < Minitest::Test
 end
 
 class TestDay5 < Minitest::Test
+  INPUT_FILE = File.join(__dir__, '..', 'day-05-sunny-with-a-chance-of-asteroids', 'input.txt')
+
   EXAMPLES = [
     ['3,9,8,9,10,9,4,9,99,-1,8', 8, 1], # 8 == 8
     ['3,9,8,9,10,9,4,9,99,-1,8', 7, 0], # 7 == 8
@@ -98,5 +100,45 @@ class TestDay5 < Minitest::Test
     computer = run_program(program, [5])
     result = computer.output.last
     assert_equal(9168267, result)
+  end
+end
+
+class TestDay7 < Minitest::Test
+  PART1_EXAMPLES = [
+    ['3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0', '4,3,2,1,0', 43210],
+    ['3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0', '0,1,2,3,4', 54321],
+    ['3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0', '1,0,4,3,2', 65210],
+  ]
+
+  PART2_EXAMPLES = [
+    ['3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5', '9,8,7,6,5', 139629729],
+    ['3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10', '9,7,8,5,6', '18216']
+  ]
+
+  def test_thruster_signal
+    PART1_EXAMPLES.each do |program_str, phase_settings_str, expected_thruster_signal|
+      program = parse_program(program_str)
+      phase_settings = phase_settings_str.split(',').map(&:to_i)
+      thruster_signal = run_amplifiers(program, phase_settings, 0)
+      assert_equal(expected_thruster_signal, thruster_signal)
+    end
+  end
+
+  def test_best_phase_setting
+    PART1_EXAMPLES.each do |program_str, expected_phase_settings_str, expected_thruster_signal|
+      program = parse_program(program_str)
+      expected_phase_settings = expected_phase_settings_str.split(',').map(&:to_i)
+      thruster_signal, best_phase_settings = *find_best_phase_setting_permutation(program, (0..4).to_a, 0)
+      assert_equal(expected_thruster_signal, thruster_signal)
+      assert_equal(expected_phase_settings, best_phase_settings)
+    end
+  end
+
+  def test_part_1
+    program_str = File.read(INPUT_FILE)
+    program = parse_program(program_str)
+    thruster_signal, best_phase_settings = *find_best_phase_setting_permutation(program, (0..4).to_a, 0)
+    assert_equal(19650, thruster_signal)
+    assert_equal([2, 0, 1, 4, 3], best_phase_settings)
   end
 end
