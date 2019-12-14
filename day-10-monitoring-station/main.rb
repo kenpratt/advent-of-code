a588148ab80074af827eb1a93303d37de8cc9275
Ken Pratt <ken@kenpratt.net>
2019-12-14 14:56:16 -0800

    Day 10 part 2

--- day-10-monitoring-station/main.rb
+++ day-10-monitoring-station/main.rb
@@ -9,14 +9,24 @@ def process_input(input_str)
   lines = input_str.split("\n")
 
   asteroids = []
+  starting_position = nil
+
   lines.each_with_index do |line, y|
     line.each_char.each_with_index do |char, x|
-      if char == '#'
+      case char
+      when '#'
         asteroids << [x, y]
+      when 'X'
+        starting_position = [x, y]
+      when '.'
+        nil
+      else
+        raise "Unknown char: #{char}"
       end
     end
   end
-  asteroids
+
+  [asteroids, starting_position]
 end
 
 def counts_per_asteroid(asteroids)
@@ -27,14 +37,43 @@ def counts_per_asteroid(asteroids)
   end.to_h
 end
 
-def group_by_angle(asteroid, others)
-  others.group_by {|other| angle(asteroid, other)}
+STARTING_ANGLE = Math.atan2(-1, 0)
+
+def vaporize_asteroids(starting_position, asteroids)
+  # group by angle, sorted by nearest first per angle
+  by_angle = group_by_angle(starting_position, asteroids)
+  by_angle.each do |angle, asteroids_for_angle|
+    asteroids_for_angle.sort_by! {|asteroid| manhattan_distance(starting_position, asteroid)}
+  end
+
+  # figure out what order to scan angles in
+  angles = by_angle.keys.sort
+  starting_index = angles.find_index {|a| a >= STARTING_ANGLE}
+  ordered_angles = angles[starting_index..-1] + angles[0...starting_index]
+
+  # what's the most things to vaporize at an angle?
+  num_passes = by_angle.map {|angle, asteroids| asteroids.size}.max
+
+  # do the vapourization passes
+  num_passes.times.map do |nth_asteroid|
+    ordered_angles.map do |angle|
+      by_angle[angle][nth_asteroid]
+    end.compact
+  end.flatten(1)
+end
+
+def group_by_angle(starting_position, asteroids)
+  asteroids.group_by {|asteroid| angle(starting_position, asteroid)}
 end
 
 def angle(point1, point2)
   dx = point2[0] - point1[0]
   dy = point2[1] - point1[1]
   Math.atan2(dy, dx)  
+end
+
+def manhattan_distance(point1, point2)
+  (point2[0] - point1[0]).abs + (point2[1] - point1[1]).abs
 end
 
 def best_location(counts)
--- day-10-monitoring-station/test.rb
+++ day-10-monitoring-station/test.rb
@@ -15,7 +15,7 @@ class TestPart1 < Minitest::Test
 
   def test_examples
     EXAMPLES.each do |input_str, expected_counts, expected_best_location, expected_best_count|
-      asteroids = process_input(input_str)
+      asteroids, _ = process_input(input_str)
       counts = counts_per_asteroid(asteroids)
       if expected_counts
         assert_equal(expected_counts, asteroids.map {|a| counts[a]})
@@ -28,7 +28,7 @@ class TestPart1 < Minitest::Test
 
   def test_input
     input_str = File.read(INPUT_FILE)
-    asteroids = process_input(input_str)
+    asteroids, _ = process_input(input_str)
     counts = counts_per_asteroid(asteroids)
     location, count = best_location(counts)
     assert_equal([13, 17], location)
@@ -36,22 +36,37 @@ class TestPart1 < Minitest::Test
   end
 end
 
-# class TestPart2 < Minitest::Test
-#   EXAMPLES = [
-#   ]
+class TestPart2 < Minitest::Test
+  EXAMPLES = [
+    [
+      ".#....#####...#..\n##...##.#####..##\n##...#...#.#####.\n..#.....X...###..\n..#.#.....#....##",
+      {0=>[8, 1], 1=>[9, 0], 2=>[9, 1], 3=>[10, 0], 4=>[9, 2], 5=>[11, 1], 6=>[12, 1], 7=>[11, 2], 8=>[15, 1], 9=>[12, 2], 10=>[13, 2], 11=>[14, 2], 12=>[15, 2], 13=>[12, 3], 14=>[16, 4], 15=>[15, 4], 16=>[10, 4], 17=>[4, 4], 18=>[2, 4], 19=>[2, 3], 20=>[0, 2], 21=>[1, 2], 22=>[0, 1], 23=>[1, 1], 24=>[5, 2], 25=>[1, 0], 26=>[5, 1], 27=>[6, 1], 28=>[6, 0], 29=>[7, 0], 30=>[8, 0], 31=>[10, 1], 32=>[14, 0], 33=>[16, 1], 34=>[13, 3], 35=>[14, 3]},
+    ],
+    [
+      ".#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.####X#####...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##",
+      {0=>[11, 12], 1=>[12, 1], 2=>[12, 2], 9=>[12, 8], 19=>[16, 0], 49=>[16, 9], 99=>[10, 16], 198=>[9, 6], 199=>[8, 2], 200=>[10, 9], 298=>[11, 1]},
+    ],
+  ]
 
-#   def test_examples
-#     EXAMPLES.each do |input_str, expected_output|
-#       input = process_input(input_str)
-#       res = part2(input)
-#       assert_equal(expected_output, res)
-#     end
-#   end
+  def test_examples
+    EXAMPLES.each do |input_str, expected_vaporizations|
+      asteroids, starting_position = process_input(input_str)
+      vaporized_order = vaporize_asteroids(starting_position, asteroids)
+      assert_equal(asteroids.size, vaporized_order.size)
+      expected_vaporizations.each do |index, asteroid|
+        assert_equal(asteroid, vaporized_order[index], "Index: #{index}")
+      end
+    end
+  end
 
-#   def test_input
-#     input_str = File.read(INPUT_FILE)
-#     input = process_input(input_str)
-#     res = part2(input)
-#     assert_equal(nil, res)
-#   end
-# end
+  def test_input
+    input_str = File.read(INPUT_FILE)
+    asteroids, starting_position = process_input(input_str)
+    assert_nil(starting_position)
+    vaporized_order = vaporize_asteroids([13, 17], asteroids)
+    assert_equal(asteroids.size, vaporized_order.size)
+
+    asteroid = vaporized_order[199]
+    assert_equal([6, 12], asteroid)
+  end
+end
