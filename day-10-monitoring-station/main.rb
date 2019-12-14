a3b9499130267b86c5c89c38c0f76d58bbd6b1ea
Ken Pratt <ken@kenpratt.net>
2019-12-14 14:04:16 -0800

    Day 10 part 1.

--- day-10-monitoring-station/main.rb
+++ day-10-monitoring-station/main.rb
@@ -6,7 +6,39 @@ require_relative '../utils/profile'
 INPUT_FILE = File.join(__dir__, 'input.txt')
 
 def process_input(input_str)
-  nil
+  lines = input_str.split("\n")
+
+  asteroids = []
+  lines.each_with_index do |line, y|
+    line.each_char.each_with_index do |char, x|
+      if char == '#'
+        asteroids << [x, y]
+      end
+    end
+  end
+  asteroids
+end
+
+def counts_per_asteroid(asteroids)
+  asteroids.each_with_index.map do |asteroid, index|
+    other_asteroids = asteroids[0...index] + asteroids[(index + 1)..-1]
+    count = group_by_angle(asteroid, other_asteroids).size
+    [asteroid, count]
+  end.to_h
+end
+
+def group_by_angle(asteroid, others)
+  others.group_by {|other| angle(asteroid, other)}
+end
+
+def angle(point1, point2)
+  dx = point2[0] - point1[0]
+  dy = point2[1] - point1[1]
+  Math.atan2(dy, dx)  
+end
+
+def best_location(counts)
+  counts.max_by {|asteroid, count| count}
 end
 
 def part1(input)
--- day-10-monitoring-station/test.rb
+++ day-10-monitoring-station/test.rb
@@ -6,40 +6,52 @@ log.level = Logger::DEBUG
 
 class TestPart1 < Minitest::Test
   EXAMPLES = [
+    [".#..#\n.....\n#####\n....#\n...##", [7, 7, 6, 7, 7, 7, 5, 7, 8, 7], [3, 4], 8],
+    ["......#.#.\n#..#.#....\n..#######.\n.#.#.###..\n.#..#.....\n..#....#.#\n#..#....#.\n.##.#..###\n##...#..#.\n.#....####", nil, [5, 8], 33],
+    ["#.#...#.#.\n.###....#.\n.#....#...\n##.#.#.#.#\n....#.#.#.\n.##..###.#\n..#...##..\n..##....##\n......#...\n.####.###.", nil, [1, 2], 35],
+    [".#..#..###\n####.###.#\n....###.#.\n..###.##.#\n##.##.#.#.\n....###..#\n..#.#..#.#\n#..#.#.###\n.##...##.#\n.....#.#..", nil, [6, 3], 41],
+    [".#..##.###...#######\n##.############..##.\n.#.######.########.#\n.###.#######.####.#.\n#####.##.#.##.###.##\n..#####..#.#########\n####################\n#.####....###.#.#.##\n##.#################\n#####.##.###..####..\n..######..##.#######\n####.##.####...##..#\n.#####..#.######.###\n##...#.##########...\n#.##########.#######\n.####.#.###.###.#.##\n....##.##.###..#####\n.#.#.###########.###\n#.#.#.#####.####.###\n###.##.####.##.#..##", nil, [11, 13], 210],
   ]
 
   def test_examples
-    EXAMPLES.each do |input_str, expected_output|
-      input = process_input(input_str)
-      res = part1(input)
-      assert_equal(expected_output, res)
+    EXAMPLES.each do |input_str, expected_counts, expected_best_location, expected_best_count|
+      asteroids = process_input(input_str)
+      counts = counts_per_asteroid(asteroids)
+      if expected_counts
+        assert_equal(expected_counts, asteroids.map {|a| counts[a]})
+      end
+      location, count = best_location(counts)
+      assert_equal(expected_best_location, location)
+      assert_equal(expected_best_count, count)
     end
   end
 
   def test_input
     input_str = File.read(INPUT_FILE)
-    input = process_input(input_str)
-    res = part1(input)
-    assert_equal(nil, res)
+    asteroids = process_input(input_str)
+    counts = counts_per_asteroid(asteroids)
+    location, count = best_location(counts)
+    assert_equal([13, 17], location)
+    assert_equal(269, count)
   end
 end
 
-class TestPart2 < Minitest::Test
-  EXAMPLES = [
-  ]
+# class TestPart2 < Minitest::Test
+#   EXAMPLES = [
+#   ]
 
-  def test_examples
-    EXAMPLES.each do |input_str, expected_output|
-      input = process_input(input_str)
-      res = part2(input)
-      assert_equal(expected_output, res)
-    end
-  end
+#   def test_examples
+#     EXAMPLES.each do |input_str, expected_output|
+#       input = process_input(input_str)
+#       res = part2(input)
+#       assert_equal(expected_output, res)
+#     end
+#   end
 
-  def test_input
-    input_str = File.read(INPUT_FILE)
-    input = process_input(input_str)
-    res = part2(input)
-    assert_equal(nil, res)
-  end
-end
+#   def test_input
+#     input_str = File.read(INPUT_FILE)
+#     input = process_input(input_str)
+#     res = part2(input)
+#     assert_equal(nil, res)
+#   end
+# end
