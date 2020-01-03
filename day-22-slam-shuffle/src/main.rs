7715cc8a6b30d1e0b7bbc8dfe60657446d577d6b
Ken Pratt <ken@kenpratt.net>
2020-01-02 19:42:28 -0800

    wip

--- day-22-slam-shuffle/src/main.rs
+++ day-22-slam-shuffle/src/main.rs
@@ -1,7 +1,6 @@
 #[macro_use] extern crate lazy_static;
 extern crate regex;
 
-use std::collections::HashMap;
 use std::convert::TryInto;
 use std::fs;
 use mod_exp::mod_exp;
@@ -26,9 +25,10 @@ fn part2() {
     println!("collapsed: {:?}", collapsed_instructions);
 
     let repeated_instructions = collapsed_instructions.multiply(101741582076661);
-    println!("repeated_instructions: {:?}", foo);
+    println!("repeated_instructions: {:?}", repeated_instructions);
 
-    // TODO now I just need to figure out what index will end up at position 2020!
+    let from_index = repeated_instructions.calculate_from_index(2020);
+    assert_eq!(from_index, 1);
 }
 
 fn shuffle(deck_size: usize, input_str: &str) -> Deck {
@@ -41,17 +41,6 @@ fn shuffle(deck_size: usize, input_str: &str) -> Deck {
     let mut deck2 = Deck::new(deck_size);
     deck2.shuffle(&collapsed_instructions.materialize());
     assert_eq!(deck.cards, deck2.cards);
-
-    // let composite_instruction = CompositeShuffleInstruction::from_instructions(&instructions, deck_size);
-    // println!("composite: {:?}", composite_instruction);
-
-    // let mut composite_cards = vec![0; deck_size];
-    // for i in 0..deck_size {
-    //     let to_index = composite_instruction.run(i);
-    //     composite_cards[to_index] = i;
-    // }
-    //println!("composite res: {:?}", composite_cards);
-    //assert_eq!(deck.cards, composite_cards);
 
     deck
 }
@@ -86,26 +75,6 @@ impl ShuffleInstruction {
         }
     }
 }
-
-// fn pow_with_modulus(base: i128, exponent: i128, modulus: i128) -> i128 {
-//     if (base < 1) || (exponent < 0) || (modulus < 1) {
-//         panic!("invalid");
-//     }
-
-//     let mut result = 1;
-//     let mut curr_base = base;
-//     let mut curr_exponent = exponent;
-
-//     while curr_exponent > 0 {
-//         if (curr_exponent % 2) == 1 {
-//             result = (result * curr_base) % modulus;
-//         }
-//         curr_base = (curr_base * curr_base) % modulus;
-//         curr_exponent = curr_exponent / 2;
-//     }
-
-//     result
-// }
 
 #[derive(Debug)]
 struct ShuffleInstructions {
@@ -213,140 +182,27 @@ impl CollapsedShuffleInstructions {
         ];
         ShuffleInstructions::new(self.deck_size, instructions)
     }
-}
 
-#[derive(Debug)]
-struct CompositeShuffleInstruction {
-    deck_size: i128,
-    multiplier: i128,
-    offset: i128,
-    reversed: bool,
-}
+    fn calculate_from_index(&self, to_index: usize) -> usize {
+        let mut from_index = to_index as i128;
 
-impl CompositeShuffleInstruction {
-    pub fn new(deck_size: i128) -> CompositeShuffleInstruction {
-        return CompositeShuffleInstruction {
-            deck_size: deck_size,
-            multiplier: 1,
-            offset: 0,
-            reversed: false,
-        }
+        // apply cut (positive since going in reverse)
+        from_index = (from_index + self.cut_value) % self.deck_size;
+
+        // apply increment
+        let offset = from_index % self.increment_value;
+        println!("want to reverse increment... from_index {}, increment value {}, offset {}, deck_size {}", from_index, self.increment_value, offset, self.deck_size);
+
+        // TODO need to do inverse of:
+        // let to_index = (from_index * self.increment_value) % self.deck_size;
+
+        // let multiple = (1..self.increment_value).find(|m| offset == (self.increment_value - ((self.deck_size * m) % self.increment_value))).unwrap();
+        // let base = self.deck_size * multiple;
+        // println!("reverse increment... from_index {}, increment value {}, offset {}, multiple {}, deck_size {}, base {}", from_index, self.increment_value, offset, multiple, self.deck_size, base);
+        // from_index = (from_index + base) / self.increment_value;
+
+        from_index as usize
     }
-
-    fn from_instructions(instructions: &ShuffleInstructions, deck_size: usize) -> CompositeShuffleInstruction {
-        let mut res = CompositeShuffleInstruction::new(deck_size as i128);
-        res.apply_all(instructions);
-        res
-    }
-
-    fn apply_all(&mut self, instructions: &ShuffleInstructions) {
-        for instruction in &instructions.instructions {
-            self.apply(instruction);
-        }
-    }
-
-    fn apply(&mut self, instruction: &ShuffleInstruction) {
-        match instruction {
-            ShuffleInstruction::Cut(n) => {
-                let x = *n as i128;
-                if !self.reversed {
-                    self.offset = self.normalize(self.offset - x);
-                } else {
-                    self.offset = self.normalize(self.offset + x);
-                }
-            },
-            ShuffleInstruction::DealWithIncrement(n) => {
-                let x = *n as i128;
-                self.multiplier = self.normalize(self.multiplier * x);
-                self.offset = self.normalize(self.offset * x); // offset is scaled
-            },
-            ShuffleInstruction::DealNewStack => {
-                self.reversed = !self.reversed;
-                //self.offset *= -1; // offset is relative to reversal
-            },
-        }
-        println!("{:?}", self);
-    }
-
-    fn normalize(&self, x: i128) -> i128 {
-        if x < 0 {
-            //-(-x % self.deck_size)
-            println!("normalize neg {} {}", x, -(-x % self.deck_size) + self.deck_size - 1);
-            -(-x % self.deck_size) + self.deck_size
-             //TODO + self.deck_size - 1 (?)
-        } else {
-            x % self.deck_size
-        }
-    }
-
-    fn run(&self, from_index: usize) -> usize {
-        // if self.reversed {
-        //     panic!("Don't know how to run a reversed composite shuffle");
-        // }
-
-        let mut to_index = from_index as i128;
-
-        println!("run from_index = {}, X to_index = {}", from_index, to_index);
-
-        // if still reversed, index into right side
-        // if self.reversed {
-        //     to_index = -(to_index + 1);
-        // }
-
-        println!("run from_index = {}, A to_index = {}", from_index, to_index);
-
-        // scale up index by current multiplier
-        to_index *= self.multiplier;
-
-        println!("run from_index = {}, B to_index = {}", from_index, to_index);
-
-        // add offset
-        to_index += self.offset;
-        // if !self.reversed {
-        //     to_index += self.offset;
-        // } else {
-        //     to_index -= self.offset;
-        // }
-
-        println!("run from_index = {}, C to_index = {}", from_index, to_index);
-
-        // if negative (due to offsets), convert to a positive index
-        // by adding enough multiples of deck size
-        if to_index < 0 {
-            to_index += ((-to_index / self.deck_size) + 1) * self.deck_size;
-        }
-
-        println!("run from_index = {}, D to_index = {}", from_index, to_index);
-
-        // normalize by deck size
-        to_index = to_index % self.deck_size;
-
-        println!("run from_index = {}, E to_index = {}", from_index, to_index);
-
-        if self.reversed {
-           to_index = self.deck_size - to_index - 1;
-        }
-
-        // safety checks to ensure in bounds
-        if to_index < 0 {
-            panic!("index below 0. from_index: {}, to_index: {}", from_index, to_index);
-        } else if to_index >= self.deck_size {
-            panic!("index above deck size. from_index: {}, to_index: {}", from_index, to_index);
-        } 
-
-        // if reversed, index into right side of array
-        // if self.reversed {
-        //     to_index = self.deck_size - to_index - 1;
-        // }
-
-        println!("run from_index = {}, final to_index = {}", from_index, to_index);
-
-        to_index as usize
-    }
-
-    // fn run_reverse(&self, to_index: usize) -> usize {
-    //     0
-    // }
 }
 
 #[derive(Debug)]
@@ -400,114 +256,6 @@ impl Deck {
     fn deal_new_stack(&mut self) {
         //println!("deal_new_stack");
         self.cards.reverse();
-    }
-}
-
-#[derive(Debug)]
-pub struct Solver {
-    deck_size: usize,
-    instructions: Vec<ShuffleInstruction>,
-    increment_offsets: HashMap<usize, Vec<usize>>,
-}
-
-impl Solver {
-    fn run(deck_size: usize, runs: usize, to_index: usize, instructions: Vec<ShuffleInstruction>) -> usize {
-        let mut solver = Solver {
-            deck_size: deck_size,
-            instructions: instructions,
-            increment_offsets: HashMap::new(),
-        };
-        solver.build_increment_offset_cache();
-        println!("Running solver: deck_size = {}, runs = {}, to_index = {}", deck_size, runs, to_index);
-        let mut result_index = to_index;
-        let mut seen: HashMap<usize, usize> = HashMap::new();
-        seen.insert(result_index, 0);
-        for i in 1..(runs+1) {
-            if i % 100000 == 0 {
-              println!("{}", i);
-            }
-            let foo = solver.run_instructions(result_index);
-            if seen.contains_key(&foo) {
-                println!("found duplicate! {}, {}", seen.get(&foo).unwrap(), i)
-            } else {
-                seen.insert(foo, i);
-            }
-            //println!("{}, {}, {}", result_index, foo, (result_index as i128) - (foo as i128));
-            result_index = foo;
-        }
-        result_index
-    }
-
-    fn build_increment_offset_cache(&mut self) {
-        for instruction in &mut self.instructions {
-            match instruction {
-                ShuffleInstruction::DealWithIncrement(raw_increment) => {
-                    let increment = *raw_increment as usize;
-                    if !self.increment_offsets.contains_key(&increment) {
-                        let offsets = Solver::build_offsets_for_increment(increment, self.deck_size);
-                        self.increment_offsets.insert(increment, offsets);
-                    }
-                },
-                _ => {},
-            }
-        }
-        //println!("offset cache: {:?}", self.increment_offsets);
-    }
-
-    fn build_offsets_for_increment(increment: usize, deck_size: usize) -> Vec<usize> {
-        let mut offsets = vec![0; increment];
-        for x in 1..increment {
-            // deck size 10, increment 3, x = 1
-            // x = 1: base = 10, offset = (3 - (10 % 3)) = (3 - 1) = 2
-            // x = 2: base = 20, offset = (3 - (20 % 3)) = (3 - 2) = 1
-            //
-            // deck size 10, increment 7, x = 1
-            // x = 1: base = 10, offset = (7 - (10 % 7)) = (7 - 3) = 4
-            // x = 2: base = 20, offset = (7 - (20 % 7)) = (7 - 6) = 1
-            // x = 3: base = 30, offset = (7 - (30 % 7)) = (7 - 2) = 5
-            // x = 4: base = 40, offset = (7 - (40 % 7)) = (7 - 5) = 2
-            // x = 5: base = 50, offset = (7 - (50 % 7)) = (7 - 1) = 6
-            // x = 6: base = 60, offset = (7 - (60 % 7)) = (7 - 4) = 3
-            let base = deck_size * x;
-            let offset = increment - (base % increment);
-            offsets[offset] = base;
-        }
-        offsets
-    }
-
-    fn get_increment_offset(&self, increment: usize, index: usize) -> usize {
-        let offset = index % increment;
-        self.increment_offsets[&increment][offset]
-    }
-
-    fn run_instructions(&self, to_index: usize) -> usize {
-        let mut result = to_index;
-        for instruction in self.instructions.iter().rev() {
-            result = self.run_instruction(result, instruction);
-        }
-        result
-    }
-
-    fn run_instruction(&self, to_index: usize, instruction: &ShuffleInstruction) -> usize {
-        match instruction {
-            ShuffleInstruction::Cut(n) => {
-                let from_index = ((to_index as i128) + (*n as i128)) as usize;
-                //println!("Cut: to_index = {}, n = {}, from_index = {}", to_index, n, from_index);
-                from_index
-            },
-            ShuffleInstruction::DealWithIncrement(raw_increment) => {
-                let increment = *raw_increment as usize;
-                let base = self.get_increment_offset(increment, to_index);
-                let from_index = (to_index + base) / increment;
-                //println!("DealWithIncrement: to_index = {}, increment = {}, base = {}, from_index = {}", to_index, increment, base, from_index);
-                from_index
-            },
-            ShuffleInstruction::DealNewStack => {
-                let from_index = self.deck_size - to_index - 1;
-                //println!("DealNewStack: to_index = {}, from_index = {}", to_index, from_index);
-                from_index
-            },
-        }
     }
 }
 
