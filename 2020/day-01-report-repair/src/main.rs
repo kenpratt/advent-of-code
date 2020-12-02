c89879830f5f8116a0b00fc69df62b4f79f07fb2
Ken Pratt <ken@kenpratt.net>
2020-12-02 09:35:40 -0800

    Cleanup

--- 2020/day-01-report-repair/src/main.rs
+++ 2020/day-01-report-repair/src/main.rs
@@ -1,14 +1,12 @@
 use std::fs;
 
 fn main() {
-    part1();
-    part2();
+    println!("part 1 result: {:?}", part1(read_input_file()));
+    println!("part 2 result: {:?}", part2(read_input_file()));
 }
 
-fn part1() {
-    let input = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    let result = run_part1(input);
-    println!("part 1 result: {:?}", result);
+fn read_input_file() -> String {
+    return fs::read_to_string("input.txt").expect("Something went wrong reading the file");
 }
 
 fn parse_input(input: String) -> Vec<u64> {
@@ -19,7 +17,7 @@ fn parse_line(line: &str) -> u64 {
     return line.parse::<u64>().unwrap();
 }
 
-fn run_part1(input: String) -> u64 {
+fn part1(input: String) -> u64 {
     let entries = parse_input(input);
     for x in &entries {
         for y in &entries {
@@ -31,13 +29,7 @@ fn run_part1(input: String) -> u64 {
     panic!("Nothing added up to 2020");
 }
 
-fn part2() {
-    let input = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    let result = run_part2(input);
-    println!("part 2 result: {:?}", result);    
-}
-
-fn run_part2(input: String) -> u64 {
+fn part2(input: String) -> u64 {
     let entries = parse_input(input);
     for x in &entries {
         for y in &entries {
@@ -53,22 +45,37 @@ fn run_part2(input: String) -> u64 {
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[test]
     fn test_part1_example1() {
-        let result = run_part1(
+        let result = part1(
             "1721\n979\n366\n299\n675\n1456".to_string()
         );
         assert_eq!(result, 514579);
     }
 
     #[test]
+    fn test_part1_solution() {
+        let result = part1(
+            read_input_file()
+        );
+        assert_eq!(result, 751776);
+    }
+
+    #[test]
     fn test_part2_example1() {
-        let result = run_part2(
+        let result = part2(
             "1721\n979\n366\n299\n675\n1456".to_string()
         );
         assert_eq!(result, 241861950);
+    }
+
+    #[test]
+    fn test_part2_solution() {
+        let result = part2(
+            read_input_file()
+        );
+        assert_eq!(result, 42275090);
     }    
 }
