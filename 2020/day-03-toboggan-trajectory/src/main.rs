de11af88f8b318a6b194e500a7d552263236be6e
Ken Pratt <ken@kenpratt.net>
2020-12-03 16:40:01 -0800

    2020 day 3 part 1.

---
+++ 2020/day-03-toboggan-trajectory/src/main.rs
@@ -0,0 +1,108 @@
+use std::fs;
+
+fn main() {
+    println!("part 1 result: {:?}", part1(read_input_file()));
+    //println!("part 2 result: {:?}", part2(read_input_file()));
+}
+
+fn read_input_file() -> String {
+    return fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+}
+
+#[derive(Debug)]
+struct Map {
+    width: usize,
+    height: usize,
+    cells: Vec<Vec<bool>>,
+}
+
+impl Map {
+    fn parse(input: String) -> Map {
+        let cells: Vec<Vec<bool>> = input.lines().map(|line| Map::parse_line(line)).collect();
+        return Map {
+            width: cells[0].len(),
+            height: cells.len(),
+            cells: cells,
+        }
+    }
+
+    fn parse_line(line: &str) -> Vec<bool> {
+        return line.chars().map(|c| Map::parse_char(&c)).collect();
+    }
+
+    fn parse_char(c: &char) -> bool {
+        return match c {
+            '.' => false,
+            '#' => true,
+            _ => panic!("Bad input on map")
+        };
+    }
+
+    fn count_trees(&self, right: usize, down: usize) -> usize {
+        let mut x = 0;
+        let mut y = 0;
+        let mut trees = 0;
+
+        while y < self.height {
+            if self.value(x, y) {
+                trees += 1
+            }
+
+            x += right;
+            y += down;
+        }
+
+        return trees;
+    }
+
+    fn value(&self, x: usize, y: usize) -> bool {
+        return self.cells[y][x % self.width];
+    }
+}
+
+fn part1(input: String) -> usize {
+    let map = Map::parse(input);
+    return map.count_trees(3, 1);
+}
+
+fn part2(input: String) -> usize {
+    let entries = Map::parse(input);
+    panic!("Error parsing input");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1_example1() {
+        let result = part1(
+            "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#".to_string()
+        );
+        assert_eq!(result, 7);
+    }
+
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(
+            read_input_file()
+        );
+        assert_eq!(result, 294);
+    }
+
+    // #[test]
+    // fn test_part2_example1() {
+    //     let result = part2(
+    //         "".to_string()
+    //     );
+    //     assert_eq!(result, 0);
+    // }
+
+    // #[test]
+    // fn test_part2_solution() {
+    //     let result = part2(
+    //         read_input_file()
+    //     );
+    //     assert_eq!(result, 0);
+    // }
+}
