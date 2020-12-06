7a5c4d784be04b14be6461df04c0bbf889f1ba67
Ken Pratt <ken@kenpratt.net>
2020-12-06 12:21:36 -0800

    2020 day 6 part 2.

--- 2020/day-06-custom-customs/src/main.rs
+++ 2020/day-06-custom-customs/src/main.rs
@@ -5,7 +5,7 @@ use indoc::indoc;
 
 fn main() {
     println!("part 1 result: {:?}", part1(read_input_file()));
-    //println!("part 2 result: {:?}", part2(read_input_file()));
+    println!("part 2 result: {:?}", part2(read_input_file()));
 }
 
 fn read_input_file() -> String {
@@ -32,6 +32,12 @@ impl Group {
         let chars_union: HashSet<char> = self.people.join("").chars().collect();
         return chars_union.len();
     }
+    
+    fn intersection_size(&self) -> usize {        
+        let base: HashSet<char> = self.people[0].chars().collect();
+        let intersection = self.people[1..].iter().fold(base, |acc, x| acc.intersection(&x.chars().collect()).cloned().collect());
+        return intersection.len();
+    }
 }
 
 fn part1(input: String) -> usize {
@@ -39,10 +45,10 @@ fn part1(input: String) -> usize {
     return groups.iter().map(|g| g.union_size()).fold(0, |acc, x| acc + x);
 }
 
-// fn part2(input: String) -> usize {
-//     let data = Group::parse(input);
-//     return data.execute();
-// }
+fn part2(input: String) -> usize {
+    let groups = parse_input(&input);
+    return groups.iter().map(|g| g.intersection_size()).fold(0, |acc, x| acc + x);
+}
 
 #[cfg(test)]
 mod tests {
@@ -79,19 +85,34 @@ mod tests {
         assert_eq!(result, 6748);
     }
 
-    // #[test]
-    // fn test_part2_example1() {
-    //     let result = part2(
-    //         "".to_string()
-    //     );
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_example1() {
+        let input = indoc! {"
+            abc
 
-    // #[test]
-    // fn test_part2_solution() {
-    //     let result = part2(
-    //         read_input_file()
-    //     );
-    //     assert_eq!(result, 0);
-    // }
+            a
+            b
+            c
+            
+            ab
+            ac
+            
+            a
+            a
+            a
+            a
+            
+            b
+        "};
+        let result = part2(input.to_string());
+        assert_eq!(result, 6);
+    }    
+
+    #[test]
+    fn test_part2_solution() {
+        let result = part2(
+            read_input_file()
+        );
+        assert_eq!(result, 3445);
+    }
 }
