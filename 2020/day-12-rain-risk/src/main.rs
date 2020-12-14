fe8fa5bd722fdecc1af9a6d412ec2dfd59552968
Ken Pratt <ken@kenpratt.net>
2020-12-14 15:22:14 -0800

    2020 day 12 part 2.

--- 2020/day-12-rain-risk/src/main.rs
+++ 2020/day-12-rain-risk/src/main.rs
@@ -4,7 +4,7 @@ use regex::Regex;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
-    // println!("part 2 result: {:?}", part2(&read_input_file()));
+    println!("part 2 result: {:?}", part2(&read_input_file()));
 }
 
 fn read_input_file() -> String {
@@ -14,7 +14,6 @@ fn read_input_file() -> String {
 #[derive(Debug)]
 struct Ship {
     instructions: Vec<Instruction>,
-    state: SimpleNavigation,
 }
 
 impl Ship {
@@ -22,47 +21,64 @@ impl Ship {
         let instructions = input.lines().map(|line| Instruction::parse(line)).collect();
         return Ship {
             instructions: instructions,
-            state: SimpleNavigation {
-                x: 0,
-                y: 0,
-                angle: 0,
-            },
         }
     }
 
-    fn execute(&mut self) {
+    fn execute(&self, use_waypoint: bool) -> usize {
+        let mut nav = Ship::construct_navigation(use_waypoint);
         for instruction in &self.instructions {
-            self.state.apply_instruction(&instruction);
-            println!("i: {:?}, after: {:?}", instruction, self.state);
+            nav.apply_instruction(&instruction);
+            println!("instruction: {:?}, after: {:?}", instruction, nav);
         }
+        return nav.manhattan_distance();
     }
 
-    fn manhattan_distance(&self) -> usize {
-        return self.state.manhattan_distance();
-    }
+    fn construct_navigation(use_waypoint: bool) -> Box<dyn Navigation> {
+        return if use_waypoint {
+            Box::new(WaypointNavigation::new(10, 1))
+        } else {
+            Box::new(SimpleNavigation::new())
+        };
+    }    
+}
+
+trait Navigation: std::fmt::Debug {
+    fn apply_instruction(&mut self, instruction: &Instruction);
+    fn manhattan_distance(&self) -> usize;
 }
 
 #[derive(Debug)]
 struct SimpleNavigation {
-    x: isize,
-    y: isize,
+    position: Point,
     angle: isize,
 }
 
 impl SimpleNavigation {
+    fn new() -> SimpleNavigation {
+        return SimpleNavigation {
+            position: Point {
+                x: 0,
+                y: 0,
+            },
+            angle: 0,
+        };
+    }
+}
+
+impl Navigation for SimpleNavigation {
     fn apply_instruction(&mut self, instruction: &Instruction) {
         match instruction {
             Instruction::North(value) => {
-                self.y += *value as isize;
+                self.position.y += *value as isize;
             },
             Instruction::South(value) => {
-                self.y -= *value as isize;
+                self.position.y -= *value as isize;
             },
             Instruction::East(value) => {
-                self.x += *value as isize;
+                self.position.x += *value as isize;
             },
             Instruction::West(value) => {
-                self.x -= *value as isize;
+                self.position.x -= *value as isize;
             },
             Instruction::Left(value) => {
                 self.angle += *value as isize;
@@ -74,10 +90,87 @@ impl SimpleNavigation {
                 let radians = (self.angle as f64).to_radians();
                 let x = radians.cos() * (*value as f64);
                 let y = radians.sin() * (*value as f64);
-                self.x += x as isize;
-                self.y += y as isize;
+                self.position.x += x.round() as isize;
+                self.position.y += y.round() as isize;
             },
         }
+    }
+
+    fn manhattan_distance(&self) -> usize {
+        return self.position.manhattan_distance();
+    }
+}
+
+#[derive(Debug)]
+struct WaypointNavigation {
+    position: Point,
+    waypoint: Point,
+}
+
+impl WaypointNavigation {
+    fn new(waypoint_x: isize, waypoint_y: isize) -> WaypointNavigation {
+        return WaypointNavigation {
+            position: Point {
+                x: 0,
+                y: 0,
+            },
+            waypoint: Point {
+                x: waypoint_x,
+                y: waypoint_y,
+            },
+        };
+    }
+}
+
+impl Navigation for WaypointNavigation {
+    fn apply_instruction(&mut self, instruction: &Instruction) {
+        match instruction {
+            Instruction::North(value) => {
+                self.waypoint.y += *value as isize;
+            },
+            Instruction::South(value) => {
+                self.waypoint.y -= *value as isize;
+            },
+            Instruction::East(value) => {
+                self.waypoint.x += *value as isize;
+            },
+            Instruction::West(value) => {
+                self.waypoint.x -= *value as isize;
+            },
+            Instruction::Left(value) => {
+                self.waypoint.rotate(*value as isize);
+            },
+            Instruction::Right(value) => {
+                self.waypoint.rotate(-(*value as isize));
+            },
+            Instruction::Forward(value) => {
+                self.position.x += self.waypoint.x * (*value as isize);
+                self.position.y += self.waypoint.y * (*value as isize);
+            },
+        }
+    }
+
+    fn manhattan_distance(&self) -> usize {
+        return self.position.manhattan_distance();
+    }
+}
+
+#[derive(Debug)]
+struct Point {
+    x: isize,
+    y: isize,
+}
+
+impl Point {
+    fn rotate(&mut self, degrees: isize) {
+        let x = self.x as f64;
+        let y = self.y as f64;
+        let radians = (degrees as f64).to_radians();
+        let cos = radians.cos();
+        let sin = radians.sin();
+
+        self.x = (x * cos - y * sin).round() as isize;
+        self.y = (x * sin + y * cos).round() as isize;
     }
 
     fn manhattan_distance(&self) -> usize {
@@ -120,15 +213,14 @@ impl Instruction {
 }
 
 fn part1(input: &str) -> usize {
-    let mut program = Ship::parse(input);
-    program.execute();
-    return program.manhattan_distance();
+    let ship = Ship::parse(input);
+    return ship.execute(false);
 }
 
-// fn part2(input: &str) -> usize {
-//     let data = Ship::parse(input);
-//     return data.execute();
-// }
+fn part2(input: &str) -> usize {
+    let ship = Ship::parse(input);
+    return ship.execute(true);
+}
 
 #[cfg(test)]
 mod tests {
@@ -175,15 +267,31 @@ mod tests {
         assert_eq!(result, 1710);
     }
 
-    // #[test]
-    // fn test_part2_example1() {
-    //     let result = part2(EXAMPLE1);
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_example1() {
+        let result = part2(EXAMPLE1);
+        assert_eq!(result, 286);
 
-    // #[test]
-    // fn test_part2_solution() {
-    //     let result = part2(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+        // p = 170,38
+        // w = -4,10
+        // F11: p + 11*w
+        // 170,38 + 11*(-4, 10)
+        // (170-44),(38+110)
+        // 126,148 => 274
+        assert_eq!(part2(EXAMPLE1_MOD1), 274);
+
+        // p = 170,38
+        // w = -10,-4
+        // F11: p + 11*w
+        // 170,38 + 11*(-110, -44)
+        // (170-110),(38-44)
+        // 60,-6 => 66
+        assert_eq!(part2(EXAMPLE1_MOD2), 66);
+    }
+
+    #[test]
+    fn test_part2_solution() {
+        let result = part2(&read_input_file());
+        assert_eq!(result, 62045);
+    }
 }
