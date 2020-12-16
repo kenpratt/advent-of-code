db485ecddf17f6b5685bc7158e41a3bd3e675a61
Ken Pratt <ken@kenpratt.net>
2020-12-15 22:12:07 -0800

    2020 day 13 part 1.

--- 2020/day-13-shuttle-search/src/main.rs
+++ 2020/day-13-shuttle-search/src/main.rs
@@ -14,38 +14,36 @@ fn read_input_file() -> String {
 
 #[derive(Debug)]
 struct Data {
-    parts: Vec<Part>,
+    earliest_time: usize,
+    shuttle_ids: Vec<usize>,
 }
 
 impl Data {
     fn parse(input: &str) -> Data {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
+        let lines: Vec<&str> = input.lines().collect();
+        assert_eq!(lines.len(), 2);
+        let earliest_time = lines[0].parse::<usize>().unwrap();
+        let shuttle_ids = lines[1].split(',').filter(|s| s != &"x").map(|s| s.parse::<usize>().unwrap()).collect();
+
         return Data {
-            parts: parts,
+            earliest_time: earliest_time,
+            shuttle_ids: shuttle_ids,
         }
     }
 
-    fn execute(&self) -> usize {
-        return 0;
+    fn earliest_available_shuttle(&self) -> (usize, usize) {
+        return self.shuttle_ids.iter().map(|id| (*id, self.calculate_wait(id))).min_by_key(|p| p.1).unwrap();
     }
-}
 
-#[derive(Debug)]
-struct Part {
-    foo: usize,
-}
-
-impl Part {
-        fn parse(input: &str) -> Part {
-        return Part {
-            foo: input.len(),
-        }
+    fn calculate_wait(&self, shuttle_id: &usize) -> usize {
+        return shuttle_id - self.earliest_time % shuttle_id;
     }
 }
 
 fn part1(input: &str) -> usize {
     let data = Data::parse(input);
-    return data.execute();
+    let (shuttle_id, wait) = data.earliest_available_shuttle();
+    return shuttle_id * wait;
 }
 
 // fn part2(input: &str) -> usize {
@@ -60,20 +58,21 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
-    "};    
+        939
+        7,13,x,x,59,x,31,19
+    "};
 
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 295);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 205);
+    }
 
     // #[test]
     // fn test_part2_example1() {
