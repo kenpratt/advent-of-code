e644e3d60b5736b1c10b582ca7efa3d93f82742c
Ken Pratt <ken@kenpratt.net>
2020-12-29 15:33:53 -0800

    2020 day 22 part 2 solution.

--- 2020/day-22-crab-combat/src/main.rs
+++ 2020/day-22-crab-combat/src/main.rs
@@ -1,12 +1,10 @@
+use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::fs;
 
-// use lazy_static::lazy_static;
-// use regex::Regex;
-
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
-    // println!("part 2 result: {:?}", part2(&read_input_file()));
+    println!("part 2 result: {:?}", part2(&read_input_file()));
 }
 
 fn read_input_file() -> String {
@@ -16,37 +14,96 @@ fn read_input_file() -> String {
 #[derive(Debug)]
 struct Combat {
     decks: Vec<Deck>,
+    previously_seen_decks: HashSet<Vec<Deck>>,
+    game: usize,
+    round: usize,
 }
 
 impl Combat {
     fn parse(input: &str) -> Combat {
         let decks = input.split("\n\n").map(|chunk| Deck::parse(chunk)).collect();
-        return Combat {
+        Combat {
             decks: decks,
+            previously_seen_decks: HashSet::new(),
+            game: 1,
+            round: 0,
         }
     }
 
-    fn run(&mut self) {
-        while !self.is_over() {
-            self.tick()
+    fn play(&mut self, recursive: bool) -> usize {
+        println!("\nGame {}:\n", self.game);
+
+        while self.winner(recursive).is_none() {
+            self.tick(recursive)
         }
+
+        let winner = self.winner(recursive).unwrap();
+        println!("\nPlayer {} wins game {}\n", winner + 1, self.game);
+        winner
     }
 
-    fn tick(&mut self) {
+    fn tick(&mut self, recursive: bool) {
+        self.round += 1;
+        println!("Round {} (Game {}):", self.round, self.game);
+        for deck in &self.decks {
+            println!("  {} {:?}", deck.player, deck.cards);
+        }
+        
+        // add deck states to list for recursive victory
+        self.previously_seen_decks.insert(self.decks.clone());
+
         let cards: Vec<usize> = self.decks.iter_mut().map(|deck| deck.draw().unwrap()).collect();
-        let (winning_card_index, winning_card) = cards.iter().enumerate().max_by_key(|(_, card)| *card).unwrap();
+        println!("cards: {:?}", cards);
+
+        // recursive game initiation
+        let winner_index: usize = if recursive && cards.iter().enumerate().all(|(index, card)| self.decks[index].len() >= *card) {
+            // recursive game
+            let decks_for_sub_game: Vec<Deck> = cards.iter().enumerate().map(|(index, card)| {
+                self.decks[index].sub_deck(*card)
+            }).collect();
+            let mut sub_game = self.sub_game(decks_for_sub_game);
+            sub_game.play(recursive)
+        } else {
+            // normal rules, highest card wins
+            let (winning_card_index, _) = cards.iter().enumerate().max_by_key(|(_, card)| *card).unwrap();
+            winning_card_index
+        };
+
+        println!("  Player {} wins Round {} of Game {}", winner_index+1, self.round, self.game);
 
         // append winning card
-        self.decks[winning_card_index].append(*winning_card);
+        let winning_card = cards[winner_index];
+        self.decks[winner_index].append(winning_card);
 
         // append losing card
-        for losing_card in cards.iter().filter(|c| *c != winning_card) {
-            self.decks[winning_card_index].append(*losing_card);
+        for losing_card in cards.iter().filter(|c| **c != winning_card) {
+            self.decks[winner_index].append(*losing_card);
+        }        
+    }
+
+    fn sub_game(&self, sub_game_decks: Vec<Deck>) -> Combat {
+        Combat {
+            decks: sub_game_decks,
+            previously_seen_decks: HashSet::new(),
+            game: self.game + 1,
+            round: 0,
         }
     }
 
-    fn is_over(&self) -> bool {
-        self.decks.iter().any(|deck| deck.is_empty())
+    fn winner(&self, recursive: bool) -> Option<usize> {
+        // if there is exactly one non-empty deck left, that's the winner
+        let non_empty_deck_indices: Vec<usize> = self.decks.iter().enumerate().filter(|(_, deck)| !deck.is_empty()).map(|(index, _)| index).collect();
+        if non_empty_deck_indices.len() == 1 {
+            return Some(non_empty_deck_indices[0]);
+        }
+
+        // recursive victory condition
+        if recursive && self.previously_seen_decks.contains(&self.decks) {
+            // player 1 victory
+            return Some(0);
+        }
+
+        None
     }
 
     fn score(&self) -> usize {
@@ -54,7 +111,7 @@ impl Combat {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 struct Deck {
     player: String,
     cards: VecDeque<usize>,
@@ -66,7 +123,7 @@ impl Deck {
         let player: String = lines.next().unwrap().to_string();
         let cards: VecDeque<usize> = lines.map(|s| s.parse::<usize>().unwrap()).collect();
 
-        return Deck {
+        Deck {
             player: player,
             cards: cards,
         }
@@ -74,6 +131,17 @@ impl Deck {
 
     fn is_empty(&self) -> bool {
         self.cards.is_empty()
+    }
+
+    fn len(&self) -> usize {
+        self.cards.len()
+    }
+
+    fn sub_deck(&self, num: usize) -> Deck {
+        Deck {
+            player: self.player.clone(),
+            cards: self.cards.iter().take(num).cloned().collect(),
+        }
     }
 
     fn draw(&mut self) -> Option<usize> {
@@ -96,14 +164,15 @@ impl Deck {
 
 fn part1(input: &str) -> usize {
     let mut game = Combat::parse(input);
-    game.run();
+    game.play(false);
     game.score()
 }
 
-// fn part2(input: &str) -> usize {
-//     let game = Combat::parse(input);
-//     return game.execute();
-// }
+fn part2(input: &str) -> usize {
+    let mut game = Combat::parse(input);
+    game.play(true);
+    game.score()
+}
 
 #[cfg(test)]
 mod tests {
@@ -139,15 +208,15 @@ mod tests {
         assert_eq!(result, 31957);
     }
 
-    // #[test]
-    // fn test_part2_example1() {
-    //     let result = part2(EXAMPLE1);
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_example1() {
+        let result = part2(EXAMPLE1);
+        assert_eq!(result, 291);
+    }
 
-    // #[test]
-    // fn test_part2_solution() {
-    //     let result = part2(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_solution() {
+        let result = part2(&read_input_file());
+        assert_eq!(result, 33212);
+    }
 }
