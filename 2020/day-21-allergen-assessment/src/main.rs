64fc89d82549d217a45474ed2563db69f9ca57ff
Ken Pratt <ken@kenpratt.net>
2020-12-29 12:21:07 -0800

    2020 day 21 part 1 parse input.

--- 2020/day-21-allergen-assessment/src/main.rs
+++ 2020/day-21-allergen-assessment/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
-// use lazy_static::lazy_static;
-// use regex::Regex;
+use lazy_static::lazy_static;
+use regex::Regex;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
@@ -14,38 +14,52 @@ fn read_input_file() -> String {
 
 #[derive(Debug)]
 struct Data {
-    parts: Vec<Part>,
+    foods: Vec<Food>,
 }
 
 impl Data {
     fn parse(input: &str) -> Data {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
+        let foods = input.lines().map(|line| Food::parse(line)).collect();
         return Data {
-            parts: parts,
+            foods: foods,
         }
     }
 
-    fn execute(&self) -> usize {
+    fn allergen_free_ingredients(&self) -> usize {
+        println!("{:?}", self);
         return 0;
     }
 }
 
 #[derive(Debug)]
-struct Part {
-    foo: usize,
+struct Food {
+    ingredients: Vec<String>,
+    allergens: Vec<String>,
 }
 
-impl Part {
-    fn parse(input: &str) -> Part {
-        return Part {
-            foo: input.len(),
+impl Food {
+    fn parse(input: &str) -> Food {
+        lazy_static! {
+            static ref ID_RE: Regex = Regex::new(r"\A([a-z\s]+) \(contains ([a-z,\s]+)\)\z").unwrap();
+        }
+        let captures = ID_RE.captures(input).unwrap();
+        let ingredients_str = captures.get(1).unwrap().as_str();
+        let allergens_str = captures.get(2).unwrap().as_str();
+
+        let ingredients: Vec<String> = ingredients_str.split(' ').map(|s| s.to_string()).collect();
+        let allergens: Vec<String> = allergens_str.split(", ").map(|s| s.to_string()).collect();
+
+        return Food {
+            ingredients: ingredients,
+            allergens: allergens,
         }
     }
 }
 
 fn part1(input: &str) -> usize {
     let data = Data::parse(input);
-    return data.execute();
+    let ingredients = data.allergen_free_ingredients();
+    return 0;
 }
 
 // fn part2(input: &str) -> usize {
@@ -60,7 +74,10 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
+        mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+        trh fvjkl sbzzf mxmxvkd (contains dairy)
+        sqjhc fvjkl (contains soy)
+        sqjhc mxmxvkd sbzzf (contains fish)
     "};    
 
     #[test]
