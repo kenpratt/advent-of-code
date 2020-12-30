a8623a121ca3c41c3e220c6a057a1c24583f8158
Ken Pratt <ken@kenpratt.net>
2020-12-29 16:33:03 -0800

    2020 day 22 part 2 solution optimizations.

--- 2020/day-22-crab-combat/src/main.rs
+++ 2020/day-22-crab-combat/src/main.rs
@@ -1,6 +1,8 @@
+use std::collections::hash_map::DefaultHasher;
 use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::fs;
+use std::hash::{Hash, Hasher};
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
@@ -14,7 +16,8 @@ fn read_input_file() -> String {
 #[derive(Debug)]
 struct Combat {
     decks: Vec<Deck>,
-    previously_seen_decks: HashSet<Vec<Deck>>,
+    decks_hash: u64,
+    previously_seen_decks: HashSet<u64>,
     game: usize,
     round: usize,
 }
@@ -22,8 +25,10 @@ struct Combat {
 impl Combat {
     fn parse(input: &str) -> Combat {
         let decks = input.split("\n\n").map(|chunk| Deck::parse(chunk)).collect();
+        let hash = Combat::calculate_decks_hash(&decks);
         Combat {
             decks: decks,
+            decks_hash: hash,
             previously_seen_decks: HashSet::new(),
             game: 1,
             round: 0,
@@ -31,35 +36,26 @@ impl Combat {
     }
 
     fn play(&mut self, recursive: bool) -> usize {
-        println!("\nGame {}:\n", self.game);
-
         while self.winner(recursive).is_none() {
             self.tick(recursive)
         }
 
-        let winner = self.winner(recursive).unwrap();
-        println!("\nPlayer {} wins game {}\n", winner + 1, self.game);
-        winner
+        self.winner(recursive).unwrap()
     }
 
     fn tick(&mut self, recursive: bool) {
         self.round += 1;
-        println!("Round {} (Game {}):", self.round, self.game);
-        for deck in &self.decks {
-            println!("  {} {:?}", deck.player, deck.cards);
-        }
-        
-        // add deck states to list for recursive victory
-        self.previously_seen_decks.insert(self.decks.clone());
 
-        let cards: Vec<usize> = self.decks.iter_mut().map(|deck| deck.draw().unwrap()).collect();
-        println!("cards: {:?}", cards);
+        // add deck states to list for recursive victory
+        self.previously_seen_decks.insert(self.decks_hash);
+
+        let cards: Vec<u8> = self.decks.iter_mut().map(|deck| deck.draw().unwrap()).collect();
 
         // recursive game initiation
-        let winner_index: usize = if recursive && cards.iter().enumerate().all(|(index, card)| self.decks[index].len() >= *card) {
+        let winner_index: usize = if recursive && cards.iter().enumerate().all(|(index, card)| self.decks[index].len() >= (*card as usize)) {
             // recursive game
             let decks_for_sub_game: Vec<Deck> = cards.iter().enumerate().map(|(index, card)| {
-                self.decks[index].sub_deck(*card)
+                self.decks[index].sub_deck(*card as usize)
             }).collect();
             let mut sub_game = self.sub_game(decks_for_sub_game);
             sub_game.play(recursive)
@@ -69,8 +65,6 @@ impl Combat {
             winning_card_index
         };
 
-        println!("  Player {} wins Round {} of Game {}", winner_index+1, self.round, self.game);
-
         // append winning card
         let winning_card = cards[winner_index];
         self.decks[winner_index].append(winning_card);
@@ -78,12 +72,17 @@ impl Combat {
         // append losing card
         for losing_card in cards.iter().filter(|c| **c != winning_card) {
             self.decks[winner_index].append(*losing_card);
-        }        
+        }
+
+        // recalculate decks hash
+        self.decks_hash = Combat::calculate_decks_hash(&self.decks);
     }
 
     fn sub_game(&self, sub_game_decks: Vec<Deck>) -> Combat {
+        let hash = Combat::calculate_decks_hash(&sub_game_decks);
         Combat {
             decks: sub_game_decks,
+            decks_hash: hash,
             previously_seen_decks: HashSet::new(),
             game: self.game + 1,
             round: 0,
@@ -91,14 +90,16 @@ impl Combat {
     }
 
     fn winner(&self, recursive: bool) -> Option<usize> {
-        // if there is exactly one non-empty deck left, that's the winner
-        let non_empty_deck_indices: Vec<usize> = self.decks.iter().enumerate().filter(|(_, deck)| !deck.is_empty()).map(|(index, _)| index).collect();
-        if non_empty_deck_indices.len() == 1 {
+        // if there's an empty deck, the game is over
+        if self.decks.iter().any(|deck| deck.is_empty()) {
+            // non-empty deck is the winner
+            let non_empty_deck_indices: Vec<usize> = self.decks.iter().enumerate().filter(|(_, deck)| !deck.is_empty()).map(|(index, _)| index).collect();
+            assert_eq!(non_empty_deck_indices.len(), 1);
             return Some(non_empty_deck_indices[0]);
         }
 
         // recursive victory condition
-        if recursive && self.previously_seen_decks.contains(&self.decks) {
+        if recursive && self.previously_seen_decks.contains(&self.decks_hash) {
             // player 1 victory
             return Some(0);
         }
@@ -109,22 +110,28 @@ impl Combat {
     fn score(&self) -> usize {
         self.decks.iter().fold(0, |acc, deck| acc + deck.score())
     }
+
+    fn calculate_decks_hash(decks: &Vec<Deck>) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        for deck in decks {
+            deck.hash(&mut hasher);
+        }
+        hasher.finish()
+    }
 }
 
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+#[derive(Debug, Hash, PartialEq)]
 struct Deck {
-    player: String,
-    cards: VecDeque<usize>,
+    cards: VecDeque<u8>,
 }
 
 impl Deck {
     fn parse(input: &str) -> Deck {
         let mut lines = input.lines();
-        let player: String = lines.next().unwrap().to_string();
-        let cards: VecDeque<usize> = lines.map(|s| s.parse::<usize>().unwrap()).collect();
+        lines.next(); // Player X
+        let cards: VecDeque<u8> = lines.map(|s| s.parse::<u8>().unwrap()).collect();
 
         Deck {
-            player: player,
             cards: cards,
         }
     }
@@ -139,16 +146,15 @@ impl Deck {
 
     fn sub_deck(&self, num: usize) -> Deck {
         Deck {
-            player: self.player.clone(),
             cards: self.cards.iter().take(num).cloned().collect(),
         }
     }
 
-    fn draw(&mut self) -> Option<usize> {
+    fn draw(&mut self) -> Option<u8> {
         self.cards.pop_front()
     }
 
-    fn append(&mut self, card: usize) {
+    fn append(&mut self, card: u8) {
         self.cards.push_back(card)
     }
 
@@ -156,7 +162,7 @@ impl Deck {
         let deck_size = self.cards.len();
         self.cards.iter().enumerate().fold(0, |acc, (index, card)| {
             let slot = deck_size - index;
-            let score = slot * card;
+            let score = slot * (*card as usize);
             acc + score
         })
     }
