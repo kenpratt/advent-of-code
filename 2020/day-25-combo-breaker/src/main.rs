ff6f2b38151cf0b5587a209075d9eea6a226c2e2
Ken Pratt <ken@kenpratt.net>
2021-01-01 12:39:05 -0800

    2020 day 25 part 1 solution.

--- 2020/day-25-combo-breaker/src/main.rs
+++ 2020/day-25-combo-breaker/src/main.rs
@@ -1,7 +1,6 @@
 use std::fs;
 
-// use lazy_static::lazy_static;
-// use regex::Regex;
+static LOOP_DIVISOR: usize = 20201227;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
@@ -9,48 +8,66 @@ fn main() {
 }
 
 fn read_input_file() -> String {
-    return fs::read_to_string("input.txt").expect("Something went wrong reading the file");
+    fs::read_to_string("input.txt").expect("Something went wrong reading the file")
 }
 
 #[derive(Debug)]
 struct Data {
-    parts: Vec<Part>,
+    public_keys: Vec<usize>,
 }
 
 impl Data {
     fn parse(input: &str) -> Data {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
-        return Data {
-            parts: parts,
+        let public_keys = input.lines().map(|line| line.parse::<usize>().unwrap()).collect();
+        Data {
+            public_keys: public_keys,
         }
     }
 
-    fn execute(&self) -> usize {
-        return 0;
+    fn calculate_encryption_key(&self) -> usize {
+        assert_eq!(self.public_keys.len(), 2);
+        let public_key0 = self.public_keys[0];
+        let public_key1 = self.public_keys[1];
+
+        let loop_size0 = determine_loop_size(7, public_key0);
+        let loop_size1 = determine_loop_size(7, public_key1);
+
+        let encryption_key0 = calculate_encryption_key(public_key1, loop_size0);
+        let encryption_key1 = calculate_encryption_key(public_key0, loop_size1);
+        assert_eq!(encryption_key0, encryption_key1);
+
+        encryption_key0
     }
 }
 
-#[derive(Debug)]
-struct Part {
-    foo: usize,
-}
-
-impl Part {
-    fn parse(input: &str) -> Part {
-        return Part {
-            foo: input.len(),
+fn determine_loop_size(subject_number: usize, public_key: usize) -> usize {
+    let mut loop_size: usize = 0;
+    let mut value: usize = 1;
+    loop {
+        loop_size += 1;
+        value = (value * subject_number) % LOOP_DIVISOR;
+        if value == public_key {
+            return loop_size;
         }
     }
+}
+
+fn calculate_encryption_key(public_key: usize, loop_size: usize) -> usize {
+    let mut value: usize = 1;
+    for _ in 0..loop_size {
+        value = (value * public_key) % LOOP_DIVISOR;
+    }
+    value
 }
 
 fn part1(input: &str) -> usize {
     let data = Data::parse(input);
-    return data.execute();
+    data.calculate_encryption_key()
 }
 
 // fn part2(input: &str) -> usize {
 //     let data = Data::parse(input);
-//     return data.execute();
+//     data.execute()
 // }
 
 #[cfg(test)]
@@ -60,20 +77,33 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
-    "};    
+        5764801
+        17807724
+    "};
+
+    #[test]
+    fn test_determine_loop_size() {
+        assert_eq!(determine_loop_size(7, 5764801), 8);
+        assert_eq!(determine_loop_size(7, 17807724), 11);
+    }
+
+    #[test]
+    fn test_calculate_encryption_key() {
+        assert_eq!(calculate_encryption_key(17807724, 8), 14897079);
+        assert_eq!(calculate_encryption_key(5764801, 11), 14897079);
+    }
 
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 14897079);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 11707042);
+    }
 
     // #[test]
     // fn test_part2_example1() {
