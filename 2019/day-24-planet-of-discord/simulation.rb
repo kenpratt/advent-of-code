602d5166e32ad5cc351b02b30199ae01e5daa8d6
Ken Pratt <ken@kenpratt.net>
2021-01-01 18:11:32 -0800

    2019 day 24 part 2 solution (and rewrite part 1).

--- 2019/day-24-planet-of-discord/simulation.rb
+++ 2019/day-24-planet-of-discord/simulation.rb
@@ -1,42 +1,140 @@
-require_relative 'grid'
+DIRECTIONS = [:left, :right, :up, :down]
+
+Coord = Struct.new(:level, :x, :y)
 
 class Simulation
-  def initialize(map_str)
+  def initialize(map_str, recursive)
     rows = map_str.split("\n").map {|line| line.each_char.map {|c| self.class.parse_char(c)}}
-    @map = StaticGrid.from_rows(rows)
-    @neighbours = @map.coords.map {|c| [c, c.neighbours.values]}.to_h
+
+    @width = rows.first.size
+    @height = rows.size
+    @recursive = recursive
+    @neighbour_cache = {}
+
+    @center_x = @width / 2
+    @center_y = @height / 2
+
+    @alive = Set.new
+    rows.each_with_index do |row, y|
+      row.each_with_index do |val, x|
+        @alive << Coord.new(0, x, y) if val
+      end
+    end
   end
 
   def run_until_repeat
     history = Set.new
-    history << self.to_s
 
-    found_duplicate = false
-    while !found_duplicate
+    while !history.include?(@alive)
+      history << @alive.clone
       tick
+    end
 
-      s = self.to_s
-      if history.include?(s)
-        found_duplicate = true
-      else
-        history << s
-      end
+    self
+  end
+
+  def run(cycles)
+    cycles.times do
+      tick
     end
 
     self
   end
 
   def tick
-    new_cells = @map.coords.map do |c|
-      [c, new_value(c)]
-    end.to_h
-    @map.set_cells!(new_cells)
+    new_alive = []
+    visit_locations do |cell|
+      new_alive << cell if new_value(cell)
+    end
+    @alive = Set.new(new_alive)
   end
 
-  def new_value(c)
-    alive = @map.value(c)
-    num_adjacent = @neighbours[c].count {|n| @map.value(n)}
-    if alive
+  def visit_locations
+    if !@recursive
+      # visit the whole grid
+      (0...@height).map do |y|
+        (0...@width).map do |x|
+          yield Coord.new(0, x, y)
+        end
+      end
+    else
+      # visit all knows levels, plus one lower and higher in case of bug
+      # infection to new recursion level
+      min_level, max_level = level_bounds
+      ((min_level - 1)..(max_level + 1)).map do |level|
+        (0...@height).map do |y|
+          (0...@width).map do |x|
+            yield Coord.new(level, x, y) unless x == @center_x && y == @center_y
+          end
+        end
+      end
+    end
+  end
+
+  def neighbours(cell)
+    @neighbour_cache[cell] ||= calculate_neighbours(cell)
+  end
+
+  def calculate_neighbours(cell)
+    DIRECTIONS.map {|d| Set.new(calculate_neighbours_in_direction(cell, d))}.inject(:|)
+  end
+
+  def calculate_neighbours_in_direction(cell, direction)
+    nx = cell.x + case direction
+    when :left then -1
+    when :right then 1
+    else 0
+    end
+
+    ny = cell.y + case direction
+    when :up then -1
+    when :down then 1
+    else 0
+    end
+
+    beyond_edge = nx < 0 || nx == @width || ny < 0 || ny == @height
+
+    if !@recursive
+      if beyond_edge
+        # hit the edge
+        []
+      else
+        # neighbour
+        [Coord.new(cell.level, nx, ny)]
+      end
+    else
+      if nx == @center_x && ny == @center_y
+        # recurse into inner board, treating whole edge as adjacent
+        lvl = cell.level + 1
+        case direction
+        when :left
+          # right edge
+          (0...@height).map {|y| Coord.new(lvl, @width - 1, y)}
+        when :right
+          # left edge
+          (0...@height).map {|y| Coord.new(lvl, 0, y)}
+        when :up
+          # bottom edge
+          (0...@width).map {|x| Coord.new(lvl, x, @height - 1)}
+        when :down
+          # top edge
+          (0...@width).map {|x| Coord.new(lvl, x, 0)}
+        end
+      elsif beyond_edge
+        # recurse into outer board, in same direction from center
+        calculate_neighbours_in_direction(Coord.new(cell.level - 1, @center_x, @center_y), direction)
+      else
+        # normal neighbour
+        [Coord.new(cell.level, nx, ny)]
+      end
+    end
+  end
+
+  def new_value(cell)
+    adjacent = neighbours(cell)
+    num_adjacent = adjacent.count {|n| @alive.include?(n)}
+
+    if @alive.include?(cell)
       # A bug dies (becoming an empty space) unless there is exactly one bug adjacent to it.
       num_adjacent == 1
     else
@@ -56,17 +154,46 @@ class Simulation
     end
   end
 
+  def level_bounds
+    min_level = 0
+    max_level = 0
+    @alive.each do |cell|
+      level = cell.level
+      min_level = level if level < min_level
+      max_level = level if level > max_level
+    end
+    [min_level, max_level]
+  end
+
   def to_s
-    @map.to_s(borders: false) {|_, alive| alive ? '#' : '.'}
+    min_level, max_level = level_bounds
+    (min_level..max_level).map do |level|
+      (@recursive ? "Depth #{level}:\n" : "") +
+      (0...@height).map do |y|
+        (0...@width).map do |x|
+          if @recursive && x == @center_x && y == @center_y
+            '?'
+          else
+            @alive.include?(Coord.new(level, x, y)) ? '#' : '.'
+          end
+        end.join('')
+      end.join("\n")
+    end.join("\n\n")
   end
 
   def biodiversity_rating
     output = 0
-    x = 1
-    @map.cells.each do |_, v|
-      output += x if v
-      x *= 2
+    f = 1
+    (0...@height).map do |y|
+      (0...@width).map do |x|
+        output += f if @alive.include?(Coord.new(0, x, y))
+        f *= 2
+      end
     end
     output
+  end
+
+  def count_bugs
+    @alive.size
   end
 end
--- 2019/day-24-planet-of-discord/test.rb
+++ 2019/day-24-planet-of-discord/test.rb
@@ -14,7 +14,7 @@ class TestPart1 < Minitest::Test
     min3_output_str = "#....\n####.\n...##\n#.##.\n.##.#"
     min4_output_str = "####.\n....#\n##..#\n.....\n##..."
 
-    simulation = Simulation.new(input_str)
+    simulation = Simulation.new(input_str, false)
     assert_equal(input_str, simulation.to_s.strip)
     simulation.tick
     assert_equal(min1_output_str, simulation.to_s.strip)
@@ -30,7 +30,7 @@ class TestPart1 < Minitest::Test
     input_str = "....#\n#..#.\n#..##\n..#..\n#...."
     output_str = ".....\n.....\n.....\n#....\n.#..."
 
-    simulation = Simulation.new(input_str)
+    simulation = Simulation.new(input_str, false)
     simulation.run_until_repeat
 
     assert_equal(output_str, simulation.to_s.strip)
@@ -40,9 +40,106 @@ class TestPart1 < Minitest::Test
   def test_input1
     input_str = File.read(INPUT_FILE)
 
-    simulation = Simulation.new(input_str)
+    simulation = Simulation.new(input_str, false)
     simulation.run_until_repeat
 
     assert_equal(32776479, simulation.biodiversity_rating)
+  end
+end
+
+class TestPart2 < Minitest::Test
+  def test_example2
+    input_str = "....#\n#..#.\n#..##\n..#..\n#...."
+    min10_output_str = <<~EOF
+      Depth -5:
+      ..#..
+      .#.#.
+      ..?.#
+      .#.#.
+      ..#..
+
+      Depth -4:
+      ...#.
+      ...##
+      ..?..
+      ...##
+      ...#.
+
+      Depth -3:
+      #.#..
+      .#...
+      ..?..
+      .#...
+      #.#..
+
+      Depth -2:
+      .#.##
+      ....#
+      ..?.#
+      ...##
+      .###.
+
+      Depth -1:
+      #..##
+      ...##
+      ..?..
+      ...#.
+      .####
+
+      Depth 0:
+      .#...
+      .#.##
+      .#?..
+      .....
+      .....
+
+      Depth 1:
+      .##..
+      #..##
+      ..?.#
+      ##.##
+      #####
+
+      Depth 2:
+      ###..
+      ##.#.
+      #.?..
+      .#.##
+      #.#..
+
+      Depth 3:
+      ..###
+      .....
+      #.?..
+      #....
+      #...#
+
+      Depth 4:
+      .###.
+      #..#.
+      #.?..
+      ##.#.
+      .....
+
+      Depth 5:
+      ####.
+      #..#.
+      #.?#.
+      ####.
+      .....
+    EOF
+
+    simulation = Simulation.new(input_str, true)
+    simulation.run(10)
+    assert_equal(min10_output_str.strip, simulation.to_s.strip)
+    assert_equal(99, simulation.count_bugs)
+  end
+
+  def test_input2
+    input_str = File.read(INPUT_FILE)
+
+    simulation = Simulation.new(input_str, true)
+    simulation.run(200)
+    assert_equal(2017, simulation.count_bugs)
   end
 end
