28086dff849e5774b638875c379302b67526e7d9
Ken Pratt <ken@kenpratt.net>
2021-12-01 21:18:24 -0800

    day 2 part 1

--- 2021/day-02-dive/src/main.rs
+++ 2021/day-02-dive/src/main.rs
@@ -1,7 +1,6 @@
 use std::fs;
 
-// use lazy_static::lazy_static;
-// use regex::Regex;
+use regex::Regex;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
@@ -12,40 +11,66 @@ fn read_input_file() -> String {
     fs::read_to_string("input.txt").expect("Something went wrong reading the file")
 }
 
-#[derive(Debug)]
-struct Data {
-    parts: Vec<Part>,
+fn parse(input: &str) -> Vec<Command> {
+    input.lines().map(|line| Command::parse(line)).collect()
 }
 
-impl Data {
-    fn parse(input: &str) -> Data {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
-        Data {
-            parts: parts,
+#[derive(Debug)]
+enum Command {
+    Forward(usize),
+    Down(usize),
+    Up(usize),
+}
+
+impl Command {
+    fn parse(input: &str) -> Command {
+        let re = Regex::new(r"^(\w+) (\d+)$").unwrap();
+        let captures = re.captures(input).unwrap();
+        let direction = captures.get(1).unwrap().as_str();
+        let amount = captures.get(2).unwrap().as_str().parse::<usize>().unwrap();
+        match direction {
+            "forward" => Command::Forward(amount),
+            "down" => Command::Down(amount),
+            "up" => Command::Up(amount),
+            _ => panic!("Bad input: {}", input)
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Submarine {
+    position: usize,
+    depth: usize,
+}
+
+impl Submarine {
+    fn new() -> Submarine {
+        Submarine {
+            position: 0,
+            depth: 0,
         }
     }
 
-    fn execute(&self) -> usize {
-        0
+    fn navigate(&mut self, commands: &[Command]) {
+        for command in commands {
+            self.step(command);
+        }
     }
-}
 
-#[derive(Debug)]
-struct Part {
-    foo: usize,
-}
-
-impl Part {
-    fn parse(input: &str) -> Part {
-        Part {
-            foo: input.len(),
+    fn step(&mut self, command: &Command) {
+        match command {
+            Command::Forward(amount) => self.position += amount,
+            Command::Down(amount) => self.depth += amount,
+            Command::Up(amount) => self.depth -= amount,
         }
     }
 }
 
 fn part1(input: &str) -> usize {
-    let data = Data::parse(input);
-    data.execute()
+    let commands = parse(input);
+    let mut sub = Submarine::new();
+    sub.navigate(&commands);
+    sub.position * sub.depth
 }
 
 // fn part2(input: &str) -> usize {
@@ -60,20 +85,25 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
     "};    
 
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 150);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 1990000);
+    }
 
     // #[test]
     // fn test_part2_example1() {
