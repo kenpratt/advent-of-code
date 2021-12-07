98c55a260e9cf01423517599b7a67c06adc2b49e
Ken Pratt <ken@kenpratt.net>
2021-12-06 17:21:01 -0800

    day 6 part 1

--- 2021/day-06-lanternfish/src/main.rs
+++ 2021/day-06-lanternfish/src/main.rs
@@ -1,9 +1,5 @@
 use std::fs;
 
-// use itertools::Itertools;
-// use lazy_static::lazy_static;
-// use regex::Regex;
-
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
     // println!("part 2 result: {:?}", part2(&read_input_file()));
@@ -13,37 +9,43 @@ fn read_input_file() -> String {
     fs::read_to_string("input.txt").expect("Something went wrong reading the file")
 }
 
-#[derive(Debug)]
-struct Data {
-    parts: Vec<Part>,
+fn parse(input: &str) -> Vec<u8> {
+    let line = input.lines().next().unwrap();
+    line.split(',').map(|s| s.parse::<u8>().unwrap()).collect()
 }
 
-impl Data {
-    fn parse(input: &str) -> Data {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
-        Data { parts: parts }
-    }
-
-    fn execute(&self) -> usize {
-        0
-    }
+// 80 rounds:
+// - fish@0: 0, 7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77 (12)
+// - fish@1: 1, 8, 15, 22, 29, 36, 43, 50, 57, 64, 71, 78 (12)
+// - fish@2: 2, ..., 72, 79 (12)
+// - fish@3: 3, ..., 73 (11)
+// - fish@4: 4, ..., 74 (11)
+// - fish@5: 5, ..., 75 (11)
+// - fish@6: 6, ..., 76 (11)
+fn count_fish(fish_timers: &[u8], rounds_remaining: &usize) -> usize {
+    fish_timers
+        .iter()
+        .map(|f| count_fish_at_day_0((*rounds_remaining as isize) - (*f as isize)))
+        .sum()
 }
 
-#[derive(Debug)]
-struct Part {
-    foo: usize,
-}
-
-impl Part {
-    fn parse(input: &str) -> Part {
-        Part { foo: input.len() }
+fn count_fish_at_day_0(rounds_remaining: isize) -> usize {
+    if rounds_remaining <= 0 {
+        return 1; // base case: count self
     }
+
+    let mut offspring = 1; // count self
+    let mut round = rounds_remaining;
+    while round > 0 {
+        offspring += count_fish_at_day_0(round - 9);
+        round -= 7;
+    }
+    offspring
 }
 
 fn part1(input: &str) -> usize {
-    let data = Data::parse(input);
-    println!("{:?}", data);
-    data.execute()
+    let fish = parse(input);
+    count_fish(&fish, &80)
 }
 
 // fn part2(input: &str) -> usize {
@@ -59,20 +61,20 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
+        3,4,3,1,2
     "};
 
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 5934);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 386755);
+    }
 
     // #[test]
     // fn test_part2_example1() {
