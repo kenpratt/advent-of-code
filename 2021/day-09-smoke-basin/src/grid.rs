3a64e6b26a43bb6a9cc8b13efcde782ab07df6e7
Ken Pratt <ken@kenpratt.net>
2021-12-09 15:55:23 -0800

    day 9 part 1

---
+++ 2021/day-09-smoke-basin/src/grid.rs
@@ -0,0 +1,143 @@
+#[derive(Debug, PartialEq)]
+pub enum Direction {
+    Up,
+    Left,
+    Right,
+    Down,
+}
+
+static DIRECTIONS: &'static [Direction] = &[
+    Direction::Up,
+    Direction::Left,
+    Direction::Right,
+    Direction::Down,
+];
+
+#[derive(Debug, PartialEq)]
+pub struct Coordinate {
+    x: usize,
+    y: usize,
+}
+
+impl Coordinate {
+    fn new(x: usize, y: usize) -> Coordinate {
+        Coordinate { x: x, y: y }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Bounds {
+    width: usize,
+    height: usize,
+}
+
+fn coordinate_to_index(pos: &Coordinate, bounds: &Bounds) -> usize {
+    (pos.y * bounds.width) + pos.x
+}
+
+fn index_to_coordinate(index: &usize, bounds: &Bounds) -> Coordinate {
+    Coordinate::new(index % bounds.width, index / bounds.width)
+}
+
+fn neighbours(pos: &Coordinate, bounds: &Bounds) -> Vec<Coordinate> {
+    DIRECTIONS
+        .iter()
+        .filter_map(|d| neighbour(pos, d, bounds))
+        .collect()
+}
+
+fn neighbour(pos: &Coordinate, direction: &Direction, bounds: &Bounds) -> Option<Coordinate> {
+    match direction {
+        Direction::Up => {
+            if pos.y > 0 {
+                Some(Coordinate::new(pos.x, pos.y - 1))
+            } else {
+                None
+            }
+        }
+        Direction::Down => {
+            if pos.y < (bounds.height - 1) {
+                Some(Coordinate::new(pos.x, pos.y + 1))
+            } else {
+                None
+            }
+        }
+        Direction::Left => {
+            if pos.x > 0 {
+                Some(Coordinate::new(pos.x - 1, pos.y))
+            } else {
+                None
+            }
+        }
+        Direction::Right => {
+            if pos.x < (bounds.width - 1) {
+                Some(Coordinate::new(pos.x + 1, pos.y))
+            } else {
+                None
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Grid<T> {
+    cells: Vec<T>,
+    bounds: Bounds,
+}
+
+impl<T> Grid<T> {
+    pub fn new(values: Vec<Vec<T>>) -> Grid<T> {
+        let height = values.len();
+        let width = values[0].len();
+        if values.iter().any(|row| row.len() != width) {
+            panic!("Grid data is not rectangular");
+        }
+        Grid {
+            cells: values.into_iter().flatten().collect(),
+            bounds: Bounds {
+                height: height,
+                width: width,
+            },
+        }
+    }
+
+    fn len(&self) -> usize {
+        self.cells.len()
+    }
+
+    pub fn value(&self, pos: &Coordinate) -> &T {
+        let index = coordinate_to_index(pos, &self.bounds);
+        &self.cells[index]
+    }
+
+    pub fn neighbours(&self, pos: &Coordinate) -> Vec<Coordinate> {
+        neighbours(pos, &self.bounds)
+    }
+
+    pub fn iter(&self) -> GridIterator<'_, T> {
+        GridIterator {
+            grid: self,
+            index: 0,
+        }
+    }
+}
+
+pub struct GridIterator<'a, T> {
+    grid: &'a Grid<T>,
+    index: usize,
+}
+
+impl<'a, T> Iterator for GridIterator<'a, T> {
+    type Item = (Coordinate, &'a T);
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.grid.len() {
+            let pos = index_to_coordinate(&self.index, &self.grid.bounds);
+            let value = &self.grid.cells[self.index];
+            let result = (pos, value);
+            self.index += 1;
+            Some(result)
+        } else {
+            None
+        }
+    }
+}
--- 2021/day-09-smoke-basin/src/main.rs
+++ 2021/day-09-smoke-basin/src/main.rs
@@ -1,8 +1,9 @@
-use std::fs;
+pub mod grid;
 
-// use itertools::Itertools;
-// use lazy_static::lazy_static;
-// use regex::Regex;
+use grid::Coordinate;
+use grid::Grid;
+
+use std::fs;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
@@ -14,42 +15,55 @@ fn read_input_file() -> String {
 }
 
 #[derive(Debug)]
-struct Data {
-    parts: Vec<Part>,
+struct HeightMap {
+    grid: Grid<u8>,
 }
 
-impl Data {
-    fn parse(input: &str) -> Data {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
-        Data { parts: parts }
+impl HeightMap {
+    fn parse(input: &str) -> HeightMap {
+        let heights: Vec<Vec<u8>> = input
+            .lines()
+            .map(|line| HeightMap::parse_line(line))
+            .collect();
+        let grid = Grid::new(heights);
+        HeightMap { grid: grid }
     }
 
-    fn execute(&self) -> usize {
-        0
+    fn parse_line(line: &str) -> Vec<u8> {
+        line.chars()
+            .flat_map(|c| c.to_digit(10))
+            .map(|v| v as u8)
+            .collect()
     }
-}
 
-#[derive(Debug)]
-struct Part {
-    foo: usize,
-}
-
-impl Part {
-    fn parse(input: &str) -> Part {
-        Part { foo: input.len() }
+    fn low_points(&self) -> Vec<(Coordinate, &u8)> {
+        self.grid
+            .iter()
+            .filter(|(pos, val)| {
+                self.grid
+                    .neighbours(pos)
+                    .iter()
+                    .all(|npos| val < &self.grid.value(npos))
+            })
+            .collect()
     }
 }
 
 fn part1(input: &str) -> usize {
-    let data = Data::parse(input);
-    println!("{:?}", data);
-    data.execute()
+    let map = HeightMap::parse(input);
+    println!("map: {:?}", map);
+    let low_points = map.low_points();
+    println!("low_points: {:?}", low_points);
+    low_points
+        .into_iter()
+        .map(|(_pos, value)| *value as usize + 1)
+        .sum()
 }
 
 // fn part2(input: &str) -> usize {
-//     let data = Data::parse(input);
-//     println!("{:?}", data);
-//     data.execute()
+//     let map = HeightMap::parse(input);
+//     println!("{:?}", map);
+//     map.execute()
 // }
 
 #[cfg(test)]
@@ -59,20 +73,24 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
+        2199943210
+        3987894921
+        9856789892
+        8767896789
+        9899965678
     "};
 
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 15);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 564);
+    }
 
     // #[test]
     // fn test_part2_example1() {
