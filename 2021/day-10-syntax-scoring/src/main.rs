fc6548aae75e8d43ace4dc7c8b3571c323f0c38a
Ken Pratt <ken@kenpratt.net>
2021-12-11 15:33:22 -0800

    day 10 part 1

--- 2021/day-10-syntax-scoring/src/main.rs
+++ 2021/day-10-syntax-scoring/src/main.rs
@@ -1,8 +1,5 @@
 use std::fs;
-
-// use itertools::Itertools;
-// use lazy_static::lazy_static;
-// use regex::Regex;
+use std::str::Chars;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
@@ -14,36 +11,101 @@ fn read_input_file() -> String {
 }
 
 #[derive(Debug)]
-struct Data {
-    parts: Vec<Part>,
-}
-
-impl Data {
-    fn parse(input: &str) -> Data {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
-        Data { parts: parts }
-    }
-
-    fn execute(&self) -> usize {
-        0
-    }
+enum ParseResult {
+    Legal,
+    Incomplete,
+    Corrupted(char),
 }
 
 #[derive(Debug)]
-struct Part {
-    foo: usize,
+struct Parser<'a> {
+    chars: Chars<'a>,
+    open_chunks: Vec<char>,
 }
 
-impl Part {
-    fn parse(input: &str) -> Part {
-        Part { foo: input.len() }
+impl Parser<'_> {
+    fn parse(line: &str) -> ParseResult {
+        let mut parser = Parser {
+            chars: line.chars(),
+            open_chunks: vec![],
+        };
+        parser.run()
+    }
+
+    fn run(&mut self) -> ParseResult {
+        loop {
+            let result = self.consume_char();
+            if result.is_some() {
+                return result.unwrap();
+            }
+        }
+    }
+
+    fn consume_char(&mut self) -> Option<ParseResult> {
+        match self.chars.next() {
+            Some(c) => match c {
+                '(' | '[' | '{' | '<' => self.open_chunk(c),
+                '>' | '}' | ']' | ')' => self.close_chunk(c),
+                _ => panic!("Invalid char: {}", c),
+            },
+            None => {
+                // we got to the end of the line. if we have nothing dangling, it's a legal line.
+                if self.open_chunks.len() == 0 {
+                    Some(ParseResult::Legal)
+                } else {
+                    Some(ParseResult::Incomplete)
+                }
+            }
+        }
+    }
+
+    fn open_chunk(&mut self, c: char) -> Option<ParseResult> {
+        self.open_chunks.push(c);
+        None
+    }
+
+    fn close_chunk(&mut self, c: char) -> Option<ParseResult> {
+        let expected_opening_delimiter = Parser::opening_delimiter(&c);
+        if self.open_chunks.last() == Some(&expected_opening_delimiter) {
+            self.open_chunks.pop();
+            None
+        } else {
+            Some(ParseResult::Corrupted(c))
+        }
+    }
+
+    fn opening_delimiter(c: &char) -> char {
+        match c {
+            '>' => '<',
+            '}' => '{',
+            ']' => '[',
+            ')' => '(',
+            _ => panic!("Invalid char: {}", c),
+        }
     }
 }
 
 fn part1(input: &str) -> usize {
-    let data = Data::parse(input);
-    println!("{:?}", data);
-    data.execute()
+    let results: Vec<ParseResult> = input.lines().map(|line| Parser::parse(line)).collect();
+    println!("{:?}", results);
+    results.iter().map(|r| score_for_result(r)).sum()
+}
+
+fn score_for_result(result: &ParseResult) -> usize {
+    match result {
+        ParseResult::Legal | ParseResult::Incomplete => 0,
+        ParseResult::Corrupted(c) => score_for_corrupting_char(c),
+    }
+}
+
+fn score_for_corrupting_char(c: &char) -> usize {
+    match c {
+        '>' => 25137,
+        '}' => 1197,
+        ']' => 57,
+        ')' => 3,
+        _ => panic!("Invalid char: {}", c),
+    }
 }
 
 // fn part2(input: &str) -> usize {
@@ -59,20 +121,29 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
+        [({(<(())[]>[[{[]{<()<>>
+        [(()[<>])]({[<{<<[]>>(
+        {([(<{}[<>[]}>{[]{[(<()>
+        (((({<>}<{<{<>}{[]{[]{}
+        [[<[([]))<([[{}[[()]]]
+        [{[{({}]{}}([{[{{{}}([]
+        {<[[]]>}<{[{[{[]{()[[[]
+        [<(<(<(<{}))><([]([]()
+        <{([([[(<>()){}]>(<<{{
+        <{([{{}}[<[[[<>{}]]]>[]]
     "};
 
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 26397);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 390993);
+    }
 
     // #[test]
     // fn test_part2_example1() {
