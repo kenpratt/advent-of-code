45b5d8f0fe46eed329784467e57d90a3330c00b3
Ken Pratt <ken@kenpratt.net>
2021-12-11 17:05:16 -0800

    day 11 part 1

---
+++ 2021/day-11-dumbo-octopus/src/grid.rs
@@ -0,0 +1,171 @@
+#[derive(Debug, PartialEq)]
+pub enum Direction {
+    UpLeft,
+    Up,
+    UpRight,
+    Left,
+    Right,
+    DownLeft,
+    Down,
+    DownRight,
+}
+
+static DIRECTIONS: &'static [Direction] = &[
+    Direction::UpLeft,
+    Direction::Up,
+    Direction::UpRight,
+    Direction::Left,
+    Direction::Right,
+    Direction::DownLeft,
+    Direction::Down,
+    Direction::DownRight,
+];
+
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+pub struct Coordinate {
+    x: usize,
+    y: usize,
+}
+
+impl Coordinate {
+    fn new(x: usize, y: usize) -> Coordinate {
+        Coordinate { x: x, y: y }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Bounds {
+    width: usize,
+    height: usize,
+}
+
+fn coordinate_to_index(pos: &Coordinate, bounds: &Bounds) -> usize {
+    (pos.y * bounds.width) + pos.x
+}
+
+fn index_to_coordinate(index: &usize, bounds: &Bounds) -> Coordinate {
+    Coordinate::new(index % bounds.width, index / bounds.width)
+}
+
+fn neighbours(pos: &Coordinate, bounds: &Bounds) -> Vec<Coordinate> {
+    DIRECTIONS
+        .iter()
+        .filter_map(|d| neighbour(pos, d, bounds))
+        .collect()
+}
+
+fn decrease(v: usize, limit: usize) -> Option<usize> {
+    if v > limit {
+        Some(v - 1)
+    } else {
+        None
+    }
+}
+
+fn increase(v: usize, limit: usize) -> Option<usize> {
+    if v < (limit - 1) {
+        Some(v + 1)
+    } else {
+        None
+    }
+}
+
+fn maybe_coord(x: Option<usize>, y: Option<usize>) -> Option<Coordinate> {
+    if x.is_some() && y.is_some() {
+        Some(Coordinate::new(x.unwrap(), y.unwrap()))
+    } else {
+        None
+    }
+}
+
+fn neighbour(pos: &Coordinate, direction: &Direction, bounds: &Bounds) -> Option<Coordinate> {
+    match direction {
+        Direction::UpLeft => maybe_coord(decrease(pos.x, 0), decrease(pos.y, 0)),
+        Direction::Up => maybe_coord(Some(pos.x), decrease(pos.y, 0)),
+        Direction::UpRight => maybe_coord(increase(pos.x, bounds.width), decrease(pos.y, 0)),
+        Direction::Left => maybe_coord(decrease(pos.x, 0), Some(pos.y)),
+        Direction::Right => maybe_coord(increase(pos.x, bounds.width), Some(pos.y)),
+        Direction::DownLeft => maybe_coord(decrease(pos.x, 0), increase(pos.y, bounds.height)),
+        Direction::Down => maybe_coord(Some(pos.x), increase(pos.y, bounds.height)),
+        Direction::DownRight => maybe_coord(
+            increase(pos.x, bounds.width),
+            increase(pos.y, bounds.height),
+        ),
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Grid<T> {
+    cells: Vec<Cell<T>>,
+    bounds: Bounds,
+}
+
+impl<T> Grid<T> {
+    pub fn new(values: Vec<Vec<T>>) -> Grid<T> {
+        let height = values.len();
+        let width = values[0].len();
+        if values.iter().any(|row| row.len() != width) {
+            panic!("Grid data is not rectangular");
+        }
+        let bounds = Bounds {
+            height: height,
+            width: width,
+        };
+        Grid {
+            cells: values
+                .into_iter()
+                .flatten()
+                .enumerate()
+                .map(|(index, value)| Cell {
+                    position: index_to_coordinate(&index, &bounds),
+                    value: value,
+                })
+                .collect(),
+            bounds: bounds,
+        }
+    }
+
+    pub fn cell(&self, pos: &Coordinate) -> &Cell<T> {
+        let index = coordinate_to_index(pos, &self.bounds);
+        &self.cells[index]
+    }
+
+    pub fn cell_mut(&mut self, pos: &Coordinate) -> &mut Cell<T> {
+        let index = coordinate_to_index(pos, &self.bounds);
+        &mut self.cells[index]
+    }
+
+    pub fn value(&self, pos: &Coordinate) -> &T {
+        &self.cell(pos).value
+    }
+
+    pub fn neighbours(&self, pos: &Coordinate) -> Vec<Coordinate> {
+        neighbours(pos, &self.bounds)
+    }
+
+    pub fn iter(&self) -> std::slice::Iter<Cell<T>> {
+        self.cells.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> std::slice::IterMut<Cell<T>> {
+        self.cells.iter_mut()
+    }
+
+    pub fn render(&self, value_to_str: fn(&T) -> String) -> String {
+        (0..(self.bounds.height))
+            .map(|y| {
+                (0..(self.bounds.width))
+                    .map(|x| value_to_str(self.value(&Coordinate::new(x, y))))
+                    .collect::<Vec<String>>()
+                    .join("")
+            })
+            .collect::<Vec<String>>()
+            .join("\n")
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Cell<T> {
+    pub position: Coordinate,
+    pub value: T,
+}
--- 2021/day-11-dumbo-octopus/src/main.rs
+++ 2021/day-11-dumbo-octopus/src/main.rs
@@ -1,8 +1,9 @@
-use std::fs;
+pub mod grid;
 
-// use itertools::Itertools;
-// use lazy_static::lazy_static;
-// use regex::Regex;
+use grid::Grid;
+
+use std::collections::VecDeque;
+use std::fs;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
@@ -14,36 +15,82 @@ fn read_input_file() -> String {
 }
 
 #[derive(Debug)]
-struct Data {
-    parts: Vec<Part>,
+struct Simulation {
+    grid: Grid<u8>,
+    flashes: usize,
 }
 
-impl Data {
-    fn parse(input: &str) -> Data {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
-        Data { parts: parts }
+impl Simulation {
+    fn parse(input: &str) -> Simulation {
+        let values: Vec<Vec<u8>> = input
+            .lines()
+            .map(|line| Simulation::parse_line(line))
+            .collect();
+        let grid = Grid::new(values);
+        Simulation {
+            grid: grid,
+            flashes: 0,
+        }
     }
 
-    fn execute(&self) -> usize {
-        0
+    fn parse_line(line: &str) -> Vec<u8> {
+        line.chars()
+            .filter_map(|c| c.to_digit(10))
+            .map(|v| v as u8)
+            .collect()
     }
-}
 
-#[derive(Debug)]
-struct Part {
-    foo: usize,
-}
+    fn tick(&mut self) {
+        let mut to_flash = VecDeque::new();
 
-impl Part {
-    fn parse(input: &str) -> Part {
-        Part { foo: input.len() }
+        for cell in self.grid.iter_mut() {
+            if cell.value < 9 {
+                cell.value += 1;
+            } else {
+                self.flashes += 1;
+                cell.value = 0;
+                to_flash.push_back(cell.position);
+            }
+        }
+
+        loop {
+            match to_flash.pop_front() {
+                Some(pos) => {
+                    for neighbour_pos in self.grid.neighbours(&pos) {
+                        let mut neighbour = self.grid.cell_mut(&neighbour_pos);
+                        match neighbour.value {
+                            0 => (), // already flashed, do nothing
+                            9 => {
+                                self.flashes += 1;
+                                neighbour.value = 0;
+                                to_flash.push_back(neighbour_pos)
+                            }
+                            _ => neighbour.value += 1,
+                        }
+                    }
+                }
+                None => break,
+            }
+        }
+    }
+
+    fn render(&self) -> String {
+        format!(
+            "{}\nflashes: {}",
+            self.grid.render(|v| v.to_string()),
+            self.flashes
+        )
     }
 }
 
 fn part1(input: &str) -> usize {
-    let data = Data::parse(input);
-    println!("{:?}", data);
-    data.execute()
+    let mut simulation = Simulation::parse(input);
+    println!("{}\n", simulation.render());
+    for _ in 0..100 {
+        simulation.tick();
+        println!("{}\n", simulation.render());
+    }
+    simulation.flashes
 }
 
 // fn part2(input: &str) -> usize {
@@ -59,20 +106,29 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
+        5483143223
+        2745854711
+        5264556173
+        6141336146
+        6357385478
+        4167524645
+        2176841721
+        6882881134
+        4846848554
+        5283751526
     "};
 
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 1656);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 1743);
+    }
 
     // #[test]
     // fn test_part2_example1() {
