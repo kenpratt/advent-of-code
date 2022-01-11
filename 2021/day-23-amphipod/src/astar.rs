4937db199a5db8a740273eec390a5541e5aaf4eb
Ken Pratt <ken@kenpratt.net>
2022-01-10 20:04:55 -0800

    day 23 part 1

---
+++ 2021/day-23-amphipod/src/astar.rs
@@ -0,0 +1,111 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::hash::Hash;
+
+pub trait AStarInterface<N: Copy + Debug + Hash + Ord> {
+    fn at_goal(&self, node: &N) -> bool;
+    fn heuristic(&self, from: &N) -> usize;
+    fn neighbours(&mut self, from: &N) -> Vec<(N, usize)>;
+
+    fn shortest_path(&mut self, start: &N) -> (Vec<(N, usize)>, usize) {
+        let mut open_set: OpenSet<N> = OpenSet::new();
+        let mut came_from: HashMap<N, (N, usize)> = HashMap::new();
+        let mut g_score: HashMap<N, usize> = HashMap::new();
+
+        // add start to open set
+        open_set.add(*start, self.heuristic(start));
+        g_score.insert(*start, 0);
+
+        while !open_set.is_empty() {
+            // select lowest score in open_set
+            let (current, _score) = open_set.pop().unwrap();
+
+            // finished?
+            if self.at_goal(&current) {
+                let path = Self::reconstruct_path(&current, &came_from);
+                let cost = *g_score.get(&current).unwrap();
+                return (path, cost);
+            }
+
+            let current_g_score = *g_score.get(&current).unwrap();
+
+            for (neighbour, cost_to_neighbour) in self.neighbours(&current) {
+                let tentative_g_score = current_g_score + cost_to_neighbour;
+                let neighbour_g_score = g_score.get(&neighbour);
+
+                if neighbour_g_score.is_none() || tentative_g_score < *neighbour_g_score.unwrap() {
+                    // new best path to neighbour!
+                    came_from.insert(neighbour, (current, cost_to_neighbour));
+                    g_score.insert(neighbour, tentative_g_score);
+                    open_set.add(neighbour, tentative_g_score + self.heuristic(&neighbour));
+                }
+            }
+        }
+
+        panic!("No path to goal");
+    }
+
+    fn reconstruct_path(to: &N, came_from: &HashMap<N, (N, usize)>) -> Vec<(N, usize)> {
+        let mut path: Vec<(N, usize)> = vec![];
+        let mut last = to;
+        loop {
+            match came_from.get(&last) {
+                Some((previous, cost)) => {
+                    path.push((*last, *cost));
+                    last = previous;
+                }
+                None => {
+                    path.reverse();
+                    return path;
+                }
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct OpenSet<N>(BinaryHeap<OpenSetEntry<N>>);
+
+impl<N: Ord> OpenSet<N> {
+    fn new() -> OpenSet<N> {
+        OpenSet(BinaryHeap::new())
+    }
+
+    fn add(&mut self, node: N, cost: usize) {
+        self.0.push(OpenSetEntry { node, cost });
+    }
+
+    fn pop(&mut self) -> Option<(N, usize)> {
+        match self.0.pop() {
+            Some(entry) => Some((entry.node, entry.cost)),
+            None => None,
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+struct OpenSetEntry<N> {
+    node: N,
+    cost: usize,
+}
+
+impl<N: Ord> Ord for OpenSetEntry<N> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other
+            .cost
+            .cmp(&self.cost)
+            .then_with(|| self.node.cmp(&other.node))
+    }
+}
+
+impl<N: Ord> PartialOrd for OpenSetEntry<N> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
---
+++ 2021/day-23-amphipod/src/main.rs
@@ -0,0 +1,541 @@
+pub mod astar;
+pub mod grid;
+
+use astar::AStarInterface;
+use grid::Cell;
+use grid::Coordinate;
+use grid::Grid;
+
+use std::collections::hash_map::DefaultHasher;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+use std::collections::HashMap;
+use std::fmt;
+use std::hash::{Hash, Hasher};
+
+use indoc::indoc;
+use itertools::Itertools;
+
+static INPUT: &str = indoc! {"
+    #############
+    #...........#
+    ###B#B#D#A###
+      #C#A#D#C#  
+      #########  
+"};
+
+fn main() {
+    println!("part 1 result: {:?}", part1(INPUT));
+    // println!("part 2 result: {:?}", part2(INPUT));
+}
+
+#[derive(Debug)]
+struct Map {
+    grid: Grid<Tile>,
+    hallways: Vec<Coordinate>,
+    rooms: BTreeMap<Amphipod, BTreeSet<Coordinate>>,
+    initial_positions: BTreeMap<Coordinate, Amphipod>,
+}
+
+impl Map {
+    fn new(input: &str) -> Map {
+        let raw_input = Self::parse(input);
+
+        // build grid
+        let tiles: Vec<Vec<Tile>> = raw_input
+            .iter()
+            .map(|row| row.iter().map(|(tile, _amphipod)| *tile).collect())
+            .collect();
+        let mut grid = Grid::new(tiles);
+        let rooms = Self::find_rooms(&grid);
+        Self::fix_entrances(&mut grid, &rooms);
+
+        let hallways = grid
+            .iter()
+            .filter(|c| c.value.is_hallway())
+            .map(|c| c.position)
+            .collect();
+
+        // build initial positions map
+        let initial_positions: BTreeMap<Coordinate, Amphipod> = raw_input
+            .into_iter()
+            .enumerate()
+            .flat_map(|(y, row)| {
+                row.into_iter()
+                    .enumerate()
+                    .map(|(x, (_tile, amphipod))| amphipod.map(|v| (Coordinate::new(x, y), v)))
+                    .collect::<Vec<Option<(Coordinate, Amphipod)>>>()
+            })
+            .filter_map(|e| e)
+            .collect();
+
+        Map {
+            grid,
+            hallways,
+            rooms,
+            initial_positions,
+        }
+    }
+
+    fn parse(input: &str) -> Vec<Vec<(Tile, Option<Amphipod>)>> {
+        input
+            .lines()
+            .map(|line| line.chars().map(|c| Tile::parse(&c)).collect())
+            .collect()
+    }
+
+    fn find_rooms(grid: &Grid<Tile>) -> BTreeMap<Amphipod, BTreeSet<Coordinate>> {
+        let rooms: Vec<Coordinate> = grid
+            .iter()
+            .filter(|c| c.value.is_room())
+            .map(|c| c.position)
+            .collect();
+
+        let x_coords: Vec<usize> = rooms.iter().map(|c| c.x).sorted().unique().collect();
+        let amphipod_mappings: BTreeMap<usize, Amphipod> = x_coords
+            .into_iter()
+            .enumerate()
+            .map(|(i, x)| (x, Amphipod::nth(i)))
+            .collect();
+
+        let mut output = BTreeMap::new();
+        for room in rooms {
+            let amphipod = *amphipod_mappings.get(&room.x).unwrap();
+            let v = output.entry(amphipod).or_insert(BTreeSet::new());
+            v.insert(room);
+        }
+        output
+    }
+
+    fn fix_entrances(
+        grid: &mut Grid<Tile>,
+        amphipod_rooms: &BTreeMap<Amphipod, BTreeSet<Coordinate>>,
+    ) {
+        for (_kind, rooms) in amphipod_rooms.iter() {
+            let entrance = Self::find_entrance(grid, rooms);
+            grid.cell_mut(&entrance).value = Tile::Entrance;
+        }
+    }
+
+    fn find_entrance(grid: &Grid<Tile>, rooms: &BTreeSet<Coordinate>) -> Coordinate {
+        let entrances: Vec<Coordinate> = rooms
+            .iter()
+            .flat_map(|c| {
+                grid.neighbours(c)
+                    .iter()
+                    .filter(|n| grid.value(n).is_hallway())
+                    .cloned()
+                    .collect::<Vec<Coordinate>>()
+            })
+            .collect();
+        assert_eq!(1, entrances.len());
+        entrances[0]
+    }
+
+    fn rooms_for(&self, kind: &Amphipod) -> &BTreeSet<Coordinate> {
+        self.rooms.get(kind).unwrap()
+    }
+
+    fn is_room(&self, location: &Coordinate) -> bool {
+        self.grid.value(location).is_room()
+    }
+
+    fn is_room_of_kind(&self, location: &Coordinate, kind: &Amphipod) -> bool {
+        self.rooms_for(kind).contains(location)
+    }
+
+    fn path_between(&self, from: &Coordinate, to: &Coordinate) -> (Vec<Coordinate>, usize) {
+        let mut pathfinding = Pathfinding {
+            goal: to,
+            grid: &self.grid,
+        };
+        let (path_with_costs, total_cost) = pathfinding.shortest_path(from);
+        let path = path_with_costs.into_iter().map(|(l, _c)| l).collect();
+        (path, total_cost)
+    }
+}
+
+#[derive(Debug)]
+struct Pathfinding<'a> {
+    goal: &'a Coordinate,
+    grid: &'a Grid<Tile>,
+}
+
+impl AStarInterface<Coordinate> for Pathfinding<'_> {
+    fn at_goal(&self, node: &Coordinate) -> bool {
+        node == self.goal
+    }
+
+    fn heuristic(&self, from: &Coordinate) -> usize {
+        from.manhattan_distance(self.goal)
+    }
+
+    fn neighbours(&mut self, from: &Coordinate) -> Vec<(Coordinate, usize)> {
+        let mut possible_moves = vec![];
+        for neighbour_location in self.grid.neighbours(from) {
+            let neighbour_tile = self.grid.value(&neighbour_location);
+            match neighbour_tile {
+                Tile::Hallway | Tile::Room => {
+                    // straightforward, move to location at cost of 1 unit
+                    possible_moves.push((neighbour_location, 1));
+                }
+                Tile::Entrance => {
+                    // can't stop at an entrance, need to keep moving!
+                    let second_neighbours = self.neighbours(&neighbour_location);
+                    for (n, cost) in second_neighbours {
+                        // can't end where we started
+                        if n != *from {
+                            // add 1 to cost
+                            possible_moves.push((n, cost + 1));
+                        }
+                    }
+                }
+                Tile::Wall | Tile::Empty => {
+                    // do nothing, impassable
+                }
+            }
+        }
+        possible_moves
+    }
+}
+
+#[derive(Clone, Copy, Debug, Hash, Eq, Ord, PartialEq, PartialOrd)]
+enum Amphipod {
+    Amber,
+    Bronze,
+    Copper,
+    Desert,
+}
+
+impl Amphipod {
+    fn parse(c: &char) -> Self {
+        match c {
+            'A' => Self::Amber,
+            'B' => Self::Bronze,
+            'C' => Self::Copper,
+            'D' => Self::Desert,
+            _ => panic!("Unrecognized char: {}", c),
+        }
+    }
+
+    fn to_char(&self) -> char {
+        match self {
+            Self::Amber => 'A',
+            Self::Bronze => 'B',
+            Self::Copper => 'C',
+            Self::Desert => 'D',
+        }
+    }
+
+    fn nth(i: usize) -> Self {
+        match i {
+            0 => Self::Amber,
+            1 => Self::Bronze,
+            2 => Self::Copper,
+            3 => Self::Desert,
+            _ => panic!("Unrecognized index: {}", i),
+        }
+    }
+
+    fn cost(&self) -> usize {
+        match self {
+            Self::Amber => 1,
+            Self::Bronze => 10,
+            Self::Copper => 100,
+            Self::Desert => 1000,
+        }
+    }
+}
+
+impl fmt::Display for Amphipod {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_char())
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum Tile {
+    Hallway,
+    Entrance,
+    Room,
+    Wall,
+    Empty,
+}
+
+impl Tile {
+    fn parse(c: &char) -> (Self, Option<Amphipod>) {
+        match c {
+            '.' => (Self::Hallway, None),
+            '#' => (Self::Wall, None),
+            ' ' => (Self::Empty, None),
+            'A' | 'B' | 'C' | 'D' => (Self::Room, Some(Amphipod::parse(c))),
+            _ => panic!("Unrecognized char: {}", c),
+        }
+    }
+
+    fn to_char(&self) -> char {
+        match self {
+            Self::Hallway => '.',
+            Self::Entrance => 'e',
+            Self::Room => 'r',
+            Self::Wall => '#',
+            Self::Empty => ' ',
+        }
+    }
+
+    fn is_room(&self) -> bool {
+        match self {
+            Self::Room => true,
+            _ => false,
+        }
+    }
+
+    fn is_hallway(&self) -> bool {
+        match self {
+            Self::Hallway => true,
+            _ => false,
+        }
+    }
+}
+
+impl fmt::Display for Tile {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.to_char())
+    }
+}
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+struct GameState {
+    positions: BTreeMap<Coordinate, Amphipod>,
+}
+
+impl GameState {
+    fn initial(map: &Map) -> GameState {
+        let positions = map.initial_positions.clone();
+        GameState { positions }
+    }
+
+    fn heuristic(&self, map: &Map) -> usize {
+        // TODO consider the case where a Amphipod is at home but blocking something else?
+        // it's probably slowing down astar a bunch to consider those already at home
+        self.positions
+            .iter()
+            .filter(|(c, a)| !map.is_room_of_kind(c, a)) // already in home room
+            .map(|(c, a)| self.cost_to_closest_unoccupied_room(c, a, map))
+            .sum()
+    }
+
+    fn cost_to_closest_unoccupied_room(
+        &self,
+        from: &Coordinate,
+        kind: &Amphipod,
+        map: &Map,
+    ) -> usize {
+        self.unoccupied_rooms_for(kind, map)
+            .iter()
+            .map(|to| {
+                let (_path, distance) = map.path_between(from, to);
+                distance * kind.cost()
+            })
+            .min()
+            .unwrap_or(0)
+    }
+
+    fn is_complete(&self, map: &Map) -> bool {
+        self.positions
+            .iter()
+            .all(|(c, a)| map.is_room_of_kind(c, a))
+    }
+
+    fn available_moves(&self, map: &Map) -> Vec<(GameState, usize)> {
+        self.positions
+            .iter()
+            .flat_map(|(c, a)| self.available_moves_for_amphipod(c, a, map))
+            .collect()
+    }
+
+    fn occupied(&self, location: &Coordinate) -> bool {
+        self.positions.contains_key(location)
+    }
+
+    fn unoccupied_rooms_for<'a>(&self, kind: &Amphipod, map: &'a Map) -> Vec<&'a Coordinate> {
+        map.rooms_for(kind)
+            .iter()
+            .filter(|c| !self.occupied(c))
+            .collect()
+    }
+
+    fn unoccupied_hallways<'a>(&self, map: &'a Map) -> Vec<&'a Coordinate> {
+        map.hallways.iter().filter(|c| !self.occupied(c)).collect()
+    }
+
+    fn available_moves_for_amphipod(
+        &self,
+        position: &Coordinate,
+        kind: &Amphipod,
+        map: &Map,
+    ) -> Vec<(GameState, usize)> {
+        // can always move to a room
+        let mut possible_destinations = self.unoccupied_rooms_for(kind, map);
+
+        if map.is_room(position) {
+            // currently in the wrong room, could move to an open hallways spot as well as a room
+            let mut hallways = self.unoccupied_hallways(map);
+            possible_destinations.append(&mut hallways);
+        }
+
+        let mut available_moves = vec![];
+        for destination in possible_destinations {
+            let (path, distance) = map.path_between(position, destination);
+            let is_obstructed = path.iter().any(|l| self.occupied(l));
+            if !is_obstructed {
+                let cost = distance * kind.cost();
+                let m = (self.state_after_move(position, destination, kind), cost);
+                available_moves.push(m);
+            }
+        }
+        available_moves
+    }
+
+    fn state_after_move(&self, from: &Coordinate, to: &Coordinate, kind: &Amphipod) -> GameState {
+        let mut cloned = self.clone();
+        cloned.apply_move(from, to, kind);
+        cloned
+    }
+
+    fn apply_move(&mut self, from: &Coordinate, to: &Coordinate, kind: &Amphipod) {
+        self.positions.remove(from);
+        self.positions.insert(*to, *kind);
+    }
+
+    fn render(&self, map: &Map) -> String {
+        (0..(map.grid.bounds.height))
+            .map(|y| {
+                (0..(map.grid.bounds.width))
+                    .map(|x| self.render_cell(map.grid.cell(&Coordinate::new(x, y))))
+                    .collect()
+            })
+            .collect::<Vec<String>>()
+            .join("\n")
+    }
+
+    fn render_cell(&self, cell: &Cell<Tile>) -> char {
+        if self.positions.contains_key(&cell.position) {
+            self.positions.get(&cell.position).unwrap().to_char()
+        } else {
+            cell.value.to_char()
+        }
+    }
+}
+
+struct Solver<'a> {
+    map: &'a Map,
+    game_states: HashMap<u64, GameState>,
+}
+
+impl Solver<'_> {
+    fn solve(map: &Map) -> usize {
+        let initial_state = GameState::initial(map);
+        let mut solver = Solver {
+            map: map,
+            game_states: HashMap::new(),
+        };
+        let initial_state = solver.find_or_insert_game_state(initial_state);
+        let (path, cost) = solver.shortest_path(&initial_state);
+        println!("solution (total cost: {})", cost);
+        for (key, cost_for_step) in &path {
+            let state = solver.game_state(key);
+            println!("{}\ncost: {}\n", state.render(map), cost_for_step);
+        }
+        println!(
+            "calculated total cost: {}",
+            path.iter().map(|(_, c)| c).sum::<usize>()
+        );
+        cost
+    }
+
+    fn find_or_insert_game_state(&mut self, game_state: GameState) -> u64 {
+        let key = calculate_hash(&game_state);
+        if !self.game_states.contains_key(&key) {
+            self.game_states.insert(key, game_state);
+        }
+        key
+    }
+
+    fn game_state(&self, hash: &u64) -> &GameState {
+        &self.game_states[hash]
+    }
+}
+
+impl AStarInterface<u64> for Solver<'_> {
+    fn at_goal(&self, node: &u64) -> bool {
+        self.game_state(node).is_complete(self.map)
+    }
+
+    fn heuristic(&self, from: &u64) -> usize {
+        self.game_state(from).heuristic(self.map)
+    }
+
+    fn neighbours(&mut self, from: &u64) -> Vec<(u64, usize)> {
+        self.game_state(from)
+            .available_moves(self.map)
+            .into_iter()
+            .map(|(state, cost)| (self.find_or_insert_game_state(state), cost))
+            .collect()
+    }
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
+fn part1(input: &str) -> usize {
+    let map = Map::new(input);
+    println!("{}", map.grid);
+    Solver::solve(&map)
+}
+
+// fn part2(input: &str) -> usize {
+//     let data = Map::parse(input);
+//     println!("{:?}", data);
+//     data.execute()
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    static EXAMPLE: &str = indoc! {"
+        #############
+        #...........#
+        ###B#C#B#D###
+          #A#D#C#A#  
+          #########  
+    "};
+
+    #[test]
+    fn test_part1_example() {
+        let result = part1(EXAMPLE);
+        assert_eq!(result, 12521);
+    }
+
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(INPUT);
+        assert_eq!(result, 11608);
+    }
+
+    // #[test]
+    // fn test_part2_example() {
+    //     let result = part2(EXAMPLE);
+    //     assert_eq!(result, 0);
+    // }
+
+    // #[test]
+    // fn test_part2_solution() {
+    //     let result = part2(INPUT);
+    //     assert_eq!(result, 0);
+    // }
+}
