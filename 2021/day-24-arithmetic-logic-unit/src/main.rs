d1a375257ca4b8b640a100f88bc34c14ab1465dd
Ken Pratt <ken@kenpratt.net>
2022-01-15 12:16:51 -0800

    day 24 part 1 parsing

--- 2021/day-24-arithmetic-logic-unit/src/main.rs
+++ 2021/day-24-arithmetic-logic-unit/src/main.rs
@@ -1,8 +1,13 @@
 use std::fs;
 
-// use itertools::Itertools;
-// use lazy_static::lazy_static;
-// use regex::Regex;
+use lazy_static::lazy_static;
+use regex::Regex;
+
+lazy_static! {
+    static ref INSTRUCTION_RE: Regex =
+        Regex::new(r"\A([a-z]+) ([a-z])( ([a-z]|\-?[0-9]+))?\z").unwrap();
+    static ref VALUE_RE: Regex = Regex::new(r"\A\-?[0-9]+\z").unwrap();
+}
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
@@ -13,37 +18,97 @@ fn read_input_file() -> String {
     fs::read_to_string("input.txt").expect("Something went wrong reading the file")
 }
 
-#[derive(Debug)]
-struct Data {
-    parts: Vec<Part>,
-}
-
-impl Data {
-    fn parse(input: &str) -> Data {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
-        Data { parts: parts }
-    }
-
-    fn execute(&self) -> usize {
-        0
-    }
+fn parse(input: &str) -> Vec<Instruction> {
+    input.lines().map(|line| Instruction::parse(line)).collect()
 }
 
 #[derive(Debug)]
-struct Part {
-    foo: usize,
+struct Instruction {
+    operation: Operation,
+    first_arg: Register,
+    second_arg: Option<RegisterOrValue>,
 }
 
-impl Part {
-    fn parse(input: &str) -> Part {
-        Part { foo: input.len() }
+impl Instruction {
+    fn parse(input: &str) -> Self {
+        let captures = INSTRUCTION_RE.captures(input).unwrap();
+        let operation = Operation::parse(captures.get(1).unwrap().as_str());
+        let first_arg = Register::parse(captures.get(2).unwrap().as_str());
+        let second_arg = captures.get(4).map(|m| RegisterOrValue::parse(m.as_str()));
+        Self {
+            operation,
+            first_arg,
+            second_arg,
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Operation {
+    Read,
+    Add,
+    Multiply,
+    Divide,
+    Modulo,
+    Equal,
+}
+
+impl Operation {
+    fn parse(input: &str) -> Self {
+        match input {
+            "inp" => Self::Read,
+            "add" => Self::Add,
+            "mul" => Self::Multiply,
+            "div" => Self::Divide,
+            "mod" => Self::Modulo,
+            "eql" => Self::Equal,
+            _ => panic!("Unrecognized operation: {}", input),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum Register {
+    W,
+    X,
+    Y,
+    Z,
+}
+
+impl Register {
+    fn parse(input: &str) -> Self {
+        match input {
+            "w" => Self::W,
+            "x" => Self::X,
+            "y" => Self::Y,
+            "z" => Self::Z,
+            _ => panic!("Unrecognized register: {}", input),
+        }
+    }
+}
+
+#[derive(Debug)]
+enum RegisterOrValue {
+    Register(Register),
+    Value(isize),
+}
+
+impl RegisterOrValue {
+    fn parse(input: &str) -> Self {
+        if VALUE_RE.is_match(input) {
+            let v: isize = input.parse().unwrap();
+            Self::Value(v)
+        } else {
+            let r = Register::parse(input);
+            Self::Register(r)
+        }
     }
 }
 
 fn part1(input: &str) -> usize {
-    let data = Data::parse(input);
-    println!("{:?}", data);
-    data.execute()
+    let instructions = parse(input);
+    println!("{:?}", instructions);
+    0
 }
 
 // fn part2(input: &str) -> usize {
@@ -59,12 +124,46 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
+        inp x
+        mul x -1
+    "};
+
+    static EXAMPLE2: &str = indoc! {"
+        inp z
+        inp x
+        mul z 3
+        eql z x
+    "};
+
+    static EXAMPLE3: &str = indoc! {"
+        inp w
+        add z w
+        mod z 2
+        div w 2
+        add y w
+        mod y 2
+        div w 2
+        add x w
+        mod x 2
+        div w 2
+        mod w 2
     "};
 
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_part1_example2() {
+        let result = part1(EXAMPLE2);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_part1_example3() {
+        let result = part1(EXAMPLE3);
         assert_eq!(result, 0);
     }
 
