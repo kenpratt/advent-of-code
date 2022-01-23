c20bf6fb2e1da07d30decf2ee22c29315553f8ef
Ken Pratt <ken@kenpratt.net>
2022-01-22 19:46:37 -0800

    day 25 part 1

--- 2021/day-25-sea-cucumber/src/main.rs
+++ 2021/day-25-sea-cucumber/src/main.rs
@@ -1,8 +1,6 @@
+use std::collections::HashMap;
+use std::fmt;
 use std::fs;
-
-// use itertools::Itertools;
-// use lazy_static::lazy_static;
-// use regex::Regex;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
@@ -14,36 +12,141 @@ fn read_input_file() -> String {
 }
 
 #[derive(Debug)]
-struct Data {
-    parts: Vec<Part>,
+struct Map {
+    cucumbers: HashMap<Coordinate, Facing>,
+    width: usize,
+    height: usize,
 }
 
-impl Data {
-    fn parse(input: &str) -> Data {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
-        Data { parts: parts }
+impl Map {
+    fn parse(input: &str) -> Map {
+        let width = input.lines().next().unwrap().chars().count();
+        let height = input.lines().count();
+        let mut cucumbers = HashMap::new();
+        for (y, line) in input.lines().enumerate() {
+            for (x, c) in line.chars().enumerate() {
+                match Self::parse_char(&c) {
+                    Some(facing) => {
+                        cucumbers.insert(Coordinate::new(x, y), facing);
+                    }
+                    None => {}
+                };
+            }
+        }
+        Map {
+            cucumbers,
+            width,
+            height,
+        }
     }
 
-    fn execute(&self) -> usize {
-        0
+    fn parse_char(c: &char) -> Option<Facing> {
+        match c {
+            '.' => None,
+            '>' => Some(Facing::East),
+            'v' => Some(Facing::South),
+            _ => panic!("Unknown char: {:?}", c),
+        }
+    }
+
+    fn run(&mut self) -> usize {
+        let mut ticks = 0;
+        loop {
+            ticks += 1;
+            let num_moves = self.tick();
+            // println!("map after tick {}:\n{}", ticks, self);
+
+            if num_moves == 0 {
+                break;
+            }
+        }
+        ticks
+    }
+
+    fn tick(&mut self) -> usize {
+        self.move_cucumbers(&Facing::East) + self.move_cucumbers(&Facing::South)
+    }
+
+    fn move_cucumbers(&mut self, facing_to_move: &Facing) -> usize {
+        let mut new_cucumbers = HashMap::new();
+        let mut num_moves = 0;
+
+        for (coord, facing) in &self.cucumbers {
+            if facing == facing_to_move {
+                // try_to_move
+                let new_position = self.try_to_move(coord, facing);
+                if new_position != *coord {
+                    num_moves += 1;
+                }
+                new_cucumbers.insert(new_position, *facing);
+            } else {
+                // not your turn
+                new_cucumbers.insert(*coord, *facing);
+            }
+        }
+
+        self.cucumbers = new_cucumbers;
+        num_moves
+    }
+
+    fn try_to_move(&self, position: &Coordinate, facing: &Facing) -> Coordinate {
+        let neighbour_pos = self.neighbour_position(position, facing);
+        if self.cucumbers.contains_key(&neighbour_pos) {
+            // occupied, stay in current position
+            *position
+        } else {
+            // empty, move to new position
+            neighbour_pos
+        }
+    }
+
+    fn neighbour_position(&self, coord: &Coordinate, facing: &Facing) -> Coordinate {
+        match facing {
+            Facing::East => Coordinate::new((coord.x + 1) % self.width, coord.y),
+            Facing::South => Coordinate::new(coord.x, (coord.y + 1) % self.height),
+        }
     }
 }
 
-#[derive(Debug)]
-struct Part {
-    foo: usize,
+impl fmt::Display for Map {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let coord = Coordinate::new(x, y);
+                match self.cucumbers.get(&coord) {
+                    Some(Facing::East) => write!(f, ">")?,
+                    Some(Facing::South) => write!(f, "v")?,
+                    None => write!(f, ".")?,
+                }
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
 }
 
-impl Part {
-    fn parse(input: &str) -> Part {
-        Part { foo: input.len() }
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+struct Coordinate {
+    x: usize,
+    y: usize,
+}
+
+impl Coordinate {
+    fn new(x: usize, y: usize) -> Coordinate {
+        Coordinate { x, y }
     }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+enum Facing {
+    East,
+    South,
 }
 
 fn part1(input: &str) -> usize {
-    let data = Data::parse(input);
-    println!("{:?}", data);
-    data.execute()
+    let mut map = Map::parse(input);
+    println!("initial:\n{}", map);
+    map.run()
 }
 
 // fn part2(input: &str) -> usize {
@@ -59,20 +162,28 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
+        v...>>.vv>
+        .vv>>.vv..
+        >>.>v>...v
+        >>v>>.>.v.
+        v>v.vv.v..
+        >.>>..v...
+        .vv..>.>v.
+        v.v..>>v.v
+        ....v..v.>
     "};
 
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 58);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 308);
+    }
 
     // #[test]
     // fn test_part2_example1() {
