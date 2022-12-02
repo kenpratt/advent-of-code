d04a7691aec763943d278e2d6565fbb5c0f08105
Ken Pratt <ken@kenpratt.net>
2022-12-01 20:23:38 -0800

    day 1 part 1

--- 2022/day-01-calorie-counting/src/main.rs
+++ 2022/day-01-calorie-counting/src/main.rs
@@ -14,40 +14,54 @@ fn read_input_file() -> String {
 }
 
 #[derive(Debug)]
-struct Data {
-    parts: Vec<Part>,
+struct Elves {
+    inventories: Vec<Inventory>,
 }
 
-impl Data {
-    fn parse(input: &str) -> Data {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
-        Data { parts: parts }
+impl Elves {
+    fn parse(input: &str) -> Elves {
+        let inventories = input
+            .split("\n\n")
+            .map(|part| Inventory::parse(part))
+            .collect();
+        Elves {
+            inventories: inventories,
+        }
     }
 
     fn execute(&self) -> usize {
-        0
+        self.inventories
+            .iter()
+            .map(|inv| inv.total_calories())
+            .max()
+            .unwrap()
     }
 }
 
 #[derive(Debug)]
-struct Part {
-    foo: usize,
+struct Inventory {
+    items: Vec<usize>,
 }
 
-impl Part {
-    fn parse(input: &str) -> Part {
-        Part { foo: input.len() }
+impl Inventory {
+    fn parse(input: &str) -> Inventory {
+        let items = input.lines().map(|line| line.parse().unwrap()).collect();
+        Inventory { items: items }
+    }
+
+    fn total_calories(&self) -> usize {
+        self.items.iter().sum()
     }
 }
 
 fn part1(input: &str) -> usize {
-    let data = Data::parse(input);
+    let data = Elves::parse(input);
     println!("{:?}", data);
     data.execute()
 }
 
 // fn part2(input: &str) -> usize {
-//     let data = Data::parse(input);
+//     let data = Elves::parse(input);
 //     println!("{:?}", data);
 //     data.execute()
 // }
@@ -59,20 +73,33 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
+        1000
+        2000
+        3000
+        
+        4000
+        
+        5000
+        6000
+        
+        7000
+        8000
+        9000
+        
+        10000
     "};
 
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 24000);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 66186);
+    }
 
     // #[test]
     // fn test_part2_example1() {
