16ff3f34ddc715fdc755aeea04204d9f88af3b94
Ken Pratt <ken@kenpratt.net>
2022-12-05 10:46:12 -0800

    day 4 part 1

--- 2022/day-04-camp-cleanup/src/main.rs
+++ 2022/day-04-camp-cleanup/src/main.rs
@@ -1,8 +1,8 @@
 use std::fs;
+use std::ops::RangeInclusive;
 
-// use itertools::Itertools;
-// use lazy_static::lazy_static;
-// use regex::Regex;
+use lazy_static::lazy_static;
+use regex::Regex;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
@@ -13,37 +13,41 @@ fn read_input_file() -> String {
     fs::read_to_string("input.txt").expect("Something went wrong reading the file")
 }
 
-#[derive(Debug)]
-struct Data {
-    parts: Vec<Part>,
+type Pair = (RangeInclusive<usize>, RangeInclusive<usize>);
+
+fn parse_pairs(input: &str) -> Vec<Pair> {
+    input.lines().map(|line| parse_pair(line)).collect()
 }
 
-impl Data {
-    fn parse(input: &str) -> Data {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
-        Data { parts: parts }
+fn parse_pair(input: &str) -> Pair {
+    lazy_static! {
+        static ref PAIR_RE: Regex = Regex::new(r"\A(\d+)\-(\d+),(\d+)\-(\d+)\z").unwrap();
     }
-
-    fn execute(&self) -> usize {
-        0
-    }
+    let caps = PAIR_RE.captures(input).unwrap();
+    let nums: Vec<usize> = caps
+        .iter()
+        .skip(1)
+        .map(|s| s.unwrap().as_str().parse::<usize>().unwrap())
+        .collect();
+    let pair = (nums[0]..=nums[1], nums[2]..=nums[3]);
+    println!("{:?} -> {:?} -> {:?}", input, nums, pair);
+    pair
 }
 
-#[derive(Debug)]
-struct Part {
-    foo: usize,
-}
-
-impl Part {
-    fn parse(input: &str) -> Part {
-        Part { foo: input.len() }
-    }
+fn contains_range<U>(range: &RangeInclusive<U>, other: &RangeInclusive<U>) -> bool
+where
+    U: PartialOrd,
+{
+    range.contains(other.start()) && range.contains(other.end())
 }
 
 fn part1(input: &str) -> usize {
-    let data = Data::parse(input);
-    println!("{:?}", data);
-    data.execute()
+    let pairs = parse_pairs(input);
+    println!("{:?}", pairs);
+    pairs
+        .into_iter()
+        .filter(|(a, b)| contains_range(a, b) || contains_range(b, a))
+        .count()
 }
 
 // fn part2(input: &str) -> usize {
@@ -59,20 +63,25 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
+        2-4,6-8
+        2-3,4-5
+        5-7,7-9
+        2-8,3-7
+        6-6,4-6
+        2-6,4-8
     "};
 
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 2);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 599);
+    }
 
     // #[test]
     // fn test_part2_example1() {
