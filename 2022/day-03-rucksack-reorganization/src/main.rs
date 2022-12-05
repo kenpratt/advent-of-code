d2ae3cbd2c2b3f2d1019daa51476d459d64816e6
Ken Pratt <ken@kenpratt.net>
2022-12-04 22:17:07 -0800

    day 3 part 2

--- 2022/day-03-rucksack-reorganization/src/main.rs
+++ 2022/day-03-rucksack-reorganization/src/main.rs
@@ -1,89 +1,80 @@
+use std::collections::HashSet;
 use std::fs;
 
-use std::collections::HashSet;
+use itertools::Itertools;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
-    // println!("part 2 result: {:?}", part2(&read_input_file()));
+    println!("part 2 result: {:?}", part2(&read_input_file()));
 }
 
 fn read_input_file() -> String {
     fs::read_to_string("input.txt").expect("Something went wrong reading the file")
 }
 
-#[derive(Debug)]
-struct RucksackList {
-    rucksacks: Vec<Rucksack>,
+fn common_char(strings: &[&str]) -> char {
+    let in_common = strings
+        .iter()
+        .map(|s| s.chars().collect::<HashSet<char>>())
+        .reduce(|acc, set| acc.intersection(&set).cloned().collect())
+        .unwrap();
+    assert_eq!(in_common.len(), 1);
+    in_common.into_iter().next().unwrap()
 }
 
-impl RucksackList {
-    fn parse(input: &str) -> RucksackList {
-        let rucksacks = input.lines().map(|line| Rucksack::parse(line)).collect();
-        RucksackList { rucksacks }
-    }
-
-    fn sum_priorites_for_common_items(&self) -> usize {
-        self.rucksacks
-            .iter()
-            .map(|r| r.common_item_priority())
-            .sum()
-    }
-}
-
-#[derive(Debug)]
-struct Rucksack {
-    first_compartment: HashSet<char>,
-    second_compartment: HashSet<char>,
-}
-
-impl Rucksack {
-    fn parse(input: &str) -> Rucksack {
-        assert!(input.len() % 2 == 0);
-        let midpoint = input.len() / 2;
-        let first_compartment = input[0..midpoint].chars().collect();
-        let second_compartment = input[midpoint..].chars().collect();
-        Rucksack {
-            first_compartment,
-            second_compartment,
-        }
-    }
-
-    fn common_items(&self) -> HashSet<&char> {
-        self.first_compartment
-            .intersection(&self.second_compartment)
-            .collect()
-    }
-
-    fn common_item_priority(&self) -> usize {
-        let items = self.common_items();
-        assert_eq!(items.len(), 1);
-        let item = items.iter().next().unwrap();
-        Self::priority(item)
-    }
-
-    fn priority(item: &char) -> usize {
-        let n = *item as usize;
-        match n {
-            // a..z
-            97..=122 => (n - 97) + 1, // a=1
-            // A..Z
-            65..=90 => (n - 65) + 27, // A=27
-            _ => panic!("Unexpected char: {}", item),
-        }
+fn item_priority(item: &char) -> usize {
+    let n = *item as usize;
+    match n {
+        // a..z
+        97..=122 => (n - 97) + 1, // a=1
+        // A..Z
+        65..=90 => (n - 65) + 27, // A=27
+        _ => panic!("Unexpected char: {}", item),
     }
 }
 
 fn part1(input: &str) -> usize {
-    let rucksacks = RucksackList::parse(input);
-    println!("{:?}", rucksacks);
-    rucksacks.sum_priorites_for_common_items()
+    let in_common: Vec<char> = input
+        .lines()
+        .map(|line| common_char_in_rucksack_compartments(line))
+        .collect();
+    println!("in_common: {:?}", in_common);
+
+    let priorities: Vec<usize> = in_common.iter().map(|item| item_priority(item)).collect();
+    println!("priorities: {:?}", priorities);
+
+    let sum = priorities.iter().sum();
+    println!("sum: {:?}", sum);
+
+    sum
 }
 
-// fn part2(input: &str) -> usize {
-//     let rucksacks = RucksackList::parseinput);
-//     println!("{:?}", rucksacks);
-//     rucksacks.sum_priorites_for_common_items()
-// }
+fn common_char_in_rucksack_compartments(input: &str) -> char {
+    assert!(input.len() % 2 == 0);
+    let midpoint = input.len() / 2;
+    let first_compartment = &input[0..midpoint];
+    let second_compartment = &input[midpoint..];
+    common_char(&[first_compartment, second_compartment])
+}
+
+fn part2(input: &str) -> usize {
+    let in_common: Vec<char> = input
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .map(|chunk| chunk.collect::<Vec<&str>>())
+        .map(|chunk| common_char(&chunk))
+        .collect();
+    println!("in_common: {:?}", in_common);
+
+    let priorities: Vec<usize> = in_common.iter().map(|item| item_priority(item)).collect();
+    println!("priorities: {:?}", priorities);
+
+    let sum = priorities.iter().sum();
+    println!("sum: {:?}", sum);
+
+    sum
+}
 
 #[cfg(test)]
 mod tests {
@@ -112,15 +103,15 @@ mod tests {
         assert_eq!(result, 7597);
     }
 
-    // #[test]
-    // fn test_part2_example1() {
-    //     let result = part2(EXAMPLE1);
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_example1() {
+        let result = part2(EXAMPLE1);
+        assert_eq!(result, 70);
+    }
 
-    // #[test]
-    // fn test_part2_solution() {
-    //     let result = part2(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_solution() {
+        let result = part2(&read_input_file());
+        assert_eq!(result, 2607);
+    }
 }
