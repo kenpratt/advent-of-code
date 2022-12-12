154a70f560871c5399632b69ced5e9ca287796a4
Ken Pratt <ken@kenpratt.net>
2022-12-12 10:33:22 -0800

    day 12 part 1

---
+++ 2022/day-12-hill-climbing-algorithm/src/grid.rs
@@ -0,0 +1,172 @@
+use std::fmt;
+
+#[derive(Debug, PartialEq)]
+pub enum Direction {
+    Up,
+    Left,
+    Right,
+    Down,
+}
+
+static DIRECTIONS: &'static [Direction] = &[
+    Direction::Up,
+    Direction::Left,
+    Direction::Right,
+    Direction::Down,
+];
+
+#[derive(Clone, Copy, Debug, Hash, Eq, Ord, PartialEq, PartialOrd)]
+pub struct Coordinate {
+    pub x: usize,
+    pub y: usize,
+}
+
+impl Coordinate {
+    pub fn new(x: usize, y: usize) -> Coordinate {
+        Coordinate { x: x, y: y }
+    }
+
+    pub fn manhattan_distance(&self, other: &Coordinate) -> usize {
+        abs_diff(self.x, other.x) + abs_diff(self.y, other.y)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Bounds {
+    pub width: usize,
+    pub height: usize,
+}
+
+fn coordinate_to_index(pos: &Coordinate, bounds: &Bounds) -> usize {
+    (pos.y * bounds.width) + pos.x
+}
+
+fn index_to_coordinate(index: &usize, bounds: &Bounds) -> Coordinate {
+    Coordinate::new(index % bounds.width, index / bounds.width)
+}
+
+fn neighbours(pos: &Coordinate, bounds: &Bounds) -> Vec<Coordinate> {
+    DIRECTIONS
+        .iter()
+        .filter_map(|d| neighbour(pos, d, bounds))
+        .collect()
+}
+
+fn neighbour(pos: &Coordinate, direction: &Direction, bounds: &Bounds) -> Option<Coordinate> {
+    match direction {
+        Direction::Up => {
+            if pos.y > 0 {
+                Some(Coordinate::new(pos.x, pos.y - 1))
+            } else {
+                None
+            }
+        }
+        Direction::Down => {
+            if pos.y < (bounds.height - 1) {
+                Some(Coordinate::new(pos.x, pos.y + 1))
+            } else {
+                None
+            }
+        }
+        Direction::Left => {
+            if pos.x > 0 {
+                Some(Coordinate::new(pos.x - 1, pos.y))
+            } else {
+                None
+            }
+        }
+        Direction::Right => {
+            if pos.x < (bounds.width - 1) {
+                Some(Coordinate::new(pos.x + 1, pos.y))
+            } else {
+                None
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Grid<T> {
+    cells: Vec<Cell<T>>,
+    pub bounds: Bounds,
+}
+
+impl<T> Grid<T> {
+    pub fn new(values: Vec<Vec<T>>) -> Grid<T> {
+        let height = values.len();
+        let width = values[0].len();
+        if values.iter().any(|row| row.len() != width) {
+            panic!("Grid data is not rectangular");
+        }
+        let bounds = Bounds {
+            height: height,
+            width: width,
+        };
+        let cells = values
+            .into_iter()
+            .flatten()
+            .enumerate()
+            .map(|(index, value)| Cell {
+                position: index_to_coordinate(&index, &bounds),
+                value: value,
+            })
+            .collect();
+        Grid { cells, bounds }
+    }
+
+    pub fn len(&self) -> usize {
+        self.cells.len()
+    }
+
+    pub fn cell(&self, pos: &Coordinate) -> &Cell<T> {
+        let index = coordinate_to_index(pos, &self.bounds);
+        &self.cells[index]
+    }
+
+    pub fn cell_mut(&mut self, pos: &Coordinate) -> &mut Cell<T> {
+        let index = coordinate_to_index(pos, &self.bounds);
+        &mut self.cells[index]
+    }
+
+    pub fn value(&self, pos: &Coordinate) -> &T {
+        &self.cell(pos).value
+    }
+
+    pub fn neighbours(&self, pos: &Coordinate) -> Vec<Coordinate> {
+        neighbours(pos, &self.bounds)
+    }
+
+    pub fn iter(&self) -> std::slice::Iter<Cell<T>> {
+        self.cells.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> std::slice::IterMut<Cell<T>> {
+        self.cells.iter_mut()
+    }
+}
+
+impl<T: fmt::Display> fmt::Display for Grid<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for y in 0..self.bounds.height {
+            for x in 0..self.bounds.width {
+                write!(f, "{}", self.value(&Coordinate::new(x, y))).unwrap();
+            }
+            write!(f, "\n").unwrap();
+        }
+        Ok(())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Cell<T> {
+    pub position: Coordinate,
+    pub value: T,
+}
+
+fn abs_diff(a: usize, b: usize) -> usize {
+    if a >= b {
+        a - b
+    } else {
+        b - a
+    }
+}
--- 2022/day-12-hill-climbing-algorithm/src/main.rs
+++ 2022/day-12-hill-climbing-algorithm/src/main.rs
@@ -1,3 +1,9 @@
+pub mod astar;
+pub mod grid;
+
+use astar::AStarInterface;
+use grid::*;
+
 use std::fs;
 
 // use itertools::Itertools;
@@ -14,36 +20,57 @@ fn read_input_file() -> String {
 }
 
 #[derive(Debug)]
-struct Data {
-    parts: Vec<Part>,
+struct Solver {
+    map: Grid<char>,
+    start: Coordinate,
+    end: Coordinate,
 }
 
-impl Data {
+impl Solver {
     fn parse(input: &str) -> Self {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
-        Self { parts: parts }
+        let mut map = Grid::new(input.lines().map(|line| line.chars().collect()).collect());
+
+        let start = Self::find_and_replace(&mut map, 'S', 'a');
+        let end = Self::find_and_replace(&mut map, 'E', 'z');
+
+        Self { map, start, end }
     }
 
-    fn execute(&self) -> usize {
-        0
+    fn find_and_replace(map: &mut Grid<char>, find: char, replace: char) -> Coordinate {
+        let cell = map.iter_mut().find(|cell| cell.value == find).unwrap();
+        cell.value = replace;
+        cell.position
+    }
+
+    fn run(&self) -> usize {
+        let (_path, length) = self.shortest_path(&self.start, true);
+        length
     }
 }
 
-#[derive(Debug)]
-struct Part {
-    foo: usize,
-}
+impl AStarInterface<Coordinate> for Solver {
+    fn at_goal(&self, node: &Coordinate) -> bool {
+        node == &self.end
+    }
 
-impl Part {
-    fn parse(input: &str) -> Self {
-        Self { foo: input.len() }
+    fn heuristic(&self, from: &Coordinate) -> usize {
+        from.manhattan_distance(&self.end)
+    }
+
+    fn neighbours(&self, from: &Coordinate) -> Vec<(Coordinate, usize)> {
+        let h = *self.map.value(from) as isize;
+        self.map
+            .neighbours(from)
+            .iter()
+            .filter(|n| (*self.map.value(n) as isize - h) <= 1)
+            .map(|n| (*n, 1))
+            .collect()
     }
 }
 
 fn part1(input: &str) -> usize {
-    let data = Data::parse(input);
-    dbg!(&data);
-    data.execute()
+    let solver = Solver::parse(input);
+    solver.run()
 }
 
 // fn part2(input: &str) -> usize {
@@ -59,20 +86,24 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE1: &str = indoc! {"
-        foo
+        Sabqponm
+        abcryxxl
+        accszExk
+        acctuvwj
+        abdefghi
     "};
 
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 31);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 472);
+    }
 
     // #[test]
     // fn test_part2_example1() {
