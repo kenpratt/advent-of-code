d513cc9fae00e73f3054576ee6c41db653af81fa
Ken Pratt <ken@kenpratt.net>
2022-12-18 09:35:03 -0800

    day 13 part 1

--- 2022/day-13-distress-signal/src/main.rs
+++ 2022/day-13-distress-signal/src/main.rs
@@ -1,8 +1,7 @@
+use std::cmp::Ordering;
 use std::fs;
 
-// use itertools::Itertools;
-// use lazy_static::lazy_static;
-// use regex::Regex;
+use itertools::Itertools;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
@@ -14,36 +13,170 @@ fn read_input_file() -> String {
 }
 
 #[derive(Debug)]
-struct Data {
-    parts: Vec<Part>,
+enum Token {
+    OpenParen,
+    CloseParen,
+    Number(u32),
 }
 
-impl Data {
-    fn parse(input: &str) -> Self {
-        let parts = input.lines().map(|line| Part::parse(line)).collect();
-        Self { parts: parts }
+const RADIX: u32 = 10;
+
+struct Tokenizer {
+    curr_num: Option<u32>,
+    result: Vec<Token>,
+}
+
+impl Tokenizer {
+    fn run(input: &str) -> Vec<Token> {
+        let mut tokenizer = Self {
+            curr_num: None,
+            result: vec![],
+        };
+        tokenizer.run_(input);
+        tokenizer.result
     }
 
-    fn execute(&self) -> usize {
-        0
+    fn run_(&mut self, input: &str) {
+        use Token::*;
+        for c in input.chars() {
+            match c {
+                '[' => self.result.push(OpenParen),
+                ',' => self.close_number(),
+                ']' => {
+                    self.close_number();
+                    self.result.push(CloseParen)
+                }
+                '0'..='9' => self.add_number(c),
+                _ => panic!("Unexpected char: {}", c),
+            }
+        }
+    }
+
+    fn add_number(&mut self, c: char) {
+        let d = c.to_digit(RADIX).unwrap();
+        self.curr_num = match self.curr_num {
+            None => Some(d),
+            Some(x) => Some(x * 10 + d),
+        }
+    }
+
+    fn close_number(&mut self) {
+        use Token::*;
+        if self.curr_num.is_some() {
+            self.result.push(Number(self.curr_num.unwrap()));
+        }
+        self.curr_num = None;
+    }
+}
+
+type NodeList = Vec<Node>;
+
+#[derive(Debug, Eq, PartialEq)]
+enum Node {
+    List(NodeList),
+    Number(u32),
+}
+
+impl Node {
+    fn parse(input: &str) -> Self {
+        let tokens = Tokenizer::run(input);
+        let mut token_iter = tokens.into_iter();
+        let res = Self::parse_node(&mut token_iter).unwrap();
+        assert!(token_iter.next().is_none());
+        res
+    }
+
+    fn parse_node<I: Iterator<Item = Token>>(tokens: &mut I) -> Option<Self> {
+        match tokens.next().unwrap() {
+            Token::OpenParen => {
+                let mut res = vec![];
+                loop {
+                    match Self::parse_node(tokens) {
+                        Some(n) => res.push(n),
+                        None => break,
+                    };
+                }
+                Some(Node::List(res))
+            }
+            Token::CloseParen => None,
+            Token::Number(n) => Some(Node::Number(n)),
+        }
+    }
+}
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> Ordering {
+        use itertools::EitherOrBoth::{Both, Left, Right};
+        use Node::*;
+        use Ordering::*;
+
+        match (self, other) {
+            (Number(l), Number(r)) => l.cmp(r),
+            (List(l), List(r)) => l
+                .iter()
+                .zip_longest(r)
+                .find_map(|e| match e {
+                    Both(li, ri) => match li.cmp(ri) {
+                        Equal => None, // keep going
+                        x => Some(x),  // stop
+                    },
+                    Left(_) => Some(Greater), // stop
+                    Right(_) => Some(Less),   // stop
+                })
+                .unwrap_or(Equal), // both ran out of elements
+            (Number(l), List(_)) => List(vec![Number(*l)]).cmp(other),
+            (List(_), Number(r)) => self.cmp(&List(vec![Number(*r)])),
+        }
+    }
+}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
 #[derive(Debug)]
-struct Part {
-    foo: usize,
+struct PacketPair {
+    left: NodeList,
+    right: NodeList,
 }
 
-impl Part {
+impl PacketPair {
     fn parse(input: &str) -> Self {
-        Self { foo: input.len() }
+        let lines: Vec<&str> = input.lines().collect();
+        assert_eq!(lines.len(), 2);
+        let left = Self::parse_packet(lines[0]);
+        let right = Self::parse_packet(lines[1]);
+        Self { left, right }
+    }
+
+    fn parse_packet(input: &str) -> NodeList {
+        match Node::parse(input) {
+            Node::List(l) => l,
+            Node::Number(_) => panic!("Not expecting a number: {}", input),
+        }
+    }
+
+    fn in_order(&self) -> bool {
+        self.left < self.right
     }
 }
 
+fn parse_packet_pairs(input: &str) -> Vec<PacketPair> {
+    input
+        .split("\n\n")
+        .map(|chunk| PacketPair::parse(chunk))
+        .collect()
+}
+
 fn part1(input: &str) -> usize {
-    let data = Data::parse(input);
-    dbg!(&data);
-    data.execute()
+    let pairs = parse_packet_pairs(input);
+    pairs
+        .iter()
+        .enumerate()
+        .filter(|(_, p)| p.in_order())
+        .map(|(i, _)| i + 1)
+        .sum()
 }
 
 // fn part2(input: &str) -> usize {
@@ -87,14 +220,14 @@ mod tests {
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 13);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 5852);
+    }
 
     // #[test]
     // fn test_part2_example1() {
