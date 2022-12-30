2ccb742442b791b4d684cc30ddeaf08fb6fa4fd9
Ken Pratt <ken@kenpratt.net>
2022-12-30 13:57:00 -0800

    day 18 base

---
+++ 2022/day-18-boiling-boulders/src/main.rs
@@ -0,0 +1,88 @@
+use std::fs;
+
+// use itertools::Itertools;
+// use lazy_static::lazy_static;
+// use regex::Regex;
+
+fn main() {
+    println!("part 1 result: {:?}", part1(&read_input_file()));
+    // println!("part 2 result: {:?}", part2(&read_input_file()));
+}
+
+fn read_input_file() -> String {
+    fs::read_to_string("input.txt").expect("Something went wrong reading the file")
+}
+
+#[derive(Debug)]
+struct Data {
+    parts: Vec<Part>,
+}
+
+impl Data {
+    fn parse(input: &str) -> Self {
+        let parts = input.lines().map(|line| Part::parse(line)).collect();
+        Self { parts: parts }
+    }
+
+    fn execute(&self) -> usize {
+        0
+    }
+}
+
+#[derive(Debug)]
+struct Part {
+    foo: usize,
+}
+
+impl Part {
+    fn parse(input: &str) -> Self {
+        Self { foo: input.len() }
+    }
+}
+
+fn part1(input: &str) -> usize {
+    let data = Data::parse(input);
+    dbg!(&data);
+    data.execute()
+}
+
+// fn part2(input: &str) -> usize {
+//     let data = Data::parse(input);
+//     dbg!(&data);
+//     data.execute()
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use indoc::indoc;
+
+    static EXAMPLE1: &str = indoc! {"
+        foo
+    "};
+
+    #[test]
+    fn test_part1_example1() {
+        let result = part1(EXAMPLE1);
+        assert_eq!(result, 0);
+    }
+
+    // #[test]
+    // fn test_part1_solution() {
+    //     let result = part1(&read_input_file());
+    //     assert_eq!(result, 0);
+    // }
+
+    // #[test]
+    // fn test_part2_example1() {
+    //     let result = part2(EXAMPLE1);
+    //     assert_eq!(result, 0);
+    // }
+
+    // #[test]
+    // fn test_part2_solution() {
+    //     let result = part2(&read_input_file());
+    //     assert_eq!(result, 0);
+    // }
+}
