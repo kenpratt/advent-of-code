0e355807edf80527879f9f726ebe9e825e582805
Ken Pratt <ken@kenpratt.net>
2023-01-12 20:12:24 -0800

    day 21 part 2

--- 2022/day-21-monkey-math/src/main.rs
+++ 2022/day-21-monkey-math/src/main.rs
@@ -8,7 +8,7 @@ use regex::Regex;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
-    // println!("part 2 result: {:?}", part2(&read_input_file()));
+    println!("part 2 result: {:?}", part2(&read_input_file()));
 }
 
 fn read_input_file() -> String {
@@ -44,6 +44,7 @@ impl Monkeys {
 enum Job {
     Value(isize),
     Operation(Operator, String, String),
+    Variable,
 }
 
 impl Job {
@@ -84,6 +85,7 @@ enum Operator {
     Minus,
     Multiply,
     Divide,
+    Equals,
 }
 
 impl Operator {
@@ -105,14 +107,15 @@ impl Operator {
             Minus => left - right,
             Multiply => left * right,
             Divide => left / right,
+            Equals => panic!("Should not directly apply equals"),
         }
     }
 }
 
 #[derive(Debug)]
 struct Stack {
-    main: Vec<StackEntry>,
-    values: Vec<isize>,
+    main: Vec<MainStackEntry>,
+    values: Vec<ValueStackEntry>,
 }
 
 impl Stack {
@@ -124,15 +127,13 @@ impl Stack {
     }
 
     fn run(&mut self, start_name: &String, jobs: &HashMap<String, Job>) -> isize {
-        use StackEntry::*;
+        use MainStackEntry::*;
 
         // start with just one name entry on the stack, to expand
         self.main.push(Name(start_name.clone()));
 
         while !self.main.is_empty() {
-            // println!("\nbefore: {:?}", self);
             let entry = self.main.pop().unwrap();
-            // println!("applying {:?}", entry);
             match entry {
                 Name(name) => {
                     // expansion, add job to stack
@@ -140,28 +141,36 @@ impl Stack {
                     self.push_job(job)
                 }
                 Operator(op) => {
-                    // apply operation an push result onto stack
+                    // apply operation, getting operands from values stack, and
+                    // push result onto pending values stack
                     let left = self.values.pop().unwrap();
                     let right = self.values.pop().unwrap();
-                    let result = op.apply(left, right);
-                    self.main.push(Value(result));
+                    let result = ValueStackEntry::apply_operator(op, left, right);
+                    self.values.push(result);
                 }
                 Value(v) => {
                     // move this to the pending values stack
                     // for the operator to apply
-                    self.values.push(v);
+                    self.values.push(ValueStackEntry::Value(v));
+                }
+                Variable => {
+                    // move this to the pending values stack
+                    // for the operator to apply
+                    self.values.push(ValueStackEntry::Variable);
                 }
             }
-            // println!("after: {:?}", self);
         }
 
         // return last value on the stack
         assert_eq!(1, self.values.len());
-        self.values.pop().unwrap()
+        match self.values.pop().unwrap() {
+            ValueStackEntry::Value(v) => v,
+            _ => panic!("Expected last item on the values stack to be a value"),
+        }
     }
 
     fn push_job(&mut self, job: &Job) {
-        use StackEntry::*;
+        use MainStackEntry::*;
 
         match job {
             Job::Operation(op, l, r) => {
@@ -172,27 +181,121 @@ impl Stack {
             Job::Value(v) => {
                 self.main.push(Value(*v));
             }
+            Job::Variable => {
+                self.main.push(Variable);
+            }
         }
     }
 }
 
 #[derive(Debug)]
-enum StackEntry {
+enum MainStackEntry {
     Name(String),
     Operator(Operator),
     Value(isize),
+    Variable,
+}
+
+#[derive(Debug)]
+enum ValueStackEntry {
+    Value(isize),
+    Variable,
+    VariableOperation(Operator, Box<ValueStackEntry>, Box<ValueStackEntry>),
+}
+
+impl ValueStackEntry {
+    fn apply_operator(operator: Operator, left: Self, right: Self) -> Self {
+        use ValueStackEntry::*;
+
+        match (operator, left, right) {
+            // if both sides are literal values, reduce now
+            (op, Value(l), Value(r)) => Value(op.apply(l, r)),
+
+            // found an equals with a variable
+            (Operator::Equals, l, Value(r)) => Self::solve_for_variable(l, r),
+            (Operator::Equals, Value(l), r) => Self::solve_for_variable(r, l),
+
+            // otherwise, it must be a non-equals operation containing a
+            // variable -- leave it for later
+            (op, l, r) => VariableOperation(op, Box::new(l), Box::new(r)),
+        }
+    }
+
+    fn solve_for_variable(initial_expression: ValueStackEntry, initial_rhs: isize) -> Self {
+        use Operator::*;
+        use ValueStackEntry::*;
+
+        let mut expression = initial_expression;
+        let mut rhs = initial_rhs;
+        loop {
+            match expression {
+                VariableOperation(operation, left, right) => {
+                    match (operation, *left, *right) {
+                        (Plus, expr, Value(val)) | (Plus, Value(val), expr) => {
+                            // order doesn't matter, either way subtract the value from rhs
+                            expression = expr;
+                            rhs = Minus.apply(rhs, val);
+                        }
+                        (Multiply, expr, Value(val)) | (Multiply, Value(val), expr) => {
+                            // order doesn't matter, either way divide rhs by the value
+                            expression = expr;
+                            rhs = Divide.apply(rhs, val);
+                        }
+                        (Minus, expr, Value(val)) => {
+                            expression = expr;
+                            rhs = Plus.apply(rhs, val);
+                        }
+                        (Minus, Value(val), expr) => {
+                            expression = expr;
+                            rhs = -Minus.apply(rhs, val);
+                        }
+                        (Divide, expr, Value(val)) => {
+                            expression = expr;
+                            rhs = Multiply.apply(rhs, val);
+                        }
+                        (Divide, Value(_), _) => {
+                            panic!("Division with value on left is unsupported");
+                        }
+                        (Equals, _, _) => {
+                            panic!("Equals nested inside an expression is unsupported");
+                        } 
+                        _ => panic!("Unexpected operation format"),
+                    }
+                }
+                Variable => return Value(rhs), // done!
+                Value(_) => panic!("Unexpected value in expression argument: {:?}", expression),
+            }
+        }
+    }
+}
+
+lazy_static! {
+    static ref ROOT: String = "root".to_string();
+    static ref HUMN: String = "humn".to_string();
 }
 
 fn part1(input: &str) -> isize {
     let monkeys = Monkeys::parse(input);
-    monkeys.run(&"root".to_string())
+    monkeys.run(&ROOT)
 }
 
-// fn part2(input: &str) -> usize {
-//     let items = Data::parse(input);
-//     dbg!(&items);
-//     0
-// }
+fn part2(input: &str) -> isize {
+    let mut monkeys = Monkeys::parse(input);
+
+    // change operator of root to =
+    let root = monkeys.jobs.get_mut(&*ROOT).unwrap();
+    if let Job::Operation(_, left, right) = root {
+        *root = Job::Operation(Operator::Equals, left.clone(), right.clone());
+    } else {
+        panic!("root must be an operation");
+    }
+
+    // change the value of humn to a variable
+    let humn = monkeys.jobs.get_mut(&*HUMN).unwrap();
+    *humn = Job::Variable;
+
+    monkeys.run(&*ROOT)
+}
 
 #[cfg(test)]
 mod tests {
@@ -230,15 +333,15 @@ mod tests {
         assert_eq!(result, 194501589693264);
     }
 
-    // #[test]
-    // fn test_part2_example() {
-    //     let result = part2(EXAMPLE);
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_example() {
+        let result = part2(EXAMPLE);
+        assert_eq!(result, 301);
+    }
 
-    // #[test]
-    // fn test_part2_solution() {
-    //     let result = part2(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_solution() {
+        let result = part2(&read_input_file());
+        assert_eq!(result, 3887609741189);
+    }
 }
