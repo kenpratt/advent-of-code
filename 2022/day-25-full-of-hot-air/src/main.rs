870cf141ad1adf5058f699f6d52a2d5a4f867a89
Ken Pratt <ken@kenpratt.net>
2023-01-23 11:17:58 -0800

    day 25 part 1

--- 2022/day-25-full-of-hot-air/src/main.rs
+++ 2022/day-25-full-of-hot-air/src/main.rs
@@ -1,52 +1,75 @@
 use std::fs;
 
-// use itertools::Itertools;
-use lazy_static::lazy_static;
-use regex::Regex;
-
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
-    // println!("part 2 result: {:?}", part2(&read_input_file()));
 }
 
 fn read_input_file() -> String {
     fs::read_to_string("input.txt").expect("Something went wrong reading the file")
 }
 
-#[derive(Debug)]
-struct Item {
-    foo: String,
-    bar: usize,
+fn snafu_to_decimal(num: &str) -> usize {
+    num.chars()
+        .fold(0, |acc, c| acc * 5 + snafu_char_to_int(&c)) as usize
 }
 
-impl Item {
-    fn parse_list(input: &str) -> Vec<Self> {
-        input.lines().map(|line| Self::parse(line)).collect()
-    }
+fn decimal_to_snafu(num: usize) -> String {
+    let mut rev_digits: Vec<char> = vec![];
 
-    fn parse(input: &str) -> Self {
-        lazy_static! {
-            static ref ITEM_RE: Regex = Regex::new(r"\A(.+)=(\d+)\z").unwrap();
+    let mut val = num;
+    while val > 0 {
+        let mut div = val / 5;
+        let mut rem = (val % 5) as isize;
+
+        if rem > 2 {
+            div += 1;
+            rem -= 5;
         }
 
-        let caps = ITEM_RE.captures(input).unwrap();
-        let foo = caps.get(1).unwrap().as_str().to_string();
-        let bar = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
-        Self { foo, bar }
+        val = div;
+        rev_digits.push(int_to_snafu_char(rem));
+    }
+
+    rev_digits.into_iter().rev().collect()
+}
+
+fn snafu_char_to_int(c: &char) -> isize {
+    match c {
+        '2' => 2,
+        '1' => 1,
+        '0' => 0,
+        '-' => -1,
+        '=' => -2,
+        _ => panic!("Unexpected SNAFU char: {}", c),
     }
 }
 
-fn part1(input: &str) -> usize {
-    let items = Item::parse_list(input);
-    dbg!(&items);
-    0
+fn int_to_snafu_char(n: isize) -> char {
+    match n {
+        2 => '2',
+        1 => '1',
+        0 => '0',
+        -1 => '-',
+        -2 => '=',
+        _ => panic!("Unexpected int: {}", n),
+    }
 }
 
-// fn part2(input: &str) -> usize {
-//     let items = Data::parse(input);
-//     dbg!(&items);
-//     0
-// }
+fn part1(input: &str) -> String {
+    let decimal_nums: Vec<usize> = input
+        .lines()
+        .map(|s| {
+            // verify
+            let n = snafu_to_decimal(s);
+            let s2 = decimal_to_snafu(n);
+            assert_eq!(s, &s2);
+            n
+        })
+        .collect();
+
+    let sum = decimal_nums.iter().sum::<usize>();
+    decimal_to_snafu(sum)
+}
 
 #[cfg(test)]
 mod tests {
@@ -55,30 +78,30 @@ mod tests {
     use indoc::indoc;
 
     static EXAMPLE: &str = indoc! {"
-        foo=22
+        1=-0-2
+        12111
+        2=0=
+        21
+        2=01
+        111
+        20012
+        112
+        1=-1=
+        1-12
+        12
+        1=
+        122
     "};
 
     #[test]
     fn test_part1_example() {
         let result = part1(EXAMPLE);
-        assert_eq!(result, 0);
+        assert_eq!(result, "2=-1=0");
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
-
-    // #[test]
-    // fn test_part2_example() {
-    //     let result = part2(EXAMPLE);
-    //     assert_eq!(result, 0);
-    // }
-
-    // #[test]
-    // fn test_part2_solution() {
-    //     let result = part2(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, "2-121-=10=200==2==21");
+    }
 }
