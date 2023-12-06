69faf77b5ee61951752c3de543d12524f0ad7cd0
Ken Pratt <ken@kenpratt.net>
2023-12-06 11:13:02 -0800

    day 6 part 1

--- 2023/day-06-wait-for-it/src/main.rs
+++ 2023/day-06-wait-for-it/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
+use std::iter::zip;
 
-// use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
 
@@ -14,37 +14,64 @@ fn read_input_file() -> String {
 }
 
 #[derive(Debug)]
-struct Item {
-    foo: String,
-    bar: usize,
+struct Race {
+    duration: usize,
+    record: usize,
 }
 
-impl Item {
+impl Race {
     fn parse_list(input: &str) -> Vec<Self> {
-        input.lines().map(|line| Self::parse(line)).collect()
-    }
-
-    fn parse(input: &str) -> Self {
         lazy_static! {
-            static ref ITEM_RE: Regex = Regex::new(r"\A(.+)=(\d+)\z").unwrap();
+            static ref INPUT_RE: Regex =
+                Regex::new(r"\ATime:\s+([\s\d]+)\nDistance:\s+([\s\d]+)\n?\z").unwrap();
         }
 
-        let caps = ITEM_RE.captures(input).unwrap();
-        let foo = caps.get(1).unwrap().as_str().to_string();
-        let bar = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
-        Self { foo, bar }
+        let caps = INPUT_RE.captures(input).unwrap();
+        let durations = parse_number_list(caps.get(1).unwrap().as_str());
+        let records = parse_number_list(caps.get(2).unwrap().as_str());
+
+        zip(durations, records)
+            .map(|(duration, record)| Race { duration, record })
+            .collect()
     }
+
+    fn strategies(&self) -> Vec<(usize, usize)> {
+        (0..=self.duration)
+            .map(|hold| {
+                let travel = self.duration - hold;
+                let distance = hold * travel;
+                (hold, distance)
+            })
+            .collect()
+    }
+
+    fn num_strategies_beating_record(&self) -> usize {
+        self.strategies()
+            .iter()
+            .filter(|(_hold, distance)| distance > &self.record)
+            .count()
+    }
+}
+
+fn parse_number_list(input: &str) -> Vec<usize> {
+    input
+        .trim()
+        .split_whitespace()
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect()
 }
 
 fn part1(input: &str) -> usize {
-    let items = Item::parse_list(input);
-    dbg!(&items);
-    0
+    let races = Race::parse_list(input);
+    races
+        .iter()
+        .map(|race| race.num_strategies_beating_record())
+        .product()
 }
 
 // fn part2(input: &str) -> usize {
-//     let items = Data::parse(input);
-//     dbg!(&items);
+//     let races = Data::parse(input);
+//     dbg!(&races);
 //     0
 // }
 
@@ -62,14 +89,14 @@ mod tests {
     #[test]
     fn test_part1_example() {
         let result = part1(EXAMPLE);
-        assert_eq!(result, 0);
+        assert_eq!(result, 288);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 3316275);
+    }
 
     // #[test]
     // fn test_part2_example() {
