db434de18da88a3b7ce645c74e9a93d46ffdbefc
Ken Pratt <ken@kenpratt.net>
2023-12-07 11:12:33 -0800

    day 7 part 1

--- 2023/day-07-camel-cards/src/main.rs
+++ 2023/day-07-camel-cards/src/main.rs
@@ -1,6 +1,6 @@
-use std::fs;
+use std::{cmp::Ordering, fs};
 
-// use itertools::Itertools;
+use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
 
@@ -13,38 +13,129 @@ fn read_input_file() -> String {
     fs::read_to_string("input.txt").expect("Something went wrong reading the file")
 }
 
-#[derive(Debug)]
-struct Item {
-    foo: String,
-    bar: usize,
+#[derive(Debug, Eq, PartialEq)]
+struct Round {
+    hand: Hand,
+    bid: usize,
 }
 
-impl Item {
+impl Round {
     fn parse_list(input: &str) -> Vec<Self> {
         input.lines().map(|line| Self::parse(line)).collect()
     }
 
     fn parse(input: &str) -> Self {
         lazy_static! {
-            static ref ITEM_RE: Regex = Regex::new(r"\A(.+)=(\d+)\z").unwrap();
+            static ref ROUND_RE: Regex = Regex::new(r"\A([1-9AKQJT]+) (\d+)\z").unwrap();
         }
 
-        let caps = ITEM_RE.captures(input).unwrap();
-        let foo = caps.get(1).unwrap().as_str().to_string();
-        let bar = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
-        Self { foo, bar }
+        let caps = ROUND_RE.captures(input).unwrap();
+        let hand = Hand::parse(caps.get(1).unwrap().as_str());
+        let bid = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
+
+        Self { hand, bid }
+    }
+}
+
+impl Ord for Round {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.hand.cmp(&other.hand)
+    }
+}
+
+impl PartialOrd for Round {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct Hand {
+    cards: [u8; 5],
+    strength: u8,
+}
+
+impl Hand {
+    fn parse(input: &str) -> Self {
+        let cards = Self::parse_cards(input);
+        let strength = Self::calculate_strength(&cards);
+
+        Self { cards, strength }
+    }
+
+    fn parse_cards(input: &str) -> [u8; 5] {
+        assert_eq!(input.len(), 5);
+        input
+            .chars()
+            .map(|c| match c {
+                'A' => 14,
+                'K' => 13,
+                'Q' => 12,
+                'J' => 11,
+                'T' => 10,
+                '9' => 9,
+                '8' => 8,
+                '7' => 7,
+                '6' => 6,
+                '5' => 5,
+                '4' => 4,
+                '3' => 3,
+                '2' => 2,
+                '1' => 1,
+                _ => panic!("Unknown card: {}", c),
+            })
+            .collect::<Vec<u8>>()
+            .try_into()
+            .unwrap()
+    }
+
+    fn calculate_strength(cards: &[u8; 5]) -> u8 {
+        let counts = cards.iter().counts();
+        let num_duplicates: Vec<&usize> = counts.values().sorted().rev().collect();
+
+        match num_duplicates.as_slice() {
+            [5] => 6,             // five of a kind
+            [4, 1] => 5,          // four of a kind
+            [3, 2] => 4,          // full house
+            [3, 1, 1] => 3,       // three of a kind
+            [2, 2, 1] => 2,       // two pair
+            [2, 1, 1, 1] => 1,    // one pair
+            [1, 1, 1, 1, 1] => 0, // high card
+            _ => panic!("Unknown hand type: {:?}", num_duplicates),
+        }
+    }
+}
+
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.strength
+            .cmp(&other.strength)
+            .then(self.cards.cmp(&other.cards))
+            .reverse()
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
 fn part1(input: &str) -> usize {
-    let items = Item::parse_list(input);
-    dbg!(&items);
-    0
+    let mut rounds = Round::parse_list(input);
+    rounds.sort();
+
+    let num_rounds = rounds.len();
+    let winnings = rounds.iter().enumerate().map(|(index, round)| {
+        let rank = num_rounds - index;
+        rank * round.bid
+    });
+    winnings.sum()
 }
 
 // fn part2(input: &str) -> usize {
-//     let items = Data::parse(input);
-//     dbg!(&items);
+//     let hands = Data::parse(input);
+//     dbg!(&hands);
 //     0
 // }
 
@@ -65,14 +156,14 @@ mod tests {
     #[test]
     fn test_part1_example() {
         let result = part1(EXAMPLE);
-        assert_eq!(result, 0);
+        assert_eq!(result, 6440);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 250120186);
+    }
 
     // #[test]
     // fn test_part2_example() {
