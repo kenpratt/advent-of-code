9a21c9dc5c8cd4d8de0a967520c4f2eb635317a3
Ken Pratt <ken@kenpratt.net>
2023-12-08 20:27:19 -0800

    day 8 part 1

--- 2023/day-08-haunted-wasteland/src/main.rs
+++ 2023/day-08-haunted-wasteland/src/main.rs
@@ -1,6 +1,5 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
-// use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
 
@@ -14,37 +13,93 @@ fn read_input_file() -> String {
 }
 
 #[derive(Debug)]
-struct Item {
-    foo: String,
-    bar: usize,
+struct Navigation {
+    instructions: Vec<Direction>,
+    nodes: HashMap<String, (String, String)>,
 }
 
-impl Item {
-    fn parse_list(input: &str) -> Vec<Self> {
-        input.lines().map(|line| Self::parse(line)).collect()
+impl Navigation {
+    fn parse(input: &str) -> Self {
+        let parts: Vec<&str> = input.split("\n\n").collect();
+        assert_eq!(parts.len(), 2);
+
+        let instructions = Direction::parse_list(&parts[0]);
+        let nodes = parts[1]
+            .lines()
+            .map(|line| Self::parse_node(line))
+            .collect();
+
+        Self {
+            instructions,
+            nodes,
+        }
     }
 
-    fn parse(input: &str) -> Self {
+    fn parse_node(input: &str) -> (String, (String, String)) {
         lazy_static! {
-            static ref ITEM_RE: Regex = Regex::new(r"\A(.+)=(\d+)\z").unwrap();
+            static ref NODE_RE: Regex =
+                Regex::new(r"\A([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)\z").unwrap();
         }
 
-        let caps = ITEM_RE.captures(input).unwrap();
-        let foo = caps.get(1).unwrap().as_str().to_string();
-        let bar = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
-        Self { foo, bar }
+        let caps = NODE_RE.captures(input).unwrap();
+        let node = caps.get(1).unwrap().as_str().to_string();
+        let left = caps.get(2).unwrap().as_str().to_string();
+        let right = caps.get(3).unwrap().as_str().to_string();
+        (node, (left, right))
+    }
+
+    fn navigate(&self, node: &str, instruction: &Direction) -> &str {
+        let pair = self.nodes.get(node).unwrap();
+        match instruction {
+            Direction::Left => &pair.0,
+            Direction::Right => &pair.1,
+        }
+    }
+
+    fn steps_needed(&self, start: &str, end: &str) -> usize {
+        let mut instructions_iter = self.instructions.iter().cycle();
+
+        let mut curr = start;
+        let mut steps = 0;
+
+        while curr != end {
+            let instruction = instructions_iter.next().unwrap();
+            curr = self.navigate(curr, instruction);
+            steps += 1;
+        }
+
+        steps
+    }
+}
+
+#[derive(Debug)]
+enum Direction {
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn parse_list(input: &str) -> Vec<Self> {
+        input.chars().map(|c| Self::parse(&c)).collect()
+    }
+
+    fn parse(input: &char) -> Self {
+        use Direction::*;
+        match input {
+            'L' => Left,
+            'R' => Right,
+            _ => panic!("Unknown direction: {}", input),
+        }
     }
 }
 
 fn part1(input: &str) -> usize {
-    let items = Item::parse_list(input);
-    dbg!(&items);
-    0
+    let navigation = Navigation::parse(input);
+    navigation.steps_needed(&"AAA", &"ZZZ")
 }
 
 // fn part2(input: &str) -> usize {
-//     let items = Data::parse(input);
-//     dbg!(&items);
+//     let navigations = Data::parse(input);
 //     0
 // }
 
@@ -77,20 +132,20 @@ mod tests {
     #[test]
     fn test_part1_example1() {
         let result = part1(EXAMPLE1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 2);
     }
 
     #[test]
     fn test_part1_example2() {
         let result = part1(EXAMPLE2);
-        assert_eq!(result, 0);
+        assert_eq!(result, 6);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 21389);
+    }
 
     // #[test]
     // fn test_part2_example() {
