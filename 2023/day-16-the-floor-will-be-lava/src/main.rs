0a70c3c5a0a088df720f1f67bf367f9f06e6a3d2
Ken Pratt <ken@kenpratt.net>
2023-12-14 04:20:42 -0800

    day 16 base

---
+++ 2023/day-16-the-floor-will-be-lava/src/main.rs
@@ -0,0 +1,93 @@
+use std::fs;
+
+// use itertools::Itertools;
+use lazy_static::lazy_static;
+use regex::Regex;
+
+fn main() {
+    println!("part 1 result: {:?}", part1(&read_input_file()));
+    // println!("part 2 result: {:?}", part2(&read_input_file()));
+}
+
+fn read_input_file() -> String {
+    fs::read_to_string("input.txt").expect("Something went wrong reading the file")
+}
+
+#[derive(Debug)]
+struct Item {
+    foo: String,
+    bar: usize,
+}
+
+impl Item {
+    fn parse_list(input: &str) -> Vec<Self> {
+        input.lines().map(|line| Self::parse(line)).collect()
+    }
+
+    fn parse(input: &str) -> Self {
+        lazy_static! {
+            static ref ITEM_RE: Regex = Regex::new(r"\A(.+)=(\d+)\z").unwrap();
+        }
+
+        let caps = ITEM_RE.captures(input).unwrap();
+        let foo = caps.get(1).unwrap().as_str().to_string();
+        let bar = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
+        Self { foo, bar }
+    }
+}
+
+fn part1(input: &str) -> usize {
+    let items = Item::parse_list(input);
+    dbg!(&items);
+    0
+}
+
+// fn part2(input: &str) -> usize {
+//     let items = Data::parse(input);
+//     dbg!(&items);
+//     0
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use indoc::indoc;
+
+    static EXAMPLE: &str = indoc! {"
+        .|...\....
+        |.-.\.....
+        .....|-...
+        ........|.
+        ..........
+        .........\
+        ..../.\\..
+        .-.-/..|..
+        .|....-|.\
+        ..//.|....
+    "};
+
+    #[test]
+    fn test_part1_example() {
+        let result = part1(EXAMPLE);
+        assert_eq!(result, 0);
+    }
+
+    // #[test]
+    // fn test_part1_solution() {
+    //     let result = part1(&read_input_file());
+    //     assert_eq!(result, 0);
+    // }
+
+    // #[test]
+    // fn test_part2_example() {
+    //     let result = part2(EXAMPLE);
+    //     assert_eq!(result, 0);
+    // }
+
+    // #[test]
+    // fn test_part2_solution() {
+    //     let result = part2(&read_input_file());
+    //     assert_eq!(result, 0);
+    // }
+}
