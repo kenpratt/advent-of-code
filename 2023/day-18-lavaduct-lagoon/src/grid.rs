8b1306111cc3b7044233255442dce6bb7d02c445
Ken Pratt <ken@kenpratt.net>
2023-12-18 14:35:17 -0800

    day 18 part 1

--- 2023/day-18-lavaduct-lagoon/src/grid.rs
+++ 2023/day-18-lavaduct-lagoon/src/grid.rs
@@ -1,84 +1,39 @@
-use std::collections::HashMap;
-
-#[derive(Debug)]
-pub struct Grid<T> {
-    cells: HashMap<Coord, T>,
-    pub width: usize,
-    pub height: usize,
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Coord {
+    pub x: i16,
+    pub y: i16,
 }
 
-impl<T> Grid<T> {
-    pub fn parse(input: &str, parse_value: fn(&char) -> T) -> Self {
-        let width = input.lines().next().unwrap().len();
-        let height = input.lines().count();
-
-        let mut cells = HashMap::new();
-
-        for (y, line) in input.lines().enumerate() {
-            for (x, c) in line.chars().enumerate() {
-                let pos = Coord::new(x, y);
-                let cell = parse_value(&c);
-                cells.insert(pos, cell);
-            }
-        }
-
-        Self {
-            cells,
-            width,
-            height,
-        }
+impl Coord {
+    pub fn new(x: i16, y: i16) -> Self {
+        Self { x, y }
     }
 
-    pub fn value(&self, pos: &Coord) -> &T {
-        self.cells.get(pos).unwrap()
-    }
-
-    pub fn shift(&self, pos: &Coord, dir: &Direction) -> Option<Coord> {
-        if (dir == &Direction::West && pos.x == 0)
-            || (dir == &Direction::North && pos.y == 0)
-            || (dir == &Direction::East && pos.x == (self.width - 1))
-            || (dir == &Direction::South && pos.y == (self.height - 1))
-        {
-            None
-        } else {
-            Some(pos.shift(dir))
+    pub fn shift(&self, direction: &Direction, distance: &i16) -> Self {
+        use Direction::*;
+        match direction {
+            North => Self::new(self.x, self.y - distance),
+            West => Self::new(self.x - distance, self.y),
+            South => Self::new(self.x, self.y + distance),
+            East => Self::new(self.x + distance, self.y),
         }
     }
 }
 
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct Coord {
-    x: usize,
-    y: usize,
-}
-
-impl Coord {
-    pub fn new(x: usize, y: usize) -> Self {
-        Self { x, y }
-    }
-
-    fn shift(&self, direction: &Direction) -> Self {
-        use Direction::*;
-        match direction {
-            North => Self::new(self.x, self.y - 1),
-            West => Self::new(self.x - 1, self.y),
-            South => Self::new(self.x, self.y + 1),
-            East => Self::new(self.x + 1, self.y),
-        }
-    }
-
-    pub fn manhattan_distance(&self, other: &Coord) -> usize {
-        abs_diff(self.x, other.x) + abs_diff(self.y, other.y)
-    }
-}
-
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
 pub enum Direction {
     North,
     West,
     South,
     East,
 }
+
+pub const ALL_DIRECTIONS: [Direction; 4] = [
+    Direction::North,
+    Direction::West,
+    Direction::South,
+    Direction::East,
+];
 
 impl Direction {
     pub fn clockwise(&self) -> Self {
@@ -101,13 +56,5 @@ impl Direction {
             South => East,
             East => North,
         }
-    }
-}
-
-fn abs_diff(x: usize, y: usize) -> usize {
-    if x <= y {
-        y - x
-    } else {
-        x - y
     }
 }
--- 2023/day-18-lavaduct-lagoon/src/main.rs
+++ 2023/day-18-lavaduct-lagoon/src/main.rs
@@ -1,6 +1,10 @@
 pub mod grid;
 
-use std::fs;
+use std::{
+    collections::{BTreeSet, HashMap},
+    fs,
+    ops::RangeInclusive,
+};
 
 use grid::*;
 
@@ -19,8 +23,7 @@ fn read_input_file() -> String {
 #[derive(Debug)]
 struct Instruction {
     direction: Direction,
-    distance: usize,
-    colour: String,
+    distance: i16,
 }
 
 impl Instruction {
@@ -36,13 +39,12 @@ impl Instruction {
 
         let caps = INSTRUCTION_RE.captures(input).unwrap();
         let direction = Self::parse_direction(caps.get(1).unwrap().as_str());
-        let distance = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
-        let colour = caps.get(3).unwrap().as_str().to_string();
+        let distance = caps.get(2).unwrap().as_str().parse::<i16>().unwrap();
+        let _colour = caps.get(3).unwrap().as_str().to_string();
 
         Self {
             direction,
             distance,
-            colour,
         }
     }
 
@@ -57,15 +59,198 @@ impl Instruction {
     }
 }
 
+#[derive(Debug)]
+struct Edge {
+    direction: Direction,
+    points: (Coord, Coord),
+}
+
+impl Edge {
+    fn build(instructions: &[Instruction], start: Coord) -> Vec<Self> {
+        let mut last = start;
+        let res: Vec<Self> = instructions
+            .iter()
+            .map(|inst| {
+                let from = last;
+                let to = from.shift(&inst.direction, &inst.distance);
+                last = to.clone();
+                Self {
+                    direction: inst.direction,
+                    points: (from, to),
+                }
+            })
+            .collect();
+
+        // ensure we finish at the origin
+        assert_eq!(res[0].points.0, res[res.len() - 1].points.1);
+
+        res
+    }
+
+    fn determine_facing(edges: &[Edge], min_y: i16) -> Vec<Direction> {
+        let starting_index = edges
+            .iter()
+            .position(|edge| edge.horizontal() && edge.points.0.y == min_y)
+            .unwrap();
+
+        let mut tmp_facing: Vec<Option<Direction>> = vec![None; edges.len()];
+        tmp_facing[starting_index] = Some(Direction::South);
+        let mut last_facing = Direction::South;
+        let mut last_direction = edges[starting_index].direction;
+
+        for i in ((starting_index + 1)..edges.len()).chain(0..starting_index) {
+            let edge = &edges[i];
+            let curr_facing = match (last_direction, edge.direction) {
+                (Direction::North, Direction::East)
+                | (Direction::East, Direction::South)
+                | (Direction::South, Direction::West)
+                | (Direction::West, Direction::North) => last_facing.clockwise(),
+                (Direction::North, Direction::West)
+                | (Direction::West, Direction::South)
+                | (Direction::South, Direction::East)
+                | (Direction::East, Direction::North) => last_facing.counterclockwise(),
+                _ => panic!("Unreachable"),
+            };
+
+            tmp_facing[i] = Some(curr_facing);
+            last_facing = curr_facing;
+            last_direction = edge.direction;
+        }
+
+        tmp_facing.into_iter().flat_map(|f| f).collect()
+    }
+
+    fn x_range(&self) -> RangeInclusive<i16> {
+        if self.points.0.x <= self.points.1.x {
+            self.points.0.x..=self.points.1.x
+        } else {
+            self.points.1.x..=self.points.0.x
+        }
+    }
+
+    fn y_range(&self) -> RangeInclusive<i16> {
+        if self.points.0.y <= self.points.1.y {
+            self.points.0.y..=self.points.1.y
+        } else {
+            self.points.1.y..=self.points.0.y
+        }
+    }
+
+    fn horizontal(&self) -> bool {
+        self.points.0.y == self.points.1.y
+    }
+
+    fn vertical(&self) -> bool {
+        self.points.0.x == self.points.1.x
+    }
+}
+
+#[derive(Debug)]
+struct Plan {
+    instructions: Vec<Instruction>,
+}
+
+impl Plan {
+    fn new(instructions: Vec<Instruction>) -> Self {
+        Self { instructions }
+    }
+
+    fn area(&self) -> usize {
+        let edges = Edge::build(&self.instructions, Coord::new(0, 0));
+
+        let min_y = edges.iter().map(|e| *e.y_range().start()).min().unwrap();
+        let max_y = edges.iter().map(|e| *e.y_range().end()).max().unwrap();
+
+        let facing = Edge::determine_facing(&edges, min_y);
+
+        // compute metadata for rasterization
+        let mut vertical_intersections_map: HashMap<i16, BTreeSet<(i16, Direction)>> =
+            HashMap::new();
+        let mut horizontal_segments_map: HashMap<i16, BTreeSet<(i16, i16)>> = HashMap::new();
+        for y in min_y..=max_y {
+            vertical_intersections_map.insert(y, BTreeSet::new());
+            horizontal_segments_map.insert(y, BTreeSet::new());
+        }
+        for (i, edge) in edges.iter().enumerate() {
+            if edge.vertical() {
+                let x = edge.points.0.x;
+                let facing = facing[i];
+                for y in edge.y_range() {
+                    vertical_intersections_map
+                        .get_mut(&y)
+                        .unwrap()
+                        .insert((x, facing));
+                }
+            } else {
+                let y = edge.points.0.y;
+                let xr = edge.x_range();
+                horizontal_segments_map
+                    .get_mut(&y)
+                    .unwrap()
+                    .insert((*xr.start(), *xr.end()));
+            }
+        }
+
+        // rasterize
+        (min_y..=max_y)
+            .map(|y| {
+                let vertical_intersections = vertical_intersections_map.get(&y).unwrap();
+                let horizontal_segments = horizontal_segments_map.get(&y).unwrap();
+                Self::line_area(vertical_intersections, horizontal_segments)
+            })
+            .sum()
+    }
+
+    fn line_area(
+        vertical_intersections: &BTreeSet<(i16, Direction)>,
+        horizontal_segments: &BTreeSet<(i16, i16)>,
+    ) -> usize {
+        let mut segments = horizontal_segments.clone();
+
+        // find gaps between vertical lines
+        for w in vertical_intersections
+            .iter()
+            .collect::<Vec<&(i16, Direction)>>()
+            .windows(2)
+        {
+            match (w[0], w[1]) {
+                ((l, Direction::East), (r, Direction::West)) => {
+                    segments.insert((*l, *r));
+                }
+                _ => (),
+            }
+        }
+
+        // sum up!
+        let mut area = 0;
+        let mut segments_iter = segments.iter().peekable();
+        while let Some((l, r)) = segments_iter.next() {
+            assert!(r > l);
+            match segments_iter.peek() {
+                Some((ol, _or)) if ol <= r => {
+                    // overlapping ranges, use (ol - 1) instead of r
+                    assert!(ol > l);
+                    area += (ol - l) as usize;
+                }
+                _ => {
+                    // normal
+                    area += (r - l + 1) as usize;
+                }
+            }
+        }
+        area
+    }
+}
+
 fn part1(input: &str) -> usize {
     let instructions = Instruction::parse_list(input);
-    dbg!(&instructions);
-    0
+
+    let plan = Plan::new(instructions);
+    plan.area()
 }
 
 // fn part2(input: &str) -> usize {
 //     let instructions = Data::parse(input);
-//     dbg!(&instructions);
 //     0
 // }
 
@@ -95,14 +280,14 @@ mod tests {
     #[test]
     fn test_part1_example() {
         let result = part1(EXAMPLE);
-        assert_eq!(result, 0);
+        assert_eq!(result, 62);
     }
 
-    // #[test]
-    // fn test_part1_solution() {
-    //     let result = part1(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_input_file());
+        assert_eq!(result, 49578);
+    }
 
     // #[test]
     // fn test_part2_example() {
