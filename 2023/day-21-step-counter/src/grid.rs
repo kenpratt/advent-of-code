6d216f9f9c0a72bdba46605af5d55881129aaf64
Ken Pratt <ken@kenpratt.net>
2023-12-21 19:58:31 -0800

    day 21 part 2 progress

--- 2023/day-21-step-counter/src/grid.rs
+++ 2023/day-21-step-counter/src/grid.rs
@@ -1,22 +1,24 @@
 use std::collections::HashMap;
 
+use cached::proc_macro::cached;
+
 #[derive(Debug)]
 pub struct Grid<T> {
     pub cells: HashMap<Coord, T>,
-    pub width: usize,
-    pub height: usize,
+    pub width: i16,
+    pub height: i16,
 }
 
 impl<T> Grid<T> {
     pub fn parse(input: &str, parse_value: fn(&char) -> T) -> Self {
-        let width = input.lines().next().unwrap().len();
-        let height = input.lines().count();
+        let width = input.lines().next().unwrap().len() as i16;
+        let height = input.lines().count() as i16;
 
         let mut cells = HashMap::new();
 
         for (y, line) in input.lines().enumerate() {
             for (x, c) in line.chars().enumerate() {
-                let pos = Coord::new(x, y);
+                let pos = Coord::new(x as i16, y as i16);
                 let cell = parse_value(&c);
                 cells.insert(pos, cell);
             }
@@ -33,38 +35,49 @@ impl<T> Grid<T> {
         self.cells.get(pos).unwrap()
     }
 
-    pub fn shift(&self, pos: &Coord, dir: &Direction) -> Option<Coord> {
-        if (dir == &Direction::West && pos.x == 0)
-            || (dir == &Direction::North && pos.y == 0)
-            || (dir == &Direction::East && pos.x == (self.width - 1))
-            || (dir == &Direction::South && pos.y == (self.height - 1))
-        {
-            None
-        } else {
-            Some(pos.shift(dir))
-        }
+    pub fn neighbours(&self, pos: &Coord) -> Vec<(Coord, Option<Direction>)> {
+        neighbours(*pos, self.width, self.height)
     }
+}
 
-    pub fn neighbours(&self, pos: &Coord) -> Vec<Coord> {
-        ALL_DIRECTIONS
-            .iter()
-            .flat_map(|d| self.shift(pos, d))
-            .collect()
+#[cached]
+fn neighbours(pos: Coord, width: i16, height: i16) -> Vec<(Coord, Option<Direction>)> {
+    ALL_DIRECTIONS
+        .iter()
+        .map(|d: &Direction| neighbour(pos, *d, width, height))
+        .collect()
+}
+
+#[cached]
+fn neighbour(
+    pos: Coord,
+    direction: Direction,
+    width: i16,
+    height: i16,
+) -> (Coord, Option<Direction>) {
+    let r = width - 1;
+    let b = height - 1;
+    match (direction, pos.x, pos.y) {
+        (Direction::North, x, 0) => (Coord::new(x, b), Some(Direction::North)),
+        (Direction::West, 0, y) => (Coord::new(r, y), Some(Direction::West)),
+        (Direction::South, x, y) if y == b => (Coord::new(x, 0), Some(Direction::South)),
+        (Direction::East, x, y) if x == r => (Coord::new(0, y), Some(Direction::East)),
+        _ => (pos.shift(&direction), None),
     }
 }
 
 #[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Coord {
-    x: usize,
-    y: usize,
+    x: i16,
+    y: i16,
 }
 
 impl Coord {
-    pub fn new(x: usize, y: usize) -> Self {
+    pub fn new(x: i16, y: i16) -> Self {
         Self { x, y }
     }
 
-    fn shift(&self, direction: &Direction) -> Self {
+    pub fn shift(&self, direction: &Direction) -> Self {
         use Direction::*;
         match direction {
             North => Self::new(self.x, self.y - 1),
@@ -72,6 +85,10 @@ impl Coord {
             South => Self::new(self.x, self.y + 1),
             East => Self::new(self.x + 1, self.y),
         }
+    }
+
+    pub fn neighbours(&self) -> Vec<Coord> {
+        ALL_DIRECTIONS.iter().map(|d| self.shift(d)).collect()
     }
 }
 
@@ -81,6 +98,26 @@ pub enum Direction {
     West,
     South,
     East,
+}
+
+impl Direction {
+    pub fn index(&self) -> usize {
+        match self {
+            Direction::North => 0,
+            Direction::West => 1,
+            Direction::South => 2,
+            Direction::East => 3,
+        }
+    }
+
+    pub fn rev(&self) -> Self {
+        match self {
+            Direction::North => Direction::South,
+            Direction::West => Direction::East,
+            Direction::South => Direction::North,
+            Direction::East => Direction::West,
+        }
+    }
 }
 
 pub const ALL_DIRECTIONS: [Direction; 4] = [
--- 2023/day-21-step-counter/src/main.rs
+++ 2023/day-21-step-counter/src/main.rs
@@ -1,16 +1,189 @@
 pub mod grid;
 
-use std::{collections::HashSet, fs};
+use std::{
+    collections::{BTreeSet, HashMap, HashSet},
+    fs,
+};
+
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
 use grid::*;
 
+use lazy_static::lazy_static;
+
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file(), 64));
-    // println!("part 2 result: {:?}", part2(&read_input_file()));
+    println!("part 2 result: {:?}", part2(&read_input_file(), 26501365));
 }
 
 fn read_input_file() -> String {
     fs::read_to_string("input.txt").expect("Something went wrong reading the file")
+}
+
+const EMPTY_COORDS: BTreeSet<Coord> = BTreeSet::new();
+lazy_static! {
+    static ref EMPTY_STATE: u64 = calculate_hash(&EMPTY_COORDS);
+}
+
+struct Solver<'a> {
+    grid: &'a Grid<Terrain>,
+    rocks: HashSet<Coord>,
+    starting_location: Coord,
+    next_state_cache: HashMap<(u64, [u64; 4]), u64>,
+    coords_cache: HashMap<u64, BTreeSet<Coord>>,
+    outputs_cache: HashMap<u64, (BTreeSet<Coord>, [BTreeSet<Coord>; 4])>,
+}
+
+impl<'a> Solver<'a> {
+    fn solve(grid: &'a Grid<Terrain>, steps: usize) -> usize {
+        let rocks: HashSet<Coord> = grid
+            .cells
+            .iter()
+            .filter(|(_pos, terrain)| **terrain == Terrain::Rock)
+            .map(|(pos, _terrain)| *pos)
+            .collect();
+
+        let starting_location = *grid
+            .cells
+            .iter()
+            .find(|(_pos, terrain)| **terrain == Terrain::Start)
+            .map(|(pos, _terrain)| pos)
+            .unwrap();
+
+        let mut solver = Solver {
+            grid,
+            rocks,
+            starting_location,
+            next_state_cache: HashMap::new(),
+            coords_cache: HashMap::new(),
+            outputs_cache: HashMap::new(),
+        };
+
+        solver.run(steps)
+    }
+
+    fn run(&mut self, steps: usize) -> usize {
+        // insert empty coords
+        self.coords_cache.insert(*EMPTY_STATE, EMPTY_COORDS);
+
+        // insert initial state
+        let mut initial_coords: BTreeSet<Coord> = BTreeSet::new();
+        initial_coords.insert(self.starting_location);
+        let initial_state = calculate_hash(&initial_coords);
+        self.coords_cache.insert(initial_state, initial_coords);
+
+        let mut active_tiles: HashMap<Coord, u64> = HashMap::new();
+        active_tiles.insert(Coord::new(0, 0), initial_state);
+
+        for step in 1..=steps {
+            active_tiles = self.step(step, active_tiles);
+        }
+
+        active_tiles
+            .values()
+            .map(|state| self.coords_cache.get(state).unwrap().len())
+            .sum()
+    }
+
+    fn step(&mut self, _step: usize, active_tiles: HashMap<Coord, u64>) -> HashMap<Coord, u64> {
+        // println!("\nstep {}:", step);
+
+        // explore active tiles, plus direct neigbours
+        let mut to_explore: HashSet<Coord> = HashSet::new();
+        for tile in active_tiles.keys() {
+            to_explore.insert(*tile);
+            for neighbour in tile.neighbours() {
+                to_explore.insert(neighbour);
+            }
+        }
+
+        let mut next_active_tiles: HashMap<Coord, u64> = HashMap::new();
+
+        for tile_pos in to_explore {
+            let tile_state = active_tiles.get(&tile_pos).unwrap_or(&EMPTY_STATE);
+
+            let mut neighbour_tile_states: [u64; 4] = [0; 4];
+            for direction in ALL_DIRECTIONS {
+                let neighbour_tile_pos = tile_pos.shift(&direction);
+                neighbour_tile_states[direction.index()] = *active_tiles
+                    .get(&neighbour_tile_pos)
+                    .unwrap_or(&EMPTY_STATE);
+            }
+
+            let next_tile_state = self.next_state(*tile_state, neighbour_tile_states);
+            if next_tile_state != *EMPTY_STATE {
+                next_active_tiles.insert(tile_pos, next_tile_state);
+            }
+        }
+
+        next_active_tiles
+    }
+
+    fn next_state(&mut self, curr_state: u64, neighbour_states: [u64; 4]) -> u64 {
+        let key = (curr_state, neighbour_states);
+        match self.next_state_cache.get(&key) {
+            Some(val) => *val,
+            None => {
+                let val = self.calculate_next_state(curr_state, neighbour_states);
+                self.next_state_cache.insert(key, val);
+                val
+            }
+        }
+    }
+
+    fn calculate_next_state(&mut self, curr_state: u64, neighbour_states: [u64; 4]) -> u64 {
+        let (own_outputs, _) = self.outputs(curr_state);
+        let mut coords = own_outputs.clone();
+
+        for direction in ALL_DIRECTIONS {
+            let outputs = self.neighbour_outputs(neighbour_states, direction);
+            coords.append(&mut outputs.clone());
+        }
+
+        // cache result in coords
+        let state = calculate_hash(&coords);
+        self.coords_cache.insert(state, coords);
+        state
+    }
+
+    fn outputs(&mut self, curr_state: u64) -> &(BTreeSet<Coord>, [BTreeSet<Coord>; 4]) {
+        if !self.outputs_cache.contains_key(&curr_state) {
+            let val = self.calculate_outputs(curr_state);
+            self.outputs_cache.insert(curr_state, val);
+        }
+        self.outputs_cache.get(&curr_state).unwrap()
+    }
+
+    fn neighbour_outputs(
+        &mut self,
+        neighbour_states: [u64; 4],
+        direction: Direction,
+    ) -> &BTreeSet<Coord> {
+        let state = neighbour_states[direction.index()];
+        let (_, in_directions) = self.outputs(state);
+        &in_directions[direction.rev().index()]
+    }
+
+    fn calculate_outputs(&mut self, curr_state: u64) -> (BTreeSet<Coord>, [BTreeSet<Coord>; 4]) {
+        let expand_coords = self.coords_cache.get(&curr_state).unwrap();
+
+        let mut own_outputs = BTreeSet::new();
+        let mut neighbour_outputs: [BTreeSet<Coord>; 4] = Default::default();
+
+        for expand_pos in expand_coords {
+            for (neighbour_pos, neighbour_tile_direction) in self.grid.neighbours(expand_pos) {
+                if !self.rocks.contains(&neighbour_pos) {
+                    match neighbour_tile_direction {
+                        Some(d) => neighbour_outputs[d.index()].insert(neighbour_pos),
+                        None => own_outputs.insert(neighbour_pos),
+                    };
+                }
+            }
+        }
+
+        (own_outputs, neighbour_outputs)
+    }
 }
 
 #[derive(Debug)]
@@ -24,48 +197,8 @@ impl Map {
         Self { grid }
     }
 
-    fn starting_position(&self) -> &Coord {
-        self.grid
-            .cells
-            .iter()
-            .find(|(_pos, terrain)| **terrain == Terrain::Start)
-            .map(|(pos, _terrain)| pos)
-            .unwrap()
-    }
-
     fn count_plots_within_reach(&self, steps: usize) -> usize {
-        let start = self.starting_position();
-
-        // locations to expand during next step
-        let mut frontier: HashSet<Coord> = HashSet::new();
-        frontier.insert(*start);
-
-        // rock locations
-        let rocks: HashSet<&Coord> = self
-            .grid
-            .cells
-            .iter()
-            .filter(|(_pos, terrain)| **terrain == Terrain::Rock)
-            .map(|(pos, _terrain)| pos)
-            .collect();
-
-        // take the steps!
-        for _step in 1..=steps {
-            // explore the whole frontier, and build a new one
-            let mut new_frontier = HashSet::new();
-
-            for to_expand in frontier {
-                for neighbour in self.grid.neighbours(&to_expand) {
-                    if !rocks.contains(&neighbour) {
-                        new_frontier.insert(neighbour);
-                    }
-                }
-            }
-
-            frontier = new_frontier;
-        }
-
-        frontier.len()
+        Solver::solve(&self.grid, steps)
     }
 }
 
@@ -87,16 +220,21 @@ impl Terrain {
     }
 }
 
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
 fn part1(input: &str, steps: usize) -> usize {
     let map = Map::parse(input);
     map.count_plots_within_reach(steps)
 }
 
-// fn part2(input: &str) -> usize {
-//     let items = Data::parse(input);
-//     dbg!(&items);
-//     0
-// }
+fn part2(input: &str, steps: usize) -> usize {
+    let map = Map::parse(input);
+    map.count_plots_within_reach(steps)
+}
 
 #[cfg(test)]
 mod tests {
@@ -130,15 +268,20 @@ mod tests {
         assert_eq!(result, 3768);
     }
 
-    // #[test]
-    // fn test_part2_example() {
-    //     let result = part2(EXAMPLE);
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_example() {
+        // assert_eq!(part2(EXAMPLE, 6), 16);
+        // assert_eq!(part2(EXAMPLE, 10), 50);
+        // assert_eq!(part2(EXAMPLE, 50), 1594);
+        // assert_eq!(part2(EXAMPLE, 100), 6536);
+        assert_eq!(part2(EXAMPLE, 500), 167004);
+        // assert_eq!(part2(EXAMPLE, 1000), 668697);
+        // assert_eq!(part2(EXAMPLE, 5000), 16733044);
+    }
 
     // #[test]
     // fn test_part2_solution() {
-    //     let result = part2(&read_input_file());
+    //     let result = part2(&read_input_file(), 300);
     //     assert_eq!(result, 0);
     // }
 }
