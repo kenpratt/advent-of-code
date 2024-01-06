2360908ba888f5ca26a90aaed7ed936518164a39
Ken Pratt <ken@kenpratt.net>
2024-01-06 15:26:26 -0800

    day 23 part 2

--- 2023/day-23-a-long-walk/src/main.rs
+++ 2023/day-23-a-long-walk/src/main.rs
@@ -1,12 +1,15 @@
 pub mod grid;
 
-use std::{collections::BTreeSet, fs};
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+};
 
 use grid::*;
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_input_file()));
-    // println!("part 2 result: {:?}", part2(&read_input_file()));
+    println!("part 2 result: {:?}", part2(&read_input_file()));
 }
 
 fn read_input_file() -> String {
@@ -22,6 +25,127 @@ impl Map {
     fn parse(input: &str) -> Self {
         let grid = Grid::parse(input, |c| Terrain::parse(c));
         Self { grid }
+    }
+
+    fn to_graph(&self, use_slopes: bool) -> Graph {
+        // start on path in top row
+        let start = (0..self.grid.width)
+            .map(|x| Coord::new(x, 0))
+            .find(|c| self.grid.value(c) == &Terrain::Path)
+            .unwrap();
+
+        // end on path in bottom row
+        let end = (0..self.grid.width)
+            .map(|x| Coord::new(x, self.grid.height - 1))
+            .find(|c| self.grid.value(c) == &Terrain::Path)
+            .unwrap();
+
+        // find the neighbours of each tile
+        // and factor in whether to treat slopes as slippery (part 1 vs 2)
+        let neighbours_map: HashMap<Coord, Vec<Coord>> = self
+            .grid
+            .cells
+            .iter()
+            .filter(|(_pos, t)| t != &&Terrain::Forest)
+            .map(|(pos, t)| (*pos, self.neighbours(pos, t, use_slopes)))
+            .collect();
+
+        // find all the junctions where you can make a choice of where to go
+        let mut junctions: HashSet<Coord> = neighbours_map
+            .iter()
+            .filter(|(_, neighbours)| neighbours.len() > 2)
+            .map(|(pos, _)| *pos)
+            .collect();
+
+        // add start & end as synthetic junctions
+        junctions.insert(start.clone());
+        junctions.insert(end.clone());
+
+        // explore where junctions/start/end connect to each other (these will be the nodes)
+        let mut connections: HashMap<Coord, Vec<(Coord, usize)>> = HashMap::new();
+        for pos in &junctions {
+            let conn = Self::find_connections(pos, &neighbours_map, &junctions);
+            connections.insert(*pos, conn);
+        }
+
+        // okay, now we can discard all the coords and convert to simple IDs, and return a graph
+        let id_map: HashMap<&Coord, u8> = junctions
+            .iter()
+            .enumerate()
+            .map(|(id, pos)| (pos, id.try_into().unwrap()))
+            .collect();
+
+        // return a graph structure
+        Graph {
+            start: *id_map.get(&start).unwrap(),
+            end: *id_map.get(&end).unwrap(),
+            connections: connections
+                .iter()
+                .map(|(pos, conns)| {
+                    (
+                        *id_map.get(pos).unwrap(),
+                        conns
+                            .iter()
+                            .map(|(c, d)| (*id_map.get(c).unwrap(), *d))
+                            .collect(),
+                    )
+                })
+                .collect(),
+        }
+    }
+
+    fn neighbours(&self, pos: &Coord, terrain: &Terrain, use_slopes: bool) -> Vec<Coord> {
+        if terrain == &Terrain::Forest {
+            panic!("Shouldn't be a forest");
+        }
+
+        if use_slopes {
+            let directions = terrain.next_directions();
+            self.neighbours_in_directions(pos, &directions)
+        } else {
+            self.neighbours_in_directions(pos, &ALL_DIRECTIONS)
+        }
+    }
+
+    fn neighbours_in_directions(&self, pos: &Coord, directions: &[Direction]) -> Vec<Coord> {
+        directions
+            .iter()
+            .filter_map(|dir| self.grid.neighbour(pos, dir))
+            .filter(|dest| self.grid.value(dest) != &Terrain::Forest)
+            .collect()
+    }
+
+    fn find_connections(
+        origin: &Coord,
+        neighbours_map: &HashMap<Coord, Vec<Coord>>,
+        junctions: &HashSet<Coord>,
+    ) -> Vec<(Coord, usize)> {
+        let mut to_explore: Vec<(&Coord, &Coord, usize)> = neighbours_map
+            .get(origin)
+            .unwrap()
+            .iter()
+            .map(|n| (origin, n, 1))
+            .collect();
+
+        let mut results = vec![];
+
+        while let Some((from, to, dist)) = to_explore.pop() {
+            if junctions.contains(to) {
+                results.push((*to, dist));
+            } else {
+                for next in neighbours_map.get(to).unwrap() {
+                    if next != from {
+                        to_explore.push((to, next, dist + 1));
+                    }
+                }
+            }
+        }
+
+        if results.iter().any(|(c, _)| c == origin) {
+            panic!("Connection to origin coord");
+        }
+
+        results
     }
 }
 
@@ -59,92 +183,82 @@ impl Terrain {
     }
 }
 
-struct Solver<'a> {
-    grid: &'a Grid<Terrain>,
-    end: Coord,
+#[derive(Debug)]
+struct Graph {
+    start: u8,
+    end: u8,
+    connections: HashMap<u8, Vec<(u8, usize)>>,
 }
 
-impl<'a> Solver<'a> {
-    fn run(grid: &'a Grid<Terrain>) -> usize {
-        // start on path in top row
-        let start = (0..grid.width)
-            .map(|x| Coord::new(x, 0))
-            .find(|c| grid.value(c) == &Terrain::Path)
-            .unwrap();
-
-        // end on path in bottom row
-        let end = (0..grid.width)
-            .map(|x| Coord::new(x, grid.height - 1))
-            .find(|c| grid.value(c) == &Terrain::Path)
-            .unwrap();
-
-        let mut solver = Solver { grid, end };
-        let initial = SolutionState::new(start);
-
-        solver.solve(initial)
-    }
-
-    fn solve(&mut self, initial: SolutionState) -> usize {
-        let mut open_set = vec![initial];
-        let mut at_goal = vec![];
+impl Graph {
+    fn solve(&self) -> usize {
+        let mut high_score = 0;
+        let mut open_set = vec![SolutionState::new(self.start)];
 
         while let Some(curr) = open_set.pop() {
             if curr.pos == self.end {
-                at_goal.push(curr);
+                // we're at the goal!
+                if curr.distance > high_score {
+                    high_score = curr.distance;
+                }
             } else {
-                let mut next = curr.next(self.grid);
+                let conns = self.connections.get(&curr.pos).unwrap();
+                let mut next = curr.next(conns);
                 open_set.append(&mut next);
             }
         }
 
-        let tiles = at_goal.into_iter().map(|s| s.visited.len()).max().unwrap();
-        tiles - 1 // count steps
+        high_score
     }
 }
 
-#[derive(Clone, Debug, Hash, Eq, PartialEq)]
+#[derive(Debug)]
 struct SolutionState {
-    pos: Coord,
-    visited: BTreeSet<Coord>,
+    pos: u8,
+    visited: HashSet<u8>,
+    distance: usize,
 }
 
 impl SolutionState {
-    fn new(start: Coord) -> Self {
-        let visited = BTreeSet::from([start.clone()]);
-        let pos = start;
-        Self { pos, visited }
+    fn new(start: u8) -> Self {
+        Self {
+            pos: start,
+            visited: HashSet::from([start]),
+            distance: 0,
+        }
     }
 
-    fn next(&self, grid: &Grid<Terrain>) -> Vec<SolutionState> {
-        let curr_terrain = grid.value(&self.pos);
-        curr_terrain
-            .next_directions()
-            .into_iter()
-            .filter_map(|dir| grid.neighbour(&self.pos, &dir))
-            .filter(|dest| !self.visited.contains(dest))
-            .filter(|dest| grid.value(dest) != &Terrain::Forest)
-            .map(|dest| self.move_to(dest))
+    fn next(&self, connections: &[(u8, usize)]) -> Vec<SolutionState> {
+        connections
+            .iter()
+            .filter(|(dest, _)| !self.visited.contains(dest))
+            .map(|(dest, dist)| self.move_to(dest, dist))
             .collect()
     }
 
-    fn move_to(&self, dest: Coord) -> Self {
-        let mut visited = self.visited.clone();
-        visited.insert(dest.clone());
+    fn move_to(&self, destination: &u8, distance: &usize) -> Self {
+        let mut next_visited = self.visited.clone();
+        next_visited.insert(*destination);
 
-        let pos = dest;
-        Self { pos, visited }
+        Self {
+            pos: *destination,
+            visited: next_visited,
+            distance: self.distance + distance,
+        }
     }
 }
 
 fn part1(input: &str) -> usize {
     let map = Map::parse(input);
-    Solver::run(&map.grid)
+    let graph = map.to_graph(true);
+    graph.solve()
 }
 
-// fn part2(input: &str) -> usize {
-//     let items = Data::parse(input);
-//     0
-// }
+fn part2(input: &str) -> usize {
+    let map = Map::parse(input);
+    let graph = map.to_graph(false);
+    graph.solve()
+}
 
 #[cfg(test)]
 mod tests {
@@ -166,15 +280,15 @@ mod tests {
         assert_eq!(result, 1966);
     }
 
-    // #[test]
-    // fn test_part2_example() {
-    //     let result = part2(&read_example_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_example() {
+        let result = part2(&read_example_file());
+        assert_eq!(result, 154);
+    }
 
-    // #[test]
-    // fn test_part2_solution() {
-    //     let result = part2(&read_input_file());
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_solution() {
+        let result = part2(&read_input_file());
+        assert_eq!(result, 6286);
+    }
 }
