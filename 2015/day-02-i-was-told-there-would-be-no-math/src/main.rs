0e2c79c8b97f7563cc94998388b21d3ac83a6530
Ken Pratt <ken@kenpratt.net>
2024-01-16 11:05:44 -0800

    2015 day 2 part 2

--- 2015/day-02-i-was-told-there-would-be-no-math/src/main.rs
+++ 2015/day-02-i-was-told-there-would-be-no-math/src/main.rs
@@ -7,7 +7,7 @@ const INPUT_FILE: &'static str = "input.txt";
 
 fn main() {
     println!("part 1 result: {:?}", part1(&read_file(INPUT_FILE)));
-    // println!("part 2 result: {:?}", part2(&read_file(INPUT_FILE)));
+    println!("part 2 result: {:?}", part2(&read_file(INPUT_FILE)));
 }
 
 fn read_file(filename: &str) -> String {
@@ -32,9 +32,15 @@ impl Present {
         }
 
         let caps = PRESENT_RE.captures(input).unwrap();
-        let length = caps.get(1).unwrap().as_str().parse::<usize>().unwrap();
-        let width = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
-        let height = caps.get(3).unwrap().as_str().parse::<usize>().unwrap();
+        let mut nums_iter = caps
+            .iter()
+            .skip(1)
+            .flat_map(|c| c.unwrap().as_str().parse::<usize>());
+
+        let length = nums_iter.next().unwrap();
+        let width = nums_iter.next().unwrap();
+        let height = nums_iter.next().unwrap();
+        assert_eq!(None, nums_iter.next());
 
         Self {
             length,
@@ -47,9 +53,25 @@ impl Present {
         let top = self.length * self.width;
         let front = self.width * self.height;
         let side = self.height * self.length;
-        let smallest = cmp::min(top, cmp::min(front, side));
+        let smallest = min(top, front, side);
         top * 2 + front * 2 + side * 2 + smallest
     }
+
+    fn ribbon_needed(&self) -> usize {
+        let top = 2 * (self.length + self.width);
+        let front = 2 * (self.width + self.height);
+        let side = 2 * (self.height + self.length);
+        let smallest = min(top, front, side);
+        smallest + self.volume()
+    }
+
+    fn volume(&self) -> usize {
+        self.length * self.width * self.height
+    }
+}
+
+fn min(a: usize, b: usize, c: usize) -> usize {
+    cmp::min(a, cmp::min(b, c))
 }
 
 fn part1(input: &str) -> usize {
@@ -57,8 +79,10 @@ fn part1(input: &str) -> usize {
     presents.iter().map(|p| p.paper_needed()).sum()
 }
 
-// fn part2(input: &str) -> usize {
-// }
+fn part2(input: &str) -> usize {
+    let presents = Present::parse_list(input);
+    presents.iter().map(|p| p.ribbon_needed()).sum()
+}
 
 #[cfg(test)]
 mod tests {
@@ -76,15 +100,15 @@ mod tests {
         assert_eq!(result, 1588178);
     }
 
-    // #[test]
-    // fn test_part2_example() {
-    //     let result = part2(&read_file(EXAMPLE_FILE));
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_examples() {
+        assert_eq!(part2(&"2x3x4"), 34);
+        assert_eq!(part2(&"1x1x10"), 14);
+    }
 
-    // #[test]
-    // fn test_part2_solution() {
-    //     let result = part2(&read_file(INPUT_FILE));
-    //     assert_eq!(result, 0);
-    // }
+    #[test]
+    fn test_part2_solution() {
+        let result = part2(&read_file(INPUT_FILE));
+        assert_eq!(result, 3783758);
+    }
 }
