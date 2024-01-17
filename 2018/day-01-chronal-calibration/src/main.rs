5ec01b67832f180d7611bfcc786c2074d07579cb
Ken Pratt <ken@kenpratt.net>
2024-01-17 11:41:49 -0800

    2018 day 1

--- 2018/day-01-chronal-calibration/src/main.rs
+++ 2018/day-01-chronal-calibration/src/main.rs
@@ -1,33 +1,78 @@
-use std::fs;
-use std::collections::HashSet;
+use std::{collections::HashSet, fs};
+
+const INPUT_FILE: &'static str = "input.txt";
 
 fn main() {
-    // read input & split into lines
-    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    let lines = contents.lines();
-
-    let numbers: Vec<i32> = lines.map(|s| s.parse::<i32>().expect("Couldn't parse string as an integer")).collect();
-
-    println!("part 1:\n{}", find_sum(&numbers));
-    println!("part 2:\n{}", find_first_repeat(&numbers));
+    println!("part 1 result: {:?}", part1(&read_file(INPUT_FILE)));
+    println!("part 2 result: {:?}", part2(&read_file(INPUT_FILE)));
 }
 
-fn find_sum(numbers: &[i32]) -> i32 {
-    return numbers.into_iter().fold(0, |acc, x| acc + x);
+fn read_file(filename: &str) -> String {
+    fs::read_to_string(filename).expect("Something went wrong reading the file")
 }
 
-fn find_first_repeat(numbers: &[i32]) -> i32 {
-    let mut seen = HashSet::new();
+#[derive(Debug)]
+struct Change(i32);
+
+fn parse_list(input: &str) -> Vec<i32> {
+    input
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .collect()
+}
+
+fn part1(input: &str) -> i32 {
+    let changes = parse_list(input);
+    changes.into_iter().sum()
+}
+
+fn part2(input: &str) -> i32 {
+    let changes = parse_list(input);
+
     let mut curr = 0;
-
-    loop {
-        for n in numbers {
-            curr += n;
-            if seen.contains(&curr) {
-                return curr;
-            } else {
-                seen.insert(curr.clone());
-            }
+    let mut seen = HashSet::from([curr]);
+    for c in changes.iter().cycle() {
+        let next = curr + c;
+        if seen.contains(&next) {
+            return next;
+        } else {
+            seen.insert(next);
+            curr = next;
         }
+    }
+    panic!("Unreachable");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1_examples() {
+        assert_eq!(part1(&"+1\n-2\n+3\n+1"), 3);
+        assert_eq!(part1(&"+1\n+1\n+1"), 3);
+        assert_eq!(part1(&"+1\n+1\n-2"), 0);
+        assert_eq!(part1(&"-1\n-2\n-3"), -6);
+    }
+
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_file(INPUT_FILE));
+        assert_eq!(result, 472);
+    }
+
+    #[test]
+    fn test_part2_examples() {
+        assert_eq!(part2(&"+1\n-2\n+3\n+1"), 2);
+        assert_eq!(part2(&"+1\n-1"), 0);
+        assert_eq!(part2(&"+3\n+3\n+4\n-2\n-4"), 10);
+        assert_eq!(part2(&"-6\n+3\n+8\n+5\n-6"), 5);
+        assert_eq!(part2(&"+7\n+7\n-2\n-7\n-4"), 14);
+    }
+
+    #[test]
+    fn test_part2_solution() {
+        let result = part2(&read_file(INPUT_FILE));
+        assert_eq!(result, 66932);
     }
 }
