f740fd2ebbfc15ddae3d160ebedc806bae6e772b
Ken Pratt <ken@kenpratt.net>
2024-01-18 14:02:59 -0800

    2018 day 5

--- 2018/day-05-alchemical-reduction/src/main.rs
+++ 2018/day-05-alchemical-reduction/src/main.rs
@@ -1,77 +1,129 @@
 use std::fs;
 
+const INPUT_FILE: &'static str = "input.txt";
+
 fn main() {
-    // read input & split into lines
-    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    let lines: Vec<&str> = contents.lines().collect();
-
-    assert_eq!(lines.len(), 1);
-    let s = lines.first().unwrap().to_string();
-
-    let part1 = part1(s.clone());
-    println!("part 1: {:?}", part1);
-
-    let part2 = part2(s.clone());
-    println!("part 2: {:?}", part2);
+    println!("part 1 result: {:?}", part1(&read_file(INPUT_FILE)));
+    println!("part 2 result: {:?}", part2(&read_file(INPUT_FILE)));
 }
 
-fn part1(s: String) -> usize {
-    let mut done = false;
-    let mut curr = s;
-    while !done {
-        let (next, num_reductions) = reduce(curr);
-        curr = next;
-        done = num_reductions == 0;
+fn read_file(filename: &str) -> String {
+    fs::read_to_string(filename).expect("Something went wrong reading the file")
+}
+
+struct Solver<'a> {
+    chars: &'a [char],
+    len: usize,
+    to_ignore: Option<(char, char)>,
+}
+
+impl Solver<'_> {
+    fn length_after_reduction(chars: &[char], ignore: Option<&char>) -> usize {
+        let len = chars.len();
+        let to_ignore = ignore.map(|c: &char| (c.to_ascii_lowercase(), c.to_ascii_uppercase()));
+        let solver = Solver {
+            chars,
+            len,
+            to_ignore,
+        };
+        solver.solve()
     }
-    return curr.len();
-}
 
-fn reduce(s: String) -> (String, u32) {
-    let mut iter = s.chars().peekable();
+    fn solve(&self) -> usize {
+        let mut alive = vec![];
+        let mut i = self.advance_if_needed(0);
+        let mut j = self.advance(i);
 
-    let mut out = vec![];
-    let mut reductions = 0;
-
-    let mut done = false;
-    while !done {
-        match iter.next() {
-            Some(curr) => {
-                match iter.peek() {
-                    Some(next) => {
-                        let reduce = curr.is_ascii_lowercase() != next.is_ascii_lowercase() &&
-                            curr.to_ascii_lowercase() == next.to_ascii_lowercase();
-
-                        if reduce {
-                            // skip next
-                            iter.next();
-                            reductions += 1;
-                        } else {
-                            // add curr to output, leaving next for the next iteration
-                            out.push(curr);
-                        }
-                    },
+        while j < self.len {
+            if Self::should_react(&self.chars[i], &self.chars[j]) {
+                // reduction, need to remove the two current chars
+                match alive.pop() {
+                    Some(prev) => {
+                        // normal case, we  have something to go back to
+                        i = prev; // go back to previous char on the left
+                        j = self.advance(j); // advance to next char on the right
+                    }
                     None => {
-                        // second to last char in the string, just append it
-                        out.push(curr);
-                    },
+                        // special case, reducing stuff on the left side, jump ahead
+                        i = self.advance(j);
+                        j = self.advance(i);
+                    }
                 }
-            },
-            None => {
-                // end of the string
-                done = true;
-            },
+            } else {
+                // no reduction, advance to next char and store last seen left
+                alive.push(i);
+                i = j;
+                j = self.advance(j);
+            }
+        }
+        alive.len() + 1 // add one for the straggler at the end
+    }
+
+    fn advance(&self, i: usize) -> usize {
+        self.advance_if_needed(i + 1)
+    }
+
+    fn advance_if_needed(&self, start: usize) -> usize {
+        let mut i = start;
+        while i < self.len && self.should_ignore(&self.chars[i]) {
+            i += 1;
+        }
+        i
+    }
+
+    fn should_react(l: &char, r: &char) -> bool {
+        // 32 is diff between upper and lower case in ascii table
+        (*l as i16 - *r as i16).abs() == 32
+    }
+
+    fn should_ignore(&self, c: &char) -> bool {
+        match &self.to_ignore {
+            Some((l, u)) => c == l || c == u,
+            None => false,
         }
     }
-
-    (out.into_iter().collect::<String>(), reductions)
 }
 
-fn part2(s: String) -> usize {
-    let ascii_iter = (0..26).map(|x| (x + 'a' as u8) as char);
-
-    return ascii_iter.map(|c| part1(strip(s.clone(), c))).min().unwrap();
+fn part1(input: &str) -> usize {
+    let chars: Vec<char> = input.chars().collect();
+    Solver::length_after_reduction(&chars, None)
 }
 
-fn strip(s: String, c: char) -> String {
-    return s.chars().filter(|x| x.to_ascii_lowercase() != c).into_iter().collect::<String>();
+fn part2(input: &str) -> usize {
+    let chars: Vec<char> = input.chars().collect();
+    ('a'..='z')
+        .map(|ignore| Solver::length_after_reduction(&chars, Some(&ignore)))
+        .min()
+        .unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_FILE: &'static str = "example.txt";
+
+    #[test]
+    fn test_part1_examples() {
+        let result = part1(&read_file(EXAMPLE_FILE));
+        assert_eq!(result, 10);
+    }
+
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_file(INPUT_FILE));
+        assert_eq!(result, 11476);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let result = part2(&read_file(EXAMPLE_FILE));
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_part2_solution() {
+        let result = part2(&read_file(INPUT_FILE));
+        assert_eq!(result, 5446);
+    }
 }
