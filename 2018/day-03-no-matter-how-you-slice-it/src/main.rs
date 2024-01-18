5a39cc6906dfffa9077bbe0d07a3332d3bd28d06
Ken Pratt <ken@kenpratt.net>
2024-01-17 16:00:36 -0800

    2018 day 3

--- 2018/day-03-no-matter-how-you-slice-it/src/main.rs
+++ 2018/day-03-no-matter-how-you-slice-it/src/main.rs
@@ -1,109 +1,219 @@
-extern crate regex;
+use std::{
+    collections::BTreeSet,
+    fs,
+    ops::{Range, RangeInclusive},
+};
 
-use std::fs;
+use lazy_static::lazy_static;
 use regex::Regex;
 
+const INPUT_FILE: &'static str = "input.txt";
+
 fn main() {
-    // read input & split into lines
-    let contents = fs::read_to_string("input.txt").expect("Something went wrong reading the file");
-    let lines = contents.lines();
+    println!("part 1 result: {:?}", part1(&read_file(INPUT_FILE)));
+    println!("part 2 result: {:?}", part2(&read_file(INPUT_FILE)));
+}
 
-    let claims: Vec<Claim> = lines.map(|l| parse_claim(l)).collect();
-
-    let grid = make_grid(&claims);
-
-    let part1 = grid.v.iter().filter(|&x| (*x) > 1).count();
-    println!("part 1: {}", part1);
-
-    let part2: Vec<&str> = claims.iter().filter(|c| non_overlapping(&c, &grid)).map(|c| c.id).collect();
-    println!("part 2: {:?}", part2);
+fn read_file(filename: &str) -> String {
+    fs::read_to_string(filename).expect("Something went wrong reading the file")
 }
 
 #[derive(Debug)]
-struct Claim<'a> {
-    id: &'a str,
-    x: u32,
-    y: u32,
-    w: u32,
-    h: u32,
+struct Claim {
+    id: u16,
+    x_range: Range<u16>,
+    y_range: Range<u16>,
 }
 
-impl<'a> Claim<'a> {
-    fn r(&self) -> u32 {
-        return self.x + self.w;
+impl Claim {
+    fn parse_list(input: &str) -> Vec<Self> {
+        input.lines().map(|line| Self::parse(line)).collect()
     }
 
-    fn b(&self) -> u32 {
-        return self.y + self.h;
-    }
-}
+    fn parse(input: &str) -> Self {
+        lazy_static! {
+            static ref CLAIM_RE: Regex =
+                Regex::new(r"\A#(\d+) @ (\d+),(\d+): (\d+)x(\d+)\z").unwrap();
+        }
 
-fn parse_claim(line: &str) -> Claim {
-    // "#35 @ 285,967: 23x28"
-    let re = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").expect("failed to match regexp");
-    let m = re.captures(line).expect("failed to unpack re captures");
+        let caps = CLAIM_RE.captures(input).unwrap();
+        let mut nums_iter = caps
+            .iter()
+            .skip(1)
+            .flat_map(|c| c.unwrap().as_str().parse::<u16>());
 
-    let id = m.get(1).map_or("", |m| m.as_str());
-    let x: u32 = m.get(2).map_or("", |m| m.as_str()).parse().expect("failed to convert x to int");
-    let y: u32 = m.get(3).map_or("", |m| m.as_str()).parse().expect("failed to convert y to int");
-    let w: u32 = m.get(4).map_or("", |m| m.as_str()).parse().expect("failed to convert w to int");
-    let h: u32 = m.get(5).map_or("", |m| m.as_str()).parse().expect("failed to convert h to int");
+        let id = nums_iter.next().unwrap();
+        let left = nums_iter.next().unwrap();
+        let top = nums_iter.next().unwrap();
+        let width = nums_iter.next().unwrap();
+        let height = nums_iter.next().unwrap();
+        assert_eq!(nums_iter.next(), None);
 
-    return Claim {
-        id: id,
-        x: x,
-        y: y,
-        w: w,
-        h: h,
+        let x_range = left..left + width;
+        let y_range = top..top + height;
+
+        Self {
+            id,
+            x_range,
+            y_range,
+        }
     }
 }
 
 #[derive(Debug)]
 struct Grid {
-    w: u32,
-    h: u32,
-    v: Vec<u8>,
+    x_ranges: Vec<Range<u16>>,
+    y_ranges: Vec<Range<u16>>,
+    tiles: Vec<Vec<u16>>,
+    width: usize,
+    height: usize,
 }
 
-fn make_grid(claims: &[Claim]) -> Grid {
-    let vw = claims.iter().map(|c| c.r()).max().expect("failed to get right boundary");
-    let vh = claims.iter().map(|c| c.b()).max().expect("failed to get bottom boundary");
-    let vs: usize = (vw * vh) as usize;
+impl Grid {
+    fn build(claims: &[Claim]) -> Self {
+        let x_ranges = Self::calculate_ranges(claims.iter().map(|c| &c.x_range));
+        let y_ranges = Self::calculate_ranges(claims.iter().map(|c| &c.y_range));
 
-    println!("vw: {:?}", vw);
-    println!("vh: {:?}", vh);
-    println!("vs: {:?}", vs);
+        let width = x_ranges.len();
+        let height = y_ranges.len();
 
-    let mut v: Vec<u8> = vec![0; vs];
+        let tiles = vec![vec![]; width * height];
 
-    for claim in claims {
-        //println!("boop: {:?} {} {}", claim, claim.r(), claim.b());
-        for dy in claim.y..claim.b() {
-            for dx in claim.x..claim.r() {
-                let i = ((dy * vw) + dx) as usize;
-                //println!("foo: {:?} {:?} {:?} {:?}", claim, dx, dy, i);
-                v[i] += 1;
+        // blank grid
+        let mut grid = Self {
+            x_ranges,
+            y_ranges,
+            tiles,
+            width,
+            height,
+        };
+
+        // actually insert the claim ids now
+        for claim in claims {
+            grid.insert(claim);
+        }
+
+        grid
+    }
+
+    fn calculate_ranges<'a, I>(input: I) -> Vec<Range<u16>>
+    where
+        I: Iterator<Item = &'a Range<u16>>,
+    {
+        let mut splits = BTreeSet::new();
+        for range in input {
+            splits.insert(range.start);
+            splits.insert(range.end);
+        }
+
+        let splits_vec: Vec<u16> = splits.into_iter().collect();
+        splits_vec.windows(2).map(|v| (v[0]..v[1])).collect()
+    }
+
+    fn range_indices(ranges: &Vec<Range<u16>>, target: &Range<u16>) -> RangeInclusive<usize> {
+        let start_index = ranges
+            .binary_search_by_key(&target.start, |r| r.start)
+            .unwrap();
+        let end_index = ranges.binary_search_by_key(&target.end, |r| r.end).unwrap();
+        start_index..=end_index
+    }
+
+    fn tile_index(&self, x: usize, y: usize) -> usize {
+        self.width * y + x
+    }
+
+    fn insert(&mut self, claim: &Claim) {
+        let x_indices = Self::range_indices(&self.x_ranges, &claim.x_range);
+        let y_indices = Self::range_indices(&self.y_ranges, &claim.y_range);
+
+        for y in y_indices {
+            for x in x_indices.clone() {
+                let i = self.tile_index(x, y);
+                self.tiles[i].push(claim.id);
             }
         }
     }
 
-    return Grid {
-        w: vw,
-        h: vh,
-        v: v,
-    };
-}
+    fn area_with_overlap(&self) -> usize {
+        let mut area = 0;
 
-fn non_overlapping(claim: &Claim, grid: &Grid) -> bool {
-    for dy in claim.y..claim.b() {
-        for dx in claim.x..claim.r() {
-            let i = ((dy * grid.w) + dx) as usize;
-            //println!("foo: {:?} {:?} {:?} {:?} {:?}", claim, dx, dy, i, grid.v[i]);
-            if grid.v[i] > 1 {
-                return false;
+        for y in 0..self.height {
+            let y_range = &self.y_ranges[y];
+            for x in 0..self.width {
+                let x_range = &self.x_ranges[x];
+                let i = self.tile_index(x, y);
+                let tile = &self.tiles[i];
+                if tile.len() >= 2 {
+                    area += x_range.len() * y_range.len();
+                }
             }
         }
+
+        area
     }
 
-    return true;
+    fn is_non_overlapping(&self, claim: &Claim) -> bool {
+        let x_indices = Self::range_indices(&self.x_ranges, &claim.x_range);
+        let y_indices = Self::range_indices(&self.y_ranges, &claim.y_range);
+
+        for y in y_indices {
+            for x in x_indices.clone() {
+                let i = self.tile_index(x, y);
+                let tile = &self.tiles[i];
+                if tile.len() >= 2 {
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
+}
+
+fn part1(input: &str) -> usize {
+    let claims = Claim::parse_list(input);
+    let grid = Grid::build(&claims);
+    grid.area_with_overlap()
+}
+
+fn part2(input: &str) -> u16 {
+    let claims = Claim::parse_list(input);
+    let grid = Grid::build(&claims);
+    let non_overlapping: Vec<&Claim> = claims
+        .iter()
+        .filter(|c| grid.is_non_overlapping(c))
+        .collect();
+    assert_eq!(non_overlapping.len(), 1);
+    non_overlapping.first().unwrap().id
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_FILE: &'static str = "example.txt";
+
+    #[test]
+    fn test_part1_example() {
+        let result = part1(&read_file(EXAMPLE_FILE));
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn test_part1_solution() {
+        let result = part1(&read_file(INPUT_FILE));
+        assert_eq!(result, 98005);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let result = part2(&read_file(EXAMPLE_FILE));
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn test_part2_solution() {
+        let result = part2(&read_file(INPUT_FILE));
+        assert_eq!(result, 331);
+    }
 }
