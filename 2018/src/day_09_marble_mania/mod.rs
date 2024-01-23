2fec03c7246350d57fe19f0ea2c9c9eed66f98c6
Ken Pratt <ken@kenpratt.net>
2024-01-23 10:33:05 -0800

    2018 day 9 optimization

--- 2018/src/day_09_marble_mania/mod.rs
+++ 2018/src/day_09_marble_mania/mod.rs
@@ -32,120 +32,163 @@ impl Parameters {
     }
 }
 
+// my Game implementation uses a fixed vec for the marbles with the full final tally of marbles
+// with pointers to the currently used window within the vec, that shifts to the right and grows
+// over time
 #[derive(Debug)]
 struct Game {
-    neighbours: Vec<(usize, usize)>,
-    current_marble: usize,
+    num_rounds: usize,
+    num_marbles: usize,
+    marbles: Vec<usize>,
+    start_index: usize,
+    end_index: usize,
     scores: Vec<usize>,
-    existing: [usize; 23], // used as temp storage for marble insertion calculation
 }
 
 impl Game {
-    fn new(parameters: &Parameters) -> Self {
-        Self {
-            current_marble: 0,
-            neighbours: vec![(0, 0); parameters.last_marble + 1],
-            scores: vec![0; parameters.num_players],
-            existing: [0; 23],
-        }
-    }
+    // game state after round 1, with cursor at index 0
+    const AFTER_ONE_ROUND: [usize; 22] = [
+        19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15, 0, 16, 8, 17, 4, 18,
+    ];
 
     fn play(parameters: &Parameters) -> usize {
-        let mut game = Self::new(parameters);
-        let last_scoring_round = parameters.last_marble / 23;
+        let mut game = Self::after_one_round(parameters);
 
-        // place first round
-        game.place_first_round();
-
-        // now batch process each remaining round
-        for round in 2..=last_scoring_round {
-            let marble = round * 23;
-            let player_id = (marble - 1) % parameters.num_players;
-            game.place_subsequent_round(round, marble, player_id);
+        // now process each remaining round
+        for round in 2..=game.num_rounds {
+            let player_id = (round - 1) % parameters.num_players;
+            game.place_subsequent_round(round, player_id);
         }
 
         game.high_score()
+    }
+
+    // hard-code the first round, to simplify things
+    fn after_one_round(parameters: &Parameters) -> Self {
+        let num_rounds = parameters.last_marble / 23;
+
+        // we start with one marble, add 21 per round
+        let num_marbles = num_rounds * 21 + 1;
+
+        let mut marbles = vec![0; num_marbles];
+        for (i, marble) in Self::AFTER_ONE_ROUND.iter().enumerate() {
+            marbles[i] = *marble;
+        }
+        let start_index = 0;
+        let end_index = Self::AFTER_ONE_ROUND.len();
+
+        let mut scores = vec![0; parameters.num_players];
+        scores[0] = 32; // 23 plus first round removal, 9
+
+        Self {
+            num_rounds,
+            num_marbles,
+            marbles,
+            start_index,
+            end_index,
+            scores,
+        }
     }
 
     fn high_score(&self) -> usize {
         *self.scores.iter().max().unwrap()
     }
 
-    fn place_first_round(&mut self) {
-        // for the first round, we can't use the logic where we pre-calculate the
-        // existing marbles, because there aren't enough marbles yet - placing the
-        // new marbles will be neighbours to each other
-
-        // place 22 marbles
-        for i in 0..22 {
-            let marble_id = i + 1;
-
-            // find the marbles to insert between
-            let left = self.neighbours[self.current_marble].1;
-            let right = self.neighbours[left].1;
-
-            // insert marble between left and right
-            self.neighbours[left].1 = marble_id;
-            self.neighbours[right].0 = marble_id;
-            self.neighbours[marble_id] = (left, right);
-
-            self.current_marble = marble_id;
-        }
-
-        // now delete one
-
-        // find the target, the 7th marble counter-clockwise, and the marbles to the left/right of it
-        let right = (0..6).fold(self.current_marble, |last, _| self.neighbours[last].0);
-        let target = self.neighbours[right].0;
-        let left = self.neighbours[target].0;
-
-        // remove the target
-        self.neighbours[left].1 = right;
-        self.neighbours[right].0 = left;
-
-        self.current_marble = right;
-        self.scores[0] += 23 + target;
+    #[inline]
+    fn from_start(&self, offset: usize) -> usize {
+        self.from(self.start_index, offset)
     }
 
-    fn place_subsequent_round(&mut self, round: usize, marble_id: usize, player_id: usize) {
-        self.update_existing();
-
-        // place the 22 new marbles, and remove existing[18]
-        let first_marble = (round - 1) * 23 + 1;
-        for i in 0..22 {
-            // special case around 17-18:
-            // existing[18] is being removed, so the new 17 & 18 will point at each other
-            let prev = if i == 18 {
-                first_marble + 17
-            } else {
-                self.existing[i]
-            };
-            let next = if i == 17 {
-                first_marble + 18
-            } else {
-                self.existing[i + 1]
-            };
-
-            let marble = first_marble + i;
-
-            // insert the marble between prev & next
-            self.neighbours[next].0 = marble;
-            self.neighbours[prev].1 = marble;
-            self.neighbours[marble] = (prev, next);
-        }
-
-        self.current_marble = first_marble + 18;
-        self.scores[player_id] += marble_id + self.existing[18];
+    #[inline]
+    fn from_end(&self, offset: usize) -> usize {
+        self.from(self.end_index, offset)
     }
 
-    fn update_existing(&mut self) {
-        // we're going to need the next 23 marbles
-        let mut last = self.current_marble;
-        for i in 0..23 {
-            let next = self.neighbours[last].1;
-            self.existing[i] = next;
-            last = next;
+    #[inline]
+    fn before_start(&self, offset: usize) -> usize {
+        self.before(self.start_index, offset)
+    }
+
+    #[inline]
+    fn from(&self, index: usize, offset: usize) -> usize {
+        let res = index + offset;
+        if res >= self.num_marbles {
+            res - self.num_marbles
+        } else {
+            res
         }
+    }
+
+    #[inline]
+    fn before(&self, index: usize, offset: usize) -> usize {
+        if index >= offset {
+            index - offset
+        } else {
+            index + self.num_marbles - offset
+        }
+    }
+
+    fn place_subsequent_round(&mut self, round: usize, player_id: usize) {
+        // this round will look like this (with o = existing element, n = new element added this round)
+        //
+        //  o0, o1, n0, o2, n1, o3, ... o17, n16, o18, n17 | n18, o20, n19, o21, n20, o22, n21, o23, o24, ...
+        //   0,  1,  2,  3,  4,  5, ...  33,  34,  35,  36 |  37,  38,  39,  40,  41,  42,  43,  44,  45, ...
+        //
+        // the dividing line is where o19 is removed, and where the new cursor (beginning of the window) will be.
+        // everything after the line should be prepended to the queue, and before the line appended
+
+        let first_new_marble = (round - 1) * 23 + 1;
+
+        // initial marble memory for round two:
+        //   19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15, 0, 16, 8, 17, 4, 18, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -,
+
+        // swap first marble to the back:
+        //   -, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15, 0, 16, 8, 17, 4, 18, 19, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -,
+        self.marbles.swap(self.start_index, self.end_index);
+        self.start_index = self.from_start(1);
+        self.end_index = self.from_end(1);
+
+        // place 18 marbles (24-41), interspersing 18 old ones (swapping them in)
+        //   -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, 17, 4, 18, 19, 2, 24, 20, 25, 10, 26, 21, 27, 5, 28, 22, 29, 11, 30, 1, 31, 12, 32, 6, 33, 13, 34, 3, 35, 14, 36, 7, 37, 15, 38, 0, 39, 16, 40, 8, 41, -, -, -,
+        for i in 0..18 {
+            let new_marble = first_new_marble + i;
+
+            let existing_marble_prev_index = self.from_start(i);
+            let existing_marble_new_index = self.from_end(i * 2);
+            let new_marble_index = self.from_end(i * 2 + 1);
+
+            self.marbles
+                .swap(existing_marble_prev_index, existing_marble_new_index);
+            self.marbles[new_marble_index] = new_marble;
+        }
+        self.start_index = self.from_start(18);
+        self.end_index = self.from_end(36);
+
+        // remove first marble
+        //      -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, 4, 18, 19, 2, 24, 20, 25, 10, 26, 21, 27, 5, 28, 22, 29, 11, 30, 1, 31, 12, 32, 6, 33, 13, 34, 3, 35, 14, 36, 7, 37, 15, 38, 0, 39, 16, 40, 8, 41, -, -, -,
+        let removed = self.marbles[self.start_index];
+        self.start_index = self.from_start(1);
+
+        // place remaining 4 marbles (42-45), interspersing 3 old ones, at the front!
+        // first need to make room by pulling the next three backwards
+        //      -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, 42, 4, 43, 18, 44, 19, 45, 2, 24, 20, 25, 10, 26, 21, 27, 5, 28, 22, 29, 11, 30, 1, 31, 12, 32, 6, 33, 13, 34, 3, 35, 14, 36, 7, 37, 15, 38, 0, 39, 16, 40, 8, 41, -, -, -,
+        let new_start_index = self.before_start(4);
+        for i in 0..4 {
+            let new_marble = first_new_marble + i + 18;
+
+            if i < 4 {
+                let existing_marble_prev_index = self.from_start(i);
+                let existing_marble_new_index = self.from(new_start_index, i * 2 + 1);
+                self.marbles
+                    .swap(existing_marble_prev_index, existing_marble_new_index);
+            }
+
+            let new_marble_index = self.from(new_start_index, i * 2);
+            self.marbles[new_marble_index] = new_marble;
+        }
+        self.start_index = new_start_index;
+
+        self.scores[player_id] += round * 23 + removed;
     }
 }
 
