a6193199ef816e1e732b1b6725d14b50fbb619a8
Ken Pratt <ken@kenpratt.net>
2024-01-23 10:51:58 -0800

    2018 day 9 optimization

--- 2018/src/day_09_marble_mania/mod.rs
+++ 2018/src/day_09_marble_mania/mod.rs
@@ -11,8 +11,8 @@ pub fn run() {
 
 #[derive(Debug)]
 struct Parameters {
-    num_players: usize,
-    last_marble: usize,
+    num_players: u32,
+    last_marble: u32,
 }
 
 impl Parameters {
@@ -23,8 +23,8 @@ impl Parameters {
         }
 
         let caps = ITEM_RE.captures(input).unwrap();
-        let num_players = caps.get(1).unwrap().as_str().parse::<usize>().unwrap();
-        let last_marble = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
+        let num_players = caps.get(1).unwrap().as_str().parse::<u32>().unwrap();
+        let last_marble = caps.get(2).unwrap().as_str().parse::<u32>().unwrap();
         Self {
             num_players,
             last_marble,
@@ -37,26 +37,26 @@ impl Parameters {
 // over time
 #[derive(Debug)]
 struct Game {
-    num_rounds: usize,
+    num_rounds: u32,
     num_marbles: usize,
-    marbles: Vec<usize>,
+    marbles: Vec<u32>,
     start_index: usize,
     end_index: usize,
-    scores: Vec<usize>,
+    scores: Vec<u32>,
 }
 
 impl Game {
     // game state after round 1, with cursor at index 0
-    const AFTER_ONE_ROUND: [usize; 22] = [
+    const AFTER_ONE_ROUND: [u32; 22] = [
         19, 2, 20, 10, 21, 5, 22, 11, 1, 12, 6, 13, 3, 14, 7, 15, 0, 16, 8, 17, 4, 18,
     ];
 
-    fn play(parameters: &Parameters) -> usize {
+    fn play(parameters: &Parameters) -> u32 {
         let mut game = Self::after_one_round(parameters);
 
         // now process each remaining round
         for round in 2..=game.num_rounds {
-            let player_id = (round - 1) % parameters.num_players;
+            let player_id = ((round - 1) % parameters.num_players) as usize;
             game.place_subsequent_round(round, player_id);
         }
 
@@ -68,7 +68,7 @@ impl Game {
         let num_rounds = parameters.last_marble / 23;
 
         // we start with one marble, add 21 per round
-        let num_marbles = num_rounds * 21 + 1;
+        let num_marbles = (num_rounds * 21 + 1) as usize;
 
         let mut marbles = vec![0; num_marbles];
         for (i, marble) in Self::AFTER_ONE_ROUND.iter().enumerate() {
@@ -77,7 +77,7 @@ impl Game {
         let start_index = 0;
         let end_index = Self::AFTER_ONE_ROUND.len();
 
-        let mut scores = vec![0; parameters.num_players];
+        let mut scores = vec![0; parameters.num_players as usize];
         scores[0] = 32; // 23 plus first round removal, 9
 
         Self {
@@ -90,7 +90,7 @@ impl Game {
         }
     }
 
-    fn high_score(&self) -> usize {
+    fn high_score(&self) -> u32 {
         *self.scores.iter().max().unwrap()
     }
 
@@ -128,7 +128,7 @@ impl Game {
         }
     }
 
-    fn place_subsequent_round(&mut self, round: usize, player_id: usize) {
+    fn place_subsequent_round(&mut self, round: u32, player_id: usize) {
         // this round will look like this (with o = existing element, n = new element added this round)
         //
         //  o0, o1, n0, o2, n1, o3, ... o17, n16, o18, n17 | n18, o20, n19, o21, n20, o22, n21, o23, o24, ...
@@ -150,8 +150,9 @@ impl Game {
 
         // place 18 marbles (24-41), interspersing 18 old ones (swapping them in)
         //   -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, 17, 4, 18, 19, 2, 24, 20, 25, 10, 26, 21, 27, 5, 28, 22, 29, 11, 30, 1, 31, 12, 32, 6, 33, 13, 34, 3, 35, 14, 36, 7, 37, 15, 38, 0, 39, 16, 40, 8, 41, -, -, -,
-        for i in 0..18 {
-            let new_marble = first_new_marble + i;
+        for n in 0..18 {
+            let new_marble = first_new_marble + n;
+            let i = n as usize;
 
             let existing_marble_prev_index = self.from_start(i);
             let existing_marble_new_index = self.from_end(i * 2);
@@ -173,8 +174,9 @@ impl Game {
         // first need to make room by pulling the next three backwards
         //      -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, -, 42, 4, 43, 18, 44, 19, 45, 2, 24, 20, 25, 10, 26, 21, 27, 5, 28, 22, 29, 11, 30, 1, 31, 12, 32, 6, 33, 13, 34, 3, 35, 14, 36, 7, 37, 15, 38, 0, 39, 16, 40, 8, 41, -, -, -,
         let new_start_index = self.before_start(4);
-        for i in 0..4 {
-            let new_marble = first_new_marble + i + 18;
+        for n in 0..4 {
+            let new_marble = first_new_marble + n + 18;
+            let i = n as usize;
 
             if i < 4 {
                 let existing_marble_prev_index = self.from_start(i);
@@ -196,11 +198,11 @@ fn parse(input: &str) -> Parameters {
     Parameters::parse(input)
 }
 
-fn part1(parameters: &Parameters) -> usize {
+fn part1(parameters: &Parameters) -> u32 {
     Game::play(parameters)
 }
 
-fn part2(parameters: &Parameters) -> usize {
+fn part2(parameters: &Parameters) -> u32 {
     let modified_parameters = Parameters {
         num_players: parameters.num_players,
         last_marble: parameters.last_marble * 100,
@@ -212,18 +214,18 @@ fn part2(parameters: &Parameters) -> usize {
 mod tests {
     use super::*;
 
-    fn parse_examples(examples: &str) -> Vec<(Parameters, usize)> {
+    fn parse_examples(examples: &str) -> Vec<(Parameters, u32)> {
         examples.lines().map(|line| parse_example(line)).collect()
     }
 
-    fn parse_example(example: &str) -> (Parameters, usize) {
+    fn parse_example(example: &str) -> (Parameters, u32) {
         lazy_static! {
             static ref EXAMPLE_RE: Regex = Regex::new(r"\A(.*): high score is (\d+)\z").unwrap();
         }
 
         let caps = EXAMPLE_RE.captures(example).unwrap();
         let input = parse(caps.get(1).unwrap().as_str());
-        let result = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
+        let result = caps.get(2).unwrap().as_str().parse::<u32>().unwrap();
         (input, result)
     }
 
