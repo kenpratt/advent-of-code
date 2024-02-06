8dd002297d2d53aa0587bb7d04b8e8b05f2758b6
Ken Pratt <ken@kenpratt.net>
2024-02-06 08:47:47 -0800

    add command to benchmark and record median runtime

--- common/src/main.rs
+++ common/src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::BTreeMap;
 use std::env;
+use std::fs::File;
+use std::io::Write;
 use std::time::{Duration, Instant};
 
 use advent_of_code::days;
@@ -18,8 +21,14 @@ fn main() {
 
     match args.get(0).map(|s| s.as_str()) {
         // run all
-        None | Some("all") => {
-            assert!(args.len() <= 1);
+        None => {
+            assert!(args.len() == 0);
+            run_all(&timer)
+        }
+
+        // run all
+        Some("all") => {
+            assert!(args.len() == 1);
             run_all(&timer)
         }
 
@@ -27,8 +36,12 @@ fn main() {
         Some(year_s) if YEAR_RE.is_match(year_s) => {
             let year = str_to_usize(year_s);
             match args.get(1) {
-                None => run_year(year, &timer),
+                None => {
+                    assert!(args.len() == 1);
+                    run_year(year, &timer)
+                }
                 Some(day_s) if DAY_RE.is_match(day_s) => {
+                    assert!(args.len() == 2);
                     let day = str_to_usize(day_s);
                     run_day(year, day, &timer)
                 }
@@ -38,6 +51,7 @@ fn main() {
 
         // benchmark a day
         Some("bench") => {
+            assert!(args.len() == 4);
             let year_s = &args[1];
             let day_s = &args[2];
             let times_s = &args[3];
@@ -49,10 +63,17 @@ fn main() {
             benchmark_day(year, day, times, &timer)
         }
 
+        // record timings for all days
+        Some("timings") => {
+            assert!(args.len() == 1);
+            record_timings(&timer)
+        }
+
         Some(_) => panic!("Unexpected args: {:?}", args),
     };
 }
 
+// cargo run --release 2015 4
 fn run_day(year: usize, day: usize, timer: &Instant) -> Duration {
     let run = days::run_fn(year, day).unwrap();
     let start = timer.elapsed();
@@ -62,6 +83,7 @@ fn run_day(year: usize, day: usize, timer: &Instant) -> Duration {
     duration
 }
 
+// cargo run --release 2015
 fn run_year(year: usize, timer: &Instant) -> Duration {
     let total: Duration = days::days_for_year(year)
         .iter()
@@ -71,12 +93,14 @@ fn run_year(year: usize, timer: &Instant) -> Duration {
     total
 }
 
+// cargo run --release
 fn run_all(timer: &Instant) -> Duration {
     let total: Duration = days::YEARS.iter().map(|year| run_year(*year, timer)).sum();
     println!("all years total: {}ms\n", total.as_millis());
     total
 }
 
+// cargo run --release bench 2015 4 50
 fn benchmark_day(year: usize, day: usize, times: usize, timer: &Instant) -> Duration {
     let mut durations: Vec<Duration> = (0..times).map(|_| run_day(year, day, timer)).collect();
     durations.sort();
@@ -91,6 +115,35 @@ fn benchmark_day(year: usize, day: usize, times: usize, timer: &Instant) -> Dura
         durations.last().unwrap().as_millis()
     );
     median
+}
+
+//  cargo run --release timings
+fn record_timings(timer: &Instant) -> Duration {
+    let start = timer.elapsed();
+    let times = 50;
+
+    let timings: BTreeMap<usize, BTreeMap<usize, u128>> = days::YEARS
+        .iter()
+        .map(|year| {
+            (
+                *year,
+                days::days_for_year(*year)
+                    .iter()
+                    .map(|day| (*day, benchmark_day(*year, *day, times, timer).as_millis()))
+                    .collect(),
+            )
+        })
+        .collect();
+
+    let yaml = serde_yaml::to_string(&timings).unwrap();
+
+    let mut output_path = env::current_dir().unwrap();
+    output_path.push("timings.yaml");
+
+    let mut file = File::create(output_path).unwrap();
+    file.write_all(yaml.as_bytes()).unwrap();
+
+    timer.elapsed() - start
 }
 
 fn str_to_usize(s: &str) -> usize {
