6dbe6a708b63a498474c9d67d224840bb146fd67
Ken Pratt <ken@kenpratt.net>
2024-02-06 06:12:31 -0800

    2015 day 6 optimization

--- common/src/y2015/day_06_probably_a_fire_hazard/mod.rs
+++ common/src/y2015/day_06_probably_a_fire_hazard/mod.rs
@@ -1,3 +1,6 @@
+use std::collections::BTreeSet;
+use std::ops::Range;
+
 use crate::file::*;
 use crate::spatial::*;
 
@@ -77,18 +80,106 @@ impl Action {
 }
 
 #[derive(Debug)]
-struct LightArray<V> {
-    grid: Grid<u32, V>,
+struct Windows {
+    x_splits: Vec<u32>,
+    y_splits: Vec<u32>,
+    width: usize,
+    height: usize,
+    sizes: Vec<u32>,
 }
 
-impl<V> LightArray<V> {
-    fn new(initial: V) -> Self
+impl Windows {
+    fn build(instructions: &[Instruction]) -> Self {
+        let x_splits: Vec<u32> = instructions
+            .iter()
+            .fold(BTreeSet::new(), |mut set, inst| {
+                set.insert(inst.start.x);
+                set.insert(inst.end.x + 1);
+                set
+            })
+            .into_iter()
+            .collect();
+
+        let y_splits: Vec<u32> = instructions
+            .iter()
+            .fold(BTreeSet::new(), |mut set, inst| {
+                set.insert(inst.start.y);
+                set.insert(inst.end.y + 1);
+                set
+            })
+            .into_iter()
+            .collect();
+
+        let width = x_splits.len() - 1;
+        let height = y_splits.len() - 1;
+
+        let x_sizes: Vec<u32> = x_splits.windows(2).map(|w| w[1] - w[0]).collect();
+        let y_sizes: Vec<u32> = y_splits.windows(2).map(|w| w[1] - w[0]).collect();
+
+        let sizes = y_sizes
+            .iter()
+            .flat_map(|y| x_sizes.iter().map(|x| *x * *y))
+            .collect();
+
+        Self {
+            x_splits,
+            y_splits,
+            width,
+            height,
+            sizes,
+        }
+    }
+
+    fn range(splits: &[u32], start: u32, end: u32) -> Range<usize> {
+        let from = splits.binary_search_by(|val| val.cmp(&start)).unwrap();
+        let to = match splits[from..].binary_search_by(|val| val.cmp(&(end + 1))) {
+            Ok(index) => from + index,
+            Err(index) => from + index,
+        };
+        from..to
+    }
+
+    fn size(&self) -> usize {
+        self.width * self.height
+    }
+
+    fn size_of(&self, index: usize) -> usize {
+        self.sizes[index] as usize
+    }
+
+    fn apply_each_index<F>(&self, instruction: &Instruction, mut f: F)
+    where
+        F: FnMut(usize),
+    {
+        let x_range = Self::range(&self.x_splits, instruction.start.x, instruction.end.x);
+        let y_range = Self::range(&self.y_splits, instruction.start.y, instruction.end.y);
+
+        for yi in y_range {
+            for xi in x_range.clone() {
+                let i = self.index(xi, yi);
+                f(i);
+            }
+        }
+    }
+
+    fn index(&self, xi: usize, yi: usize) -> usize {
+        yi * self.width + xi
+    }
+}
+
+#[derive(Debug)]
+struct LightArray<'a, V> {
+    windows: &'a Windows,
+    values: Vec<V>,
+}
+
+impl<'a, V> LightArray<'a, V> {
+    fn new(initial: V, windows: &'a Windows) -> Self
     where
         V: Copy,
     {
-        let bounds = Bounds::new(0, 999, 0, 999);
-        let grid = Grid::new_with_intial(bounds, || Some(initial));
-        Self { grid }
+        let values = vec![initial; windows.size()];
+        Self { windows, values }
     }
 
     fn run<F>(&mut self, instructions: &[Instruction], apply_action: F)
@@ -104,36 +195,38 @@ impl<V> LightArray<V> {
     where
         F: Fn(&Action, &V) -> V,
     {
-        for y in instruction.start.y..=instruction.end.y {
-            for x in instruction.start.x..=instruction.end.x {
-                let pos = Coord::new(x, y);
-                let val = self.grid.get_mut(self.grid.coord_to_index(&pos));
-                let new_val = apply_action(&instruction.action, val.as_ref().unwrap());
-                *val = Some(new_val);
-            }
-        }
+        self.windows.apply_each_index(instruction, |i| {
+            let new_val = apply_action(&instruction.action, &self.values[i]);
+            self.values[i] = new_val;
+        });
     }
 
     fn sum_by<F>(&self, f: F) -> usize
     where
         F: Fn(&V) -> usize,
     {
-        self.grid.iter().map(|(_index, v)| f(v)).sum()
+        self.values
+            .iter()
+            .enumerate()
+            .map(|(i, v)| self.windows.size_of(i) * f(v))
+            .sum()
     }
 }
 
-fn parse(input: &str) -> Vec<Instruction> {
-    Instruction::parse_list(input)
+fn parse(input: &str) -> (Vec<Instruction>, Windows) {
+    let instructions = Instruction::parse_list(input);
+    let windows = Windows::build(&instructions);
+    (instructions, windows)
 }
 
-fn part1(instructions: &[Instruction]) -> usize {
-    let mut lights: LightArray<bool> = LightArray::new(false);
+fn part1((instructions, windows): &(Vec<Instruction>, Windows)) -> usize {
+    let mut lights: LightArray<bool> = LightArray::new(false, windows);
     lights.run(instructions, |action, on| action.apply_lit(on));
     lights.sum_by(|b| if *b { 1 } else { 0 })
 }
 
-fn part2(instructions: &[Instruction]) -> usize {
-    let mut lights: LightArray<u8> = LightArray::new(0);
+fn part2((instructions, windows): &(Vec<Instruction>, Windows)) -> usize {
+    let mut lights: LightArray<u8> = LightArray::new(0, windows);
     lights.run(instructions, |action, brightness| {
         action.apply_brightness(brightness)
     });
