da9a0814b0c7ceb4cbecc126e71e0bae27e26f40
Ken Pratt <ken@kenpratt.net>
2024-02-14 19:09:41 -0800

    2018 day 14 part 2

--- common/src/y2018/day_14_chocolate_carts/mod.rs
+++ common/src/y2018/day_14_chocolate_carts/mod.rs
@@ -1,33 +1,63 @@
 use crate::interface::AoC;
 
 pub struct Day;
-impl AoC<usize, String, usize> for Day {
+impl AoC<String, String, usize> for Day {
     const FILE: &'static str = file!();
 
-    fn parse(input: String) -> usize {
-        input.parse::<usize>().unwrap()
+    fn parse(input: String) -> String {
+        input
     }
 
-    fn part1(target: &usize) -> String {
+    fn part1(input: &String) -> String {
+        let target = input.parse::<usize>().unwrap();
+
         // run until size >= target + 10
-        let scores = run_recipes(target + 10, &[3, 7]);
+        let until_size = target + 10;
+        let scores = run_recipes(&[3, 7], |s| s.len() >= until_size);
 
         // return string of the 10 digits starting with target index
-        let r = *target as usize;
+        let r = target as usize;
         scores[r..(r + 10)].iter().map(|d| d.to_string()).collect()
     }
 
-    fn part2(_rounds: &usize) -> usize {
-        0
+    fn part2(input: &String) -> usize {
+        let target: Vec<u8> = input
+            .chars()
+            .map(|c| c.to_digit(10).unwrap() as u8)
+            .collect();
+        let target_len = target.len();
+
+        // run until last chunk matches
+        let last_chunk_matches =
+            |s: &Vec<u8>| s.len() >= target_len && &s[(s.len() - target_len)..] == &target;
+        let second_last_chunk_matches = |s: &Vec<u8>| {
+            s.len() > target_len && &s[(s.len() - target_len - 1)..(s.len() - 1)] == &target
+        };
+
+        let scores = run_recipes(&[3, 7], |s| {
+            last_chunk_matches(s) || second_last_chunk_matches(s)
+        });
+
+        // return length
+        if last_chunk_matches(&scores) {
+            scores.len() - target_len
+        } else if second_last_chunk_matches(&scores) {
+            scores.len() - target_len - 1
+        } else {
+            panic!("Unreachable");
+        }
     }
 }
 
-fn run_recipes(target_len: usize, initial: &[u8; 2]) -> Vec<u8> {
+fn run_recipes<F>(initial: &[u8; 2], stop: F) -> Vec<u8>
+where
+    F: Fn(&Vec<u8>) -> bool,
+{
     let mut scores = initial.to_vec();
 
     let mut i = 0;
     let mut j = 1;
-    while scores.len() < target_len {
+    while !stop(&scores) {
         let si = scores[i];
         let sj = scores[j];
         let s = si + sj;
@@ -52,10 +82,10 @@ mod tests {
 
     #[test]
     fn test_part1_examples() {
-        assert_eq!(Day::part1(&9), "5158916779");
-        assert_eq!(Day::part1(&5), "0124515891");
-        assert_eq!(Day::part1(&18), "9251071085");
-        assert_eq!(Day::part1(&2018), "5941429882");
+        assert_eq!(Day::part1(&"9".to_owned()), "5158916779");
+        assert_eq!(Day::part1(&"5".to_owned()), "0124515891");
+        assert_eq!(Day::part1(&"18".to_owned()), "9251071085");
+        assert_eq!(Day::part1(&"2018".to_owned()), "5941429882");
     }
 
     #[test]
@@ -65,14 +95,16 @@ mod tests {
     }
 
     #[test]
-    fn test_part2_example() {
-        let result = Day::part2(&Day::parse_example_file());
-        assert_eq!(result, 0);
+    fn test_part2_examples() {
+        assert_eq!(Day::part2(&"51589".to_owned()), 9);
+        assert_eq!(Day::part2(&"01245".to_owned()), 5);
+        assert_eq!(Day::part2(&"92510".to_owned()), 18);
+        assert_eq!(Day::part2(&"59414".to_owned()), 2018);
     }
 
     #[test]
     fn test_part2_solution() {
         let result = Day::part2(&Day::parse_input_file());
-        assert_eq!(result, 0);
+        assert_eq!(result, 20253137);
     }
 }
