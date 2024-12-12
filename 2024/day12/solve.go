c5de32e2248b40c29b3a000d99cd3a1f96d9f6f9
Ken Pratt <ken@kenpratt.net>
2024-12-12 13:41:18 -0800

    2024 day 12 part 2

--- 2024/day12/solve.go
+++ 2024/day12/solve.go
@@ -5,19 +5,23 @@ import (
 	"adventofcode/set"
 	"adventofcode/stack"
 	"adventofcode/util"
+
+	"github.com/samber/lo"
 )
 
 func Solve(path string) {
 	inputStr := util.ReadInputFile(path)
 	input := parseInput(inputStr)
 	util.AssertEqual(1452678, part1(input))
-	util.AssertEqual(0, part2(input))
+	util.AssertEqual(873584, part2(input))
 }
 
-func parseInput(input string) grid.Grid[rune] {
-	return grid.Parse(input, func(c rune, _ grid.Coord) rune {
+func parseInput(input string) []Region {
+	plots := grid.Parse(input, func(c rune, _ grid.Coord) rune {
 		return c
 	})
+	solver := MakeSolver(plots)
+	return solver.Regions()
 }
 
 type Solver struct {
@@ -26,14 +30,14 @@ type Solver struct {
 }
 
 type Region struct {
-	kind     rune
-	area     set.Set[grid.Coord]
-	toExpand stack.Stack[grid.Coord]
+	kind      rune
+	area      set.Set[grid.Coord]
+	perimeter set.Set[Edge]
 }
 
 type Edge struct {
-	a grid.Coord
-	b grid.Coord
+	pos    grid.Coord
+	facing grid.Direction
 }
 
 func MakeSolver(plots grid.Grid[rune]) Solver {
@@ -46,68 +50,9 @@ func MakeSolver(plots grid.Grid[rune]) Solver {
 	}
 }
 
-func MakeRegion(kind rune, pos grid.Coord, plots *grid.Grid[rune]) Region {
-	region := Region{kind: kind, area: set.NewSet[grid.Coord](), toExpand: stack.NewStack[grid.Coord](8)}
-	region.Add(pos, plots)
-	return region
-}
-
-func (region *Region) Add(pos grid.Coord, plots *grid.Grid[rune]) {
-	if region.area.Contains(pos) {
-		return
-	}
-	if posKind, _ := plots.At(pos); region.kind != posKind {
-		panic("Trying to add a point to region of a mismatched kind")
-	}
-
-	region.area.Add(pos)
-	for n := range plots.IterNeighbours(pos) {
-		nKind, _ := plots.At(n)
-		if region.kind == nKind {
-			// TODO try recursive?
-			region.toExpand.Push(n)
-		}
-	}
-}
-
-func (region *Region) Area() int {
-	return region.area.Len()
-}
-
-func (region *Region) Perimeter() int {
-	edgeCounts := make(map[Edge]int)
-
-	for pos := range region.area.Iter() {
-		for _, d := range grid.Directions() {
-			n := pos.MoveInDirection(d, 1)
-			switch d {
-			case grid.South, grid.East:
-				edgeCounts[Edge{pos, n}]++
-			case grid.North, grid.West:
-				edgeCounts[Edge{n, pos}]++
-			}
-		}
-	}
-
-	// any shared edge will have an edge count > 1
-	result := 0
-	for _, n := range edgeCounts {
-		if n == 1 {
-			result++
-		}
-	}
-	return result
-}
-
-func (region *Region) Price() int {
-	area := region.Area()
-	perimeter := region.Perimeter()
-	price := area * perimeter
-	return price
-}
-
-func (solver *Solver) TotalPrice() int {
-	result := 0
+func (solver *Solver) Regions() []Region {
+	regions := make([]Region, 0)
+	toExpand := stack.NewStack[grid.Coord](8)
 
 	// visit each plot
 	for i, kind := range solver.plots.Values {
@@ -119,30 +64,129 @@ func (solver *Solver) TotalPrice() int {
 
 		// start a new region
 		pos := solver.plots.Bounds.IndexToCoord(i)
-		region := MakeRegion(kind, pos, &solver.plots)
+		region := MakeRegion(kind, pos, &solver.plots, &toExpand)
 
 		// fully expand the region
-		for region.toExpand.Len() > 0 {
-			expand := region.toExpand.Pop()
+		for toExpand.Len() > 0 {
+			expand := toExpand.Pop()
 			expandi := solver.plots.Bounds.CoordToIndex(expand)
 			if !solver.explored.Values[expandi] {
 				solver.explored.Values[expandi] = true
-				region.Add(expand, &solver.plots)
+				region.Add(expand, &solver.plots, &toExpand)
 			}
 		}
 
-		// add the cost of the region
-		result += region.Price()
+		// set the perimeter
+		region.CalculatePerimeter()
+
+		// add the region
+		regions = append(regions, region)
 	}
 
-	return result
+	return regions
 }
 
-func part1(plots grid.Grid[rune]) int {
-	solver := MakeSolver(plots)
-	return solver.TotalPrice()
+func MakeRegion(kind rune, pos grid.Coord, plots *grid.Grid[rune], toExpand *stack.Stack[grid.Coord]) Region {
+	region := Region{
+		kind: kind,
+		area: set.NewSet[grid.Coord](),
+		// leave perimeter uninitialized, it will be set later
+	}
+	region.Add(pos, plots, toExpand)
+	return region
 }
 
-func part2(_ grid.Grid[rune]) int {
-	return 0
+func (region *Region) Add(pos grid.Coord, plots *grid.Grid[rune], toExpand *stack.Stack[grid.Coord]) {
+	if region.area.Contains(pos) {
+		return
+	}
+	if posKind, _ := plots.At(pos); region.kind != posKind {
+		panic("Trying to add a point to region of a mismatched kind")
+	}
+
+	region.area.Add(pos)
+	for n := range plots.IterNeighbours(pos) {
+		nKind, _ := plots.At(n)
+		if region.kind == nKind {
+			toExpand.Push(n)
+		}
+	}
+}
+
+func (region *Region) CalculatePerimeter() {
+	edges := set.NewSet[Edge]()
+
+	for pos := range region.area.Iter() {
+		for _, d := range grid.Directions() {
+			n := pos.MoveInDirection(d, 1)
+			if !region.area.Contains(n) {
+				edges.Add(Edge{n, d})
+			}
+		}
+	}
+
+	region.perimeter = edges
+}
+
+func (edge Edge) Neighbours() [2]Edge {
+	switch edge.facing {
+	case grid.North, grid.South:
+		// vertical edge, check left and right
+		left := edge.MoveInDirection(grid.West)
+		right := edge.MoveInDirection(grid.East)
+		return [2]Edge{left, right}
+	case grid.West, grid.East:
+		// horizontal edge, check above and below
+		above := edge.MoveInDirection(grid.North)
+		below := edge.MoveInDirection(grid.South)
+		return [2]Edge{above, below}
+	default:
+		panic("Unreachable")
+	}
+}
+
+func (edge Edge) MoveInDirection(d grid.Direction) Edge {
+	return Edge{
+		edge.pos.MoveInDirection(d, 1),
+		edge.facing,
+	}
+}
+
+func (region *Region) NumSides() int {
+	perimeter := region.perimeter
+
+	neighbourCount := 0
+	for edge := range region.perimeter.Iter() {
+		for _, n := range edge.Neighbours() {
+			if perimeter.Contains(n) {
+				neighbourCount++
+			}
+		}
+	}
+
+	// number of sides is equal to perimeter size minus the adjacent edges
+	// neighbours are counted twice, once from each side, so divide by 2
+	return perimeter.Len() - neighbourCount/2
+}
+
+func (region *Region) Price() int {
+	area := region.area.Len()
+	perimeter := region.perimeter.Len()
+	price := area * perimeter
+	return price
+}
+
+func (region *Region) BulkPrice() int {
+	area := region.area.Len()
+	sides := region.NumSides()
+	price := area * sides
+	return price
+}
+
+func part1(regions []Region) int {
+	return lo.SumBy(regions, func(region Region) int { return region.Price() })
+}
+
+func part2(regions []Region) int {
+	return lo.SumBy(regions, func(region Region) int { return region.BulkPrice() })
 }
--- 2024/day12/solve_test.go
+++ 2024/day12/solve_test.go
@@ -7,7 +7,18 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-const example = `RRRRIICCFF
+const example1 = `AAAA
+BBCD
+BBCC
+EEEC`
+
+const example2 = `OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO`
+
+const example3 = `RRRRIICCFF
 RRRRIICCCF
 VVRRRCCFFF
 VVRCCCJFFF
@@ -18,9 +29,36 @@ MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE`
 
-func TestPart1Example(t *testing.T) {
+const example4 = `EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE`
+
+const example5 = `AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA`
+
+func TestPart1Example1(t *testing.T) {
+	expected := 140
+	input := parseInput(example1)
+	actual := part1(input)
+	assert.Equal(t, expected, actual)
+}
+
+func TestPart1Example2(t *testing.T) {
+	expected := 772
+	input := parseInput(example2)
+	actual := part1(input)
+	assert.Equal(t, expected, actual)
+}
+
+func TestPart1Example3(t *testing.T) {
 	expected := 1930
-	input := parseInput(example)
+	input := parseInput(example3)
 	actual := part1(input)
 	assert.Equal(t, expected, actual)
 }
@@ -32,15 +70,43 @@ func TestPart1Input(t *testing.T) {
 	assert.Equal(t, expected, actual)
 }
 
-func TestPart2Example(t *testing.T) {
-	expected := 0
-	input := parseInput(example)
+func TestPart2Example1(t *testing.T) {
+	expected := 80
+	input := parseInput(example1)
+	actual := part2(input)
+	assert.Equal(t, expected, actual)
+}
+
+func TestPart2Example2(t *testing.T) {
+	expected := 436
+	input := parseInput(example2)
+	actual := part2(input)
+	assert.Equal(t, expected, actual)
+}
+
+func TestPart2Example3(t *testing.T) {
+	expected := 1206
+	input := parseInput(example3)
+	actual := part2(input)
+	assert.Equal(t, expected, actual)
+}
+
+func TestPart2Example4(t *testing.T) {
+	expected := 236
+	input := parseInput(example4)
+	actual := part2(input)
+	assert.Equal(t, expected, actual)
+}
+
+func TestPart2Example5(t *testing.T) {
+	expected := 368
+	input := parseInput(example5)
 	actual := part2(input)
 	assert.Equal(t, expected, actual)
 }
 
 func TestPart2Input(t *testing.T) {
-	expected := 0
+	expected := 873584
 	input := parseInput(util.ReadInputFile("."))
 	actual := part2(input)
 	assert.Equal(t, expected, actual)
